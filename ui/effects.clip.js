8ec7ceea10d62abafbf79ea0f21ff31a2085aa44
Aaron Eisenberger <aaronchi@gmail.com>
2008-06-16 13:18:48 +0000

    clip effect should be animating the wrapper

--- ui/effects.clip.js
+++ ui/effects.clip.js
@@ -17,7 +17,7 @@ $.effects.clip = function(o) {
 	return this.queue(function() {
 
 		// Create element
-		var el = $(this), props = ['position','top','left','width','height'];
+		var el = $(this), props = ['position','top','left'];
 		
 		// Set options
 		var mode = $.effects.setMode(el, o.options.mode || 'hide'); // Set Mode
@@ -25,13 +25,13 @@ $.effects.clip = function(o) {
 		
 		// Adjust
 		$.effects.save(el, props); el.show(); // Save & Show
-		$.effects.createWrapper(el).css({overflow:'hidden'}); // Create Wrapper
+		var wrapper = $.effects.createWrapper(el).css({overflow:'hidden'}); // Create Wrapper
 		var ref = {
 			size: (direction == 'vertical') ? 'height' : 'width',
 			position: (direction == 'vertical') ? 'top' : 'left'
 		};
-		var distance = (direction == 'vertical') ? el.height() : el.width();
-		if(mode == 'show') { el.css(ref.size, 0); el.css(ref.position, distance / 2); } // Shift
+		var distance = (direction == 'vertical') ? wrapper.height() : wrapper.width();
+		if(mode == 'show') { wrapper.css(ref.size, 0); wrapper.css(ref.position, distance / 2); } // Shift
 		
 		// Animation
 		var animation = {};
@@ -39,7 +39,7 @@ $.effects.clip = function(o) {
 		animation[ref.position] = mode == 'show' ? 0 : distance / 2;
 			
 		// Animate
-		el.animate(animation, { queue: false, duration: o.duration, easing: o.options.easing, complete: function() {
+		wrapper.animate(animation, { queue: false, duration: o.duration, easing: o.options.easing, complete: function() {
 			if(mode == 'hide') el.hide(); // Hide
 			$.effects.restore(el, props); $.effects.removeWrapper(el); // Restore
 			if(o.callback) o.callback.apply(this, arguments); // Callback
