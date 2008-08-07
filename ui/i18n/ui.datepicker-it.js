69c006c07ffcff54c33979059a7ffe2ab65b47c1
Chi Cheng <cloudream@gmail.com>
2008-08-07 16:51:43 +0000

    Datepicker:The updated and complete version of Datepicker Italian localization.(fixes #3167)

--- ui/i18n/ui.datepicker-it.js
+++ ui/i18n/ui.datepicker-it.js
@@ -2,24 +2,24 @@
 /* Written by Apaella (apaella@gmail.com). */
 jQuery(function($){
 	$.datepicker.regional['it'] = {
-		clearText: 'Svuota', clearStatus: '',
-		closeText: 'Chiudi', closeStatus: '',
-		prevText: '&#x3c;Prec', prevStatus: '',
-		prevBigText: '&#x3c;&#x3c;', prevBigStatus: '',
-		nextText: 'Succ&#x3e;', nextStatus: '',
-		nextBigText: '&#x3e;&#x3e;', nextBigStatus: '',
-		currentText: 'Oggi', currentStatus: '',
+		clearText: 'Svuota', clearStatus: 'Annulla',
+		closeText: 'Chiudi', closeStatus: 'Chiudere senza modificare',
+		prevText: '&#x3c;Prec', prevStatus: 'Mese precedente',
+		prevBigText: '&#x3c;&#x3c;', prevBigStatus: 'Mostra l\'anno precedente',
+		nextText: 'Succ&#x3e;', nextStatus: 'Mese successivo',
+		nextBigText: '&#x3e;&#x3e;', nextBigStatus: 'Mostra l\'anno successivo',
+		currentText: 'Oggi', currentStatus: 'Mese corrente',
 		monthNames: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno',
 		'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
 		monthNamesShort: ['Gen','Feb','Mar','Apr','Mag','Giu',
 		'Lug','Ago','Set','Ott','Nov','Dic'],
-		monthStatus: '', yearStatus: '',
-		weekHeader: 'Sm', weekStatus: '',
+		monthStatus: 'Seleziona un altro mese', yearStatus: 'Seleziona un altro anno',
+		weekHeader: 'Sm', weekStatus: 'Settimana dell\'anno',
 		dayNames: ['Domenica','Luned&#236','Marted&#236','Mercoled&#236','Gioved&#236','Venerd&#236','Sabato'],
 		dayNamesShort: ['Dom','Lun','Mar','Mer','Gio','Ven','Sab'],
 		dayNamesMin: ['Do','Lu','Ma','Me','Gio','Ve','Sa'],
-		dayStatus: 'DD', dateStatus: 'D, M d',
+		dayStatus: 'Usa DD come primo giorno della settimana', dateStatus: 'Seleziona D, M d',
 		dateFormat: 'dd/mm/yy', firstDay: 1, 
-		initStatus: '', isRTL: false};
+		initStatus: 'Scegliere una data', isRTL: false};
 	$.datepicker.setDefaults($.datepicker.regional['it']);
 });
