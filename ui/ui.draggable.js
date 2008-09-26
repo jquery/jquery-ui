9246c5117f476a1349e5fec2ae5785f8770f17b0
Eduardo Lundgren <eduardolundgren@gmail.com>
2008-09-26 02:29:46 +0000

    Draggable cancelHelperRemoval when is not over

--- ui/ui.draggable.js
+++ ui/ui.draggable.js
@@ -572,6 +572,7 @@ $.ui.plugin.add("draggable", "connectToSortable", {
 				
 				this.instance.options.helper = this.instance.options._helper;
 			} else {
+				this.instance.cancelHelperRemoval = false; //Remove the helper in the sortable instance
 				this.instance._propagate("deactivate", e, inst);
 			}
 			
