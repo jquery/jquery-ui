a08dfc9f6f53658494a8e5fa00a2846dea96e626
Richard Worth <rdworth@gmail.com>
2008-11-11 13:32:48 +0000

    Added new test suite for common tests across all UI plugins

---
+++ tests/ui.js
@@ -0,0 +1,38 @@
+/*
+ * core unit tests
+ */
+(function($) {
+
+var plugins = [
+	"accordion",
+	"colorpicker",
+	"datepicker",
+	"dialog",
+	"draggable",
+	"droppable",
+	"progressbar",
+	"resizable",
+	"selectable",
+	"slider",
+	"sortable",
+	"spinner",
+	"tabs"
+];
+
+module("version");
+
+test("core", function() {
+	equals($.ui.version, "@VERSION", "$.ui.version");
+});
+
+$(plugins).each(function() {
+	var pluginName = this;
+	test(pluginName, function() {
+		if ($.ui[pluginName])
+			equals($.ui[pluginName].version, "@VERSION", "$.ui." + pluginName + ".version");
+		else
+			ok(false, "$.ui." + pluginName + " undefined.");
+	});
+});
+
+})(jQuery);
