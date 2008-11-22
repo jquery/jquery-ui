0472ff10bbd12648b57905d0833fb6e572853dd5
Richard Worth <rdworth@gmail.com>
2008-11-22 19:29:03 +0000

    tests: formatting and cleanup for consistency

---
+++ tests/testsuite.js
@@ -0,0 +1,10 @@
+$(function() {
+
+$('body').prepend(
+	'<h1 id="header">' + document.title + '</h1>' +
+	'<h2 id="banner"></h2>' +
+	'<h2 id="userAgent"></h2>' +
+	'<ol id="tests"></ol>'
+);
+
+});
