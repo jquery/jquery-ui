152531d37059895a06cb7f86b7249cce481f9370
Keith Wood <kbwood.au@gmail.com>
2008-12-01 10:56:21 +0000

    Updated Dutch localisation

--- ui/i18n/ui.datepicker-nl.js
+++ ui/i18n/ui.datepicker-nl.js
@@ -1,25 +1,25 @@
-/* Dutch (UTF-8) initialisation for the jQuery UI date picker plugin. */
-/* Written by ??? */
+﻿/* Dutch (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Mathias Bynens <http://mathiasbynens.be/> */
 jQuery(function($){
-	$.datepicker.regional['nl'] = {
+	$.datepicker.regional.nl = {
 		clearText: 'Wissen', clearStatus: 'Wis de huidige datum',
 		closeText: 'Sluiten', closeStatus: 'Sluit zonder verandering',
-		prevText: '&#x3c;Terug', prevStatus: 'Laat de voorgaande maand zien',
-		prevBigText: '&#x3c;&#x3c;', prevBigStatus: '',
-		nextText: 'Volgende&#x3e;', nextStatus: 'Laat de volgende maand zien',
-		nextBigText: '&#x3e;&#x3e;', nextBigStatus: '',
-		currentText: 'Vandaag', currentStatus: 'Laat de huidige maand zien',
-		monthNames: ['Januari','Februari','Maart','April','Mei','Juni',
-		'Juli','Augustus','September','Oktober','November','December'],
-		monthNamesShort: ['Jan','Feb','Mrt','Apr','Mei','Jun',
-		'Jul','Aug','Sep','Okt','Nov','Dec'],
-		monthStatus: 'Laat een andere maand zien', yearStatus: 'Laat een ander jaar zien',
+		prevText: '←', prevStatus: 'Bekijk de vorige maand',
+		prevBigText: '«', nextBigStatus: 'Bekijk het vorige jaar',
+		nextText: '→', nextStatus: 'Bekijk de volgende maand',
+		nextBigText: '»', nextBigStatus: 'Bekijk het volgende jaar',
+		currentText: 'Vandaag', currentStatus: 'Bekijk de huidige maand',
+		monthNames: ['januari', 'februari', 'maart', 'april', 'mei', 'juni',
+		'juli', 'augustus', 'september', 'oktober', 'november', 'december'],
+		monthNamesShort: ['jan', 'feb', 'maa', 'apr', 'mei', 'jun',
+		'jul', 'aug', 'sep', 'okt', 'nov', 'dec'],
+		monthStatus: 'Bekijk een andere maand', yearStatus: 'Bekijk een ander jaar',
 		weekHeader: 'Wk', weekStatus: 'Week van het jaar',
-		dayNames: ['Zondag','Maandag','Dinsdag','Woensdag','Donderdag','Vrijdag','Zaterdag'],
-		dayNamesShort: ['Zon','Maa','Din','Woe','Don','Vri','Zat'],
-		dayNamesMin: ['Zo','Ma','Di','Wo','Do','Vr','Za'],
-		dayStatus: 'DD', dateStatus: 'D, M d',
-		dateFormat: 'dd.mm.yy', firstDay: 1,
+		dayNames: ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'],
+		dayNamesShort: ['zon', 'maa', 'din', 'woe', 'don', 'vri', 'zat'],
+		dayNamesMin: ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za'],
+		dayStatus: 'Stel DD in als eerste dag van de week', dateStatus: 'dd/mm/yy',
+		dateFormat: 'dd/mm/yy', firstDay: 1,
 		initStatus: 'Kies een datum', isRTL: false};
-	$.datepicker.setDefaults($.datepicker.regional['nl']);
+	$.datepicker.setDefaults($.datepicker.regional.nl);
 });
