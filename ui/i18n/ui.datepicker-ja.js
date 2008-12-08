b870c1c63f00596ff22a3f79222e474a58e06a46
Keith Wood <kbwood.au@gmail.com>
2008-12-08 10:12:12 +0000

    Fixed 3598. Datepicker Japanese translation replacement

--- ui/i18n/ui.datepicker-ja.js
+++ ui/i18n/ui.datepicker-ja.js
@@ -1,25 +1,26 @@
-/* Japanese (UTF-8) initialisation for the jQuery UI date picker plugin. */
-/* Written by Milly. */
+﻿/* Japanese initialisation for the jQuery UI date picker plugin. */
+/* Written by Kentaro SATO (kentaro@ranvis.com). */
 jQuery(function($){
 	$.datepicker.regional['ja'] = {
-		clearText: '&#21066;&#38500;', clearStatus: '',
-		closeText: '&#38281;&#12376;&#12427;', closeStatus: '',
-		prevText: '&#x3c;&#21069;&#26376;', prevStatus: '',
-		prevBigText: '&#x3c;&#x3c;', prevBigStatus: '',
-		nextText: '&#27425;&#26376;&#x3e;', nextStatus: '',
-		nextBigText: '&#x3e;&#x3e;', nextBigStatus: '',
-		currentText: '&#20170;&#26085;', currentStatus: '',
-		monthNames: ['1&#26376;','2&#26376;','3&#26376;','4&#26376;','5&#26376;','6&#26376;',
-		'7&#26376;','8&#26376;','9&#26376;','10&#26376;','11&#26376;','12&#26376;'],
-		monthNamesShort: ['1&#26376;','2&#26376;','3&#26376;','4&#26376;','5&#26376;','6&#26376;',
-		'7&#26376;','8&#26376;','9&#26376;','10&#26376;','11&#26376;','12&#26376;'],
-		monthStatus: '', yearStatus: '',
-		weekHeader: 'Wk', weekStatus: '',
-		dayNames: ['&#26085;','&#26376;','&#28779;','&#27700;','&#26408;','&#37329;','&#22303;'],
-		dayNamesShort: ['&#26085;','&#26376;','&#28779;','&#27700;','&#26408;','&#37329;','&#22303;'],
-		dayNamesMin: ['&#26085;','&#26376;','&#28779;','&#27700;','&#26408;','&#37329;','&#22303;'],
-		dayStatus: 'DD', dateStatus: 'D, M d',
+		clearText: 'クリア', clearStatus: '日付をクリアします',
+		closeText: '閉じる', closeStatus: '変更せずに閉じます',
+		prevText: '&#x3c;前', prevStatus: '前月を表示します',
+		prevBigText: '&#x3c;&#x3c;', prevBigStatus: '前年を表示します',
+		nextText: '次&#x3e;', nextStatus: '翌月を表示します',
+		nextBigText: '&#x3e;&#x3e;', nextBigStatus: '翌年を表示します',
+		currentText: '今日', currentStatus: '今月を表示します',
+		monthNames: ['1月','2月','3月','4月','5月','6月',
+		'7月','8月','9月','10月','11月','12月'],
+		monthNamesShort: ['1月','2月','3月','4月','5月','6月',
+		'7月','8月','9月','10月','11月','12月'],
+		monthStatus: '表示する月を変更します', yearStatus: '表示する年を変更します',
+		weekHeader: '週', weekStatus: '暦週で第何週目かを表します',
+		dayNames: ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+		dayNamesShort: ['日','月','火','水','木','金','土'],
+		dayNamesMin: ['日','月','火','水','木','金','土'],
+		dayStatus: '週の始まりをDDにします', dateStatus: 'Md日(D)',
 		dateFormat: 'yy/mm/dd', firstDay: 0,
-		initStatus: '', isRTL: false};
+		initStatus: '日付を選択します', isRTL: false,
+		showMonthAfterYear: true};
 	$.datepicker.setDefaults($.datepicker.regional['ja']);
 });
