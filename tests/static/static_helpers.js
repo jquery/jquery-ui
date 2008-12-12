ba9bb22b55e0ba281ef9ac38089f3dd98206112b
Richard Worth <rdworth@gmail.com>
2008-12-12 02:31:34 +0000

    tests/static: whitespace and formatting

--- tests/static/static_helpers.js
+++ tests/static/static_helpers.js
@@ -1,3 +1,5 @@
+/* static_helpers.js
+ */
 $(function(){
 	//add hover states on the static widgets
 	$('.ui-state-default:not(.ui-state-disabled, .ui-slider-range, .ui-progressbar-value), a.ui-datepicker-next, a.ui-datepicker-prev, .ui-dialog-titlebar-close').hover(
