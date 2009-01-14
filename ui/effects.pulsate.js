ee28d263b6ffcf73066d776d5add2726b0fdd282
Scott González <scott.gonzalez@gmail.com>
2009-01-14 02:00:25 +0000

    Pulsate: Fixed duration.

--- ui/effects.pulsate.js
+++ ui/effects.pulsate.js
@@ -22,27 +22,28 @@ $.effects.pulsate = function(o) {
 		// Set options
 		var mode = $.effects.setMode(el, o.options.mode || 'show'); // Set Mode
 		var times = o.options.times || 5; // Default # of times
-
+		var duration = o.duration ? o.duration / 2 : $.fx.speeds._default / 2;
+		
 		// Adjust
 		if (mode == 'hide') times--;
 		if (el.is(':hidden')) { // Show fadeIn
 			el.css('opacity', 0);
 			el.show(); // Show
-			el.animate({opacity: 1}, o.duration / 2, o.options.easing);
+			el.animate({opacity: 1}, duration, o.options.easing);
 			times = times-2;
 		}
 
 		// Animate
 		for (var i = 0; i < times; i++) { // Pulsate
-			el.animate({opacity: 0}, o.duration / 2, o.options.easing).animate({opacity: 1}, o.duration / 2, o.options.easing);
+			el.animate({opacity: 0}, duration, o.options.easing).animate({opacity: 1}, duration, o.options.easing);
 		};
 		if (mode == 'hide') { // Last Pulse
-			el.animate({opacity: 0}, o.duration / 2, o.options.easing, function(){
+			el.animate({opacity: 0}, duration, o.options.easing, function(){
 				el.hide(); // Hide
 				if(o.callback) o.callback.apply(this, arguments); // Callback
 			});
 		} else {
-			el.animate({opacity: 0}, o.duration / 2, o.options.easing).animate({opacity: 1}, o.duration / 2, o.options.easing, function(){
+			el.animate({opacity: 0}, duration, o.options.easing).animate({opacity: 1}, duration, o.options.easing, function(){
 				if(o.callback) o.callback.apply(this, arguments); // Callback
 			});
 		};
