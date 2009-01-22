jquery/jquery-ui

commit 0e5ff0b8b47bdabb1ddda8d69f61c59a7e1a4fe0

accordion: added missing defaults (fixes #3884)
git-svn-id: http://jquery-ui.googlecode.com/svn/trunk@1715 a1defee3-d24d-0410-b1e3-3171fe540af7

ui/ui.accordion.js
@@ -372,15 +372,19 @@ $.widget("ui.accordion", {
 $.extend($.ui.accordion, {
 	version: "@VERSION",
 	defaults: {
+		active: false,
 		autoHeight: true,
 		alwaysOpen: true,
 		animated: 'slide',
+		clearStyle: false,
 		event: "click",
+		fillSpace: false,
 		header: "a",
 		icons: {
 			header: "ui-icon-triangle-1-e",
 			headerSelected: "ui-icon-triangle-1-s"
 		},
+		navigation: false,
 		navigationFilter: function() {
 			return this.href.toLowerCase() == location.href.toLowerCase();
 		},

commit 4176d55bfbf4dc4cb1d7704fc9d52c1fb345dfc0

accordion: fixed always open regression introduced by r1715

ui/ui.accordion.js
@@ -372,7 +372,7 @@ $.widget("ui.accordion", {
 $.extend($.ui.accordion, {
 	version: "@VERSION",
 	defaults: {
-		active: false,
+		active: null,
 		autoHeight: true,
 		alwaysOpen: true,
 		animated: 'slide',