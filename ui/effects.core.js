27a7442a37e982c0bda7e11a3115eb6aba6ed930
Paul Bakaus <paul.bakaus@googlemail.com>
2009-02-05 01:39:58 +0000

    effect core: speed wasn't detected in all situations (fixes #4057)

--- ui/effects.core.js
+++ ui/effects.core.js
@@ -136,7 +136,7 @@ $.extend($.effects, {
 function _normalizeArguments(a, m) {
 
 	var o = a[1] && a[1].constructor == Object ? a[1] : {}; if(m) o.mode = m;
-	var speed = a[1] && a[1].constructor != Object ? a[1] : o.duration; //either comes from options.duration or the second argument
+	var speed = a[1] && a[1].constructor != Object ? a[1] : (o.duration ? o.duration : a[2]); //either comes from options.duration or the secon/third argument
 		speed = $.fx.off ? 0 : typeof speed === "number" ? speed : $.fx.speeds[speed] || $.fx.speeds._default;
 	var callback = o.callback || ( $.isFunction(a[2]) && a[2] ) || ( $.isFunction(a[3]) && a[3] );
 
