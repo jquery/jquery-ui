e4c5712974a98733e04a50e183e6ad077d959f2a
Richard Worth <rdworth@gmail.com>
2009-02-08 00:55:16 +0000

    Removed unused code obsoleted by #3906 and r1754

--- ui/ui.core.js
+++ ui/ui.core.js
@@ -43,24 +43,6 @@ $.ui = {
 			: a !== b && a.contains(b);
 	},
 
-	cssCache: {},
-	css: function(name) {
-		if ($.ui.cssCache[name]) { return $.ui.cssCache[name]; }
-		var tmp = $('<div class="ui-gen"></div>').addClass(name).css({position:'absolute', top:'-5000px', left:'-5000px', display:'block'}).appendTo('body');
-
-		//if (!$.browser.safari)
-			//tmp.appendTo('body');
-
-		//Opera and Safari set width and height to 0px instead of auto
-		//Safari returns rgba(0,0,0,0) when bgcolor is not set
-		$.ui.cssCache[name] = !!(
-			(!(/auto|default/).test(tmp.css('cursor')) || (/^[1-9]/).test(tmp.css('height')) || (/^[1-9]/).test(tmp.css('width')) ||
-			!(/none/).test(tmp.css('backgroundImage')) || !(/transparent|rgba\(0, 0, 0, 0\)/).test(tmp.css('backgroundColor')))
-		);
-		try { $('body').get(0).removeChild(tmp.get(0));	} catch(e){}
-		return $.ui.cssCache[name];
-	},
-
 	hasScroll: function(el, a) {
 
 		//If overflow is hidden, the element might have extra content, but the user wants to hide it
