95c5d5e3fd061dac6ef2a779ee18b033b7b09a52
Scott González <scott.gonzalez@gmail.com>
2009-02-13 04:12:28 +0000

    Datepicker: Added test for #4055 - onclick events contain references to "jQuery".

--- tests/unit/datepicker/datepicker_tickets.js
+++ tests/unit/datepicker/datepicker_tickets.js
@@ -5,4 +5,18 @@
 
 module("datepicker: tickets");
 
+test('#4055: onclick events contain references to "jQuery"', function() {
+	// no assertions, if the test fails, there will be an error
+	
+	var _jQuery = jQuery;
+	jQuery = null;
+	
+	$('<div/>').appendTo('body').datepicker()
+	// the third weekend day always exists
+	.find('tbody .ui-datepicker-week-end:eq(3)').click().end()
+	.datepicker('destroy');
+	
+	jQuery = _jQuery;
+});
+
 })(jQuery);
