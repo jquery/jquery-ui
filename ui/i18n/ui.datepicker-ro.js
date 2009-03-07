248d67eaa909950b00faf1b6afca3a82262ea9e9
Chi Cheng <cloudream@gmail.com>
2009-03-07 04:59:57 +0000

    Datepicker:  Update Romanian Localization, thanks Ionut G. Stan

--- ui/i18n/ui.datepicker-ro.js
+++ ui/i18n/ui.datepicker-ro.js
@@ -1,19 +1,22 @@
-/* Romanian initialisation for the jQuery UI date picker plugin. */
-/* Written by Edmond L. (ll_edmond@walla.com). */
+/* Romanian initialisation for the jQuery UI date picker plugin.
+ *
+ * Written by Edmond L. (ll_edmond@walla.com)
+ * and Ionut G. Stan (ionut.g.stan@gmail.com)
+ */
 jQuery(function($){
 	$.datepicker.regional['ro'] = {
-		closeText: 'Inchide',
-		prevText: '&#x3c;Anterior',
-		nextText: 'Urmator&#x3e;',
+		closeText: 'Închide',
+		prevText: '&laquo; Luna precedentă',
+		nextText: 'Luna următoare &raquo;',
 		currentText: 'Azi',
-		monthNames: ['Ianuarie','Februarie','Martie','Aprilie','Mai','Junie',
-		'Julie','August','Septembrie','Octobrie','Noiembrie','Decembrie'],
-		monthNamesShort: ['Ian', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun',
-		'Jul', 'Aug', 'Sep', 'Oct', 'Noi', 'Dec'],
-		dayNames: ['Duminica', 'Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata'],
-		dayNamesShort: ['Dum', 'Lun', 'Mar', 'Mie', 'Joi', 'Vin', 'Sam'],
-		dayNamesMin: ['Du','Lu','Ma','Mi','Jo','Vi','Sa'],
-		dateFormat: 'mm/dd/yy', firstDay: 0,
+		monthNames: ['Ianuarie','Februarie','Martie','Aprilie','Mai','Iunie',
+		'Iulie','August','Septembrie','Octombrie','Noiembrie','Decembrie'],
+		monthNamesShort: ['Ian', 'Feb', 'Mar', 'Apr', 'Mai', 'Iun',
+		'Iul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		dayNames: ['Duminică', 'Luni', 'Marţi', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă'],
+		dayNamesShort: ['Dum', 'Lun', 'Mar', 'Mie', 'Joi', 'Vin', 'Sâm'],
+		dayNamesMin: ['Du','Lu','Ma','Mi','Jo','Vi','Sâ'],
+		dateFormat: 'dd MM yy', firstDay: 1,
 		isRTL: false};
 	$.datepicker.setDefaults($.datepicker.regional['ro']);
 });
