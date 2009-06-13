d8d7cf27c38a153337dbc12f2848573e06159690
Scott González <scott.gonzalez@gmail.com>
2009-06-13 12:52:48 +0000

    Fixed Italian localization for datepicker.  (Fixed #4599 - Datepicker i18n: Fix Italian localization).  Thanks Antonello Pasella.

--- ui/i18n/ui.datepicker-it.js
+++ ui/i18n/ui.datepicker-it.js
@@ -1,5 +1,5 @@
 /* Italian initialisation for the jQuery UI date picker plugin. */
-/* Written by Apaella (apaella@gmail.com). */
+/* Written by Antonello Pasella (antonello.pasella@gmail.com). */
 jQuery(function($){
 	$.datepicker.regional['it'] = {
 		closeText: 'Chiudi',
@@ -7,12 +7,12 @@ jQuery(function($){
 		nextText: 'Succ&#x3e;',
 		currentText: 'Oggi',
 		monthNames: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno',
-		'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
+			'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
 		monthNamesShort: ['Gen','Feb','Mar','Apr','Mag','Giu',
-		'Lug','Ago','Set','Ott','Nov','Dic'],
+			'Lug','Ago','Set','Ott','Nov','Dic'],
 		dayNames: ['Domenica','Luned&#236','Marted&#236','Mercoled&#236','Gioved&#236','Venerd&#236','Sabato'],
 		dayNamesShort: ['Dom','Lun','Mar','Mer','Gio','Ven','Sab'],
-		dayNamesMin: ['Do','Lu','Ma','Me','Gio','Ve','Sa'],
+		dayNamesMin: ['Do','Lu','Ma','Me','Gi','Ve','Sa'],
 		dateFormat: 'dd/mm/yy',
 		firstDay: 1,
 		isRTL: false,
