4dd740a5ac61e0996d9ff0093f088b43967bad4f
Jörn Zaefferer <joern.zaefferer@gmail.com>
2009-09-11 18:22:52 +0000

    inject testswarm agent when the test is run by testswarm

--- tests/unit/testsuite.js
+++ tests/unit/testsuite.js
@@ -91,3 +91,13 @@ function commonWidgetTests(widget, settings) {
 	testSettingOptions(widget, options);
 	testWidgetOverrides(widget);
 }
+
+// load testswarm agent
+(function() {
+    var url = window.location.search;
+	url = decodeURIComponent( url.slice( url.indexOf("swarmURL=") + 9 ) );
+	if ( !url || url.indexOf("http") !== 0 ) {
+		return;
+	}
+    document.write("<scr" + "ipt src='http://testswarm.com/js/inject.js?" + (new Date).getTime() + "'></scr" + "ipt>");
+})();
