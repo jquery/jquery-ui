f6b3e0c6e9b52e0c2aba7ae7224b6a1c9e2deb90
Ca-Phun Ung <pazu2k@gmail.com>
2009-09-15 16:14:15 +0000

    dialog: added unit test for ticket #4826

--- tests/unit/dialog/dialog_tickets.js
+++ tests/unit/dialog/dialog_tickets.js
@@ -5,4 +5,25 @@
 
 module("dialog: tickets");
 
+test("#4826: setting resizable false toggles resizable on dialog", function() {
+	expect(6);
+
+	el = $('<div></div>').dialog({ resizable: false });
+	shouldnotresize("[default]");
+	for (var i=0; i<2; i++) {
+		el.dialog('close').dialog('open');
+		shouldnotresize('initialized with resizable false toggle ('+ (i+1) +')');		
+	}
+	el.remove();
+
+	el = $('<div></div>').dialog({ resizable: true });
+	shouldresize("[default]");
+	for (var i=0; i<2; i++) {
+		el.dialog('close').dialog('option', 'resizable', false).dialog('open');
+		shouldnotresize('set option resizable false toggle ('+ (i+1) +')');		
+	}
+	el.remove();
+	
+});
+
 })(jQuery);
