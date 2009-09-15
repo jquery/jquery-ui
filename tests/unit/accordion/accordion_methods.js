5db2f6b7f7e29a05fe19aacce5bb5405da8a6d24
Jörn Zaefferer <joern.zaefferer@gmail.com>
2009-09-15 21:19:23 +0000

    accordion: reverted previous change to test

--- tests/unit/accordion/accordion_methods.js
+++ tests/unit/accordion/accordion_methods.js
@@ -130,7 +130,7 @@ test("activate, string expression", function() {
 	ac.accordion("activate", ":eq(1)");
 	state(ac, 0, 1, 0);
 	ac.accordion("activate", ":last");
-	state(ac, 0, 1, 1);
+	state(ac, 0, 0, 1);
 });
 //[ 0, 1, 1 ] result: [ 0, 0, 1 ]
 //[   0,   1,   1] result: [   0,   0,   1]
