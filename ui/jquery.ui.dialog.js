5b61537e6e759cacc4295887200db6ac07086f54
Brant Burnett <btburnett3@gmail.com>
2010-02-01 16:10:41 +0000

    Datepicker: Fixed #4453 UI Datepicker inside UI Dialog Issue

--- ui/jquery.ui.dialog.js
+++ ui/jquery.ui.dialog.js
@@ -601,7 +601,7 @@ $.extend($.ui.dialog.overlay, {
 				if ($.ui.dialog.overlay.instances.length) {
 					$(document).bind($.ui.dialog.overlay.events, function(event) {
 						// stop events if the z-index of the target is <= the z-index of the overlay
-						return ($(event.target).zIndex() > $.ui.dialog.overlay.maxZ);
+						return ($(event.target).zIndex() >= $.ui.dialog.overlay.maxZ);
 					});
 				}
 			}, 1);
