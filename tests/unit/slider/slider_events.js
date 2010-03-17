5c5f9ad69542e7ae3806611da648c463e96c32ba
Richard Worth <rdworth@gmail.com>
2010-03-17 15:04:14 +0000

    whitespace

--- tests/unit/slider/slider_events.js
+++ tests/unit/slider/slider_events.js
@@ -3,22 +3,22 @@
  */
 (function($) {
 
-module("slider: events");
+module( "slider: events" );
 
-test("start", function() {
-	ok(false, "missing test - untested code is broken code.");
+test( "start", function() {
+	ok( false, "missing test - untested code is broken code." );
 });
 
-test("slide", function() {
-	ok(false, "missing test - untested code is broken code.");
+test( "slide", function() {
+	ok( false, "missing test - untested code is broken code." );
 });
 
-test("change", function() {
-	ok(false, "missing test - untested code is broken code.");
+test( "change", function() {
+	ok( false, "missing test - untested code is broken code." );
 });
 
-test("stop", function() {
-	ok(false, "missing test - untested code is broken code.");
+test( "stop", function() {
+	ok( false, "missing test - untested code is broken code." );
 });
 
-})(jQuery);
+}( jQuery ) );
