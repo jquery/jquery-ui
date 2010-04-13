094ab5a5d9239c094b2f1af95669b9b0baaeee4a
jzaefferer <joern.zaefferer@gmail.com>
2010-04-13 11:12:01 +0200

    Autocomplete and button unit tests: Same remove-role-attr fix as for accordion in destroy-method test

--- tests/unit/button/button_methods.js
+++ tests/unit/button/button_methods.js
@@ -9,6 +9,10 @@ module("button: methods");
 test("destroy", function() {
 	var beforeHtml = $("#button").parent().html();
 	var afterHtml = $("#button").button().button("destroy").parent().html();
+	// Opera 9 outputs role="" instead of removing the attribute like everyone else
+	if ($.browser.opera) {
+		afterHtml = afterHtml.replace(/ role=""/g, "");
+	}
 	equal( afterHtml, beforeHtml );
 });
 
