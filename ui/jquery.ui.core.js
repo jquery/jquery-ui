27095ef8e1cfe7a4229c7182af0ff744f0e85669
Scott González <scott.gonzalez@gmail.com>
2010-07-22 19:26:37 -0400

    Core: Coding standards.

--- ui/jquery.ui.core.js
+++ ui/jquery.ui.core.js
@@ -13,68 +13,74 @@
 // this is only a problem because we proxy existing functions
 // and we don't want to double proxy them
 $.ui = $.ui || {};
-if ($.ui.version) {
+if ( $.ui.version ) {
 	return;
 }
 
 //Helper functions and ui object
-$.extend($.ui, {
+$.extend( $.ui, {
 	version: "@VERSION",
 
 	// $.ui.plugin is deprecated.  Use the proxy pattern instead.
 	plugin: {
-		add: function(module, option, set) {
-			var proto = $.ui[module].prototype;
-			for(var i in set) {
-				proto.plugins[i] = proto.plugins[i] || [];
-				proto.plugins[i].push([option, set[i]]);
+		add: function( module, option, set ) {
+			var proto = $.ui[ module ].prototype;
+			for ( var i in set ) {
+				proto.plugins[ i ] = proto.plugins[ i ] || [];
+				proto.plugins[ i ].push( [ option, set[ i ] ] );
 			}
 		},
-		call: function(instance, name, args) {
-			var set = instance.plugins[name];
-			if(!set || !instance.element[0].parentNode) { return; }
+		call: function( instance, name, args ) {
+			var set = instance.plugins[ name ];
+			if ( !set || !instance.element[ 0 ].parentNode ) {
+				return;
+			}
 
-			for (var i = 0; i < set.length; i++) {
-				if (instance.options[set[i][0]]) {
-					set[i][1].apply(instance.element, args);
+			for ( var i = 0; i < set.length; i++ ) {
+				if ( instance.options[ set[ i ][ 0 ] ] ) {
+					set[ i ][ 1 ].apply( instance.element, args );
 				}
 			}
 		}
 	},
 
-	contains: function(a, b) {
-		return document.compareDocumentPosition
-			? a.compareDocumentPosition(b) & 16
-			: a !== b && a.contains(b);
+	contains: function( a, b ) {
+		return document.compareDocumentPosition ?
+			a.compareDocumentPosition( b ) & 16 :
+			a !== b && a.contains( b );
 	},
 
-	hasScroll: function(el, a) {
+	hasScroll: function( el, a ) {
 
 		//If overflow is hidden, the element might have extra content, but the user wants to hide it
-		if ($(el).css('overflow') == 'hidden') { return false; }
+		if ( $( el ).css( "overflow" ) === "hidden") {
+			return false;
+		}
 
-		var scroll = (a && a == 'left') ? 'scrollLeft' : 'scrollTop',
+		var scroll = ( a && a === "left" ) ? "scrollLeft" : "scrollTop",
 			has = false;
 
-		if (el[scroll] > 0) { return true; }
+		if ( el[ scroll ] > 0 ) {
+			return true;
+		}
 
 		// TODO: determine which cases actually cause this to happen
 		// if the element doesn't have the scroll set, see if it's possible to
 		// set the scroll
-		el[scroll] = 1;
-		has = (el[scroll] > 0);
-		el[scroll] = 0;
+		el[ scroll ] = 1;
+		has = ( el[ scroll ] > 0 );
+		el[ scroll ] = 0;
 		return has;
 	},
 
-	isOverAxis: function(x, reference, size) {
+	isOverAxis: function( x, reference, size ) {
 		//Determines when x coordinate is over "b" element axis
-		return (x > reference) && (x < (reference + size));
+		return ( x > reference ) && ( x < ( reference + size ) );
 	},
 
-	isOver: function(y, x, top, left, height, width) {
+	isOver: function( y, x, top, left, height, width ) {
 		//Determines when x, y coordinates is over "b" element
-		return $.ui.isOverAxis(y, top, height) && $.ui.isOverAxis(x, left, width);
+		return $.ui.isOverAxis( y, top, height ) && $.ui.isOverAxis( x, left, width );
 	},
 
 	keyCode: {
@@ -116,33 +122,35 @@ $.extend($.ui, {
 //jQuery plugins
 $.fn.extend({
 	_focus: $.fn.focus,
-	focus: function(delay, fn) {
-		return typeof delay === 'number'
-			? this.each(function() {
+	focus: function( delay, fn ) {
+		return typeof delay === "number" ?
+			this.each(function() {
 				var elem = this;
 				setTimeout(function() {
-					$(elem).focus();
-					(fn && fn.call(elem));
-				}, delay);
-			})
-			: this._focus.apply(this, arguments);
+					$( elem ).focus();
+					if ( fn ) {
+						fn.call( elem );
+					}
+				}, delay );
+			}) :
+			this._focus.apply( this, arguments );
 	},
-	
+
 	enableSelection: function() {
 		return this
-			.attr('unselectable', 'off')
-			.css('MozUserSelect', '');
+			.attr( "unselectable", "off" )
+			.css( "MozUserSelect", "" );
 	},
 
 	disableSelection: function() {
 		return this
-			.attr('unselectable', 'on')
-			.css('MozUserSelect', 'none');
+			.attr( "unselectable", "on" )
+			.css( "MozUserSelect", "none" );
 	},
 
 	scrollParent: function() {
 		var scrollParent;
-		if(($.browser.msie && (/(static|relative)/).test(this.css('position'))) || (/absolute/).test(this.css('position'))) {
+		if (($.browser.msie && (/(static|relative)/).test(this.css('position'))) || (/absolute/).test(this.css('position'))) {
 			scrollParent = this.parents().filter(function() {
 				return (/(relative|absolute|fixed)/).test($.curCSS(this,'position',1)) && (/(auto|scroll)/).test($.curCSS(this,'overflow',1)+$.curCSS(this,'overflow-y',1)+$.curCSS(this,'overflow-x',1));
 			}).eq(0);
@@ -155,26 +163,25 @@ $.fn.extend({
 		return (/fixed/).test(this.css('position')) || !scrollParent.length ? $(document) : scrollParent;
 	},
 
-	zIndex: function(zIndex) {
-		if (zIndex !== undefined) {
-			return this.css('zIndex', zIndex);
+	zIndex: function( zIndex ) {
+		if ( zIndex !== undefined ) {
+			return this.css( "zIndex", zIndex );
 		}
-		
-		if (this.length) {
-			var elem = $(this[0]), position, value;
-			while (elem.length && elem[0] !== document) {
+
+		if ( this.length ) {
+			var elem = $( this[ 0 ] ), position, value;
+			while ( elem.length && elem[ 0 ] !== document ) {
 				// Ignore z-index if position is set to a value where z-index is ignored by the browser
 				// This makes behavior of this function consistent across browsers
 				// WebKit always returns auto if the element is positioned
-				position = elem.css('position');
-				if (position == 'absolute' || position == 'relative' || position == 'fixed')
-				{
+				position = elem.css( "position" );
+				if ( position === "absolute" || position === "relative" || position === "fixed" ) {
 					// IE returns 0 when zIndex is not specified
 					// other browsers return a string
 					// we ignore the case of nested elements with an explicit value of 0
 					// <div style="z-index: -10;"><div style="z-index: 0;"></div></div>
-					value = parseInt(elem.css('zIndex'));
-					if (!isNaN(value) && value != 0) {
+					value = parseInt( elem.css( "zIndex" ) );
+					if ( !isNaN( value ) && value != 0 ) {
 						return value;
 					}
 				}
@@ -198,12 +205,12 @@ $.each( [ "Width", "Height" ], function( i, name ) {
 
 	function reduce( elem, size, border, margin ) {
 		$.each( side, function() {
-			size -= parseFloat( $.curCSS( elem, "padding" + this, true)) || 0;
+			size -= parseFloat( $.curCSS( elem, "padding" + this, true) ) || 0;
 			if ( border ) {
-				size -= parseFloat( $.curCSS( elem, "border" + this + "Width", true)) || 0;
+				size -= parseFloat( $.curCSS( elem, "border" + this + "Width", true) ) || 0;
 			}
 			if ( margin ) {
-				size -= parseFloat( $.curCSS( elem, "margin" + this, true)) || 0;
+				size -= parseFloat( $.curCSS( elem, "margin" + this, true) ) || 0;
 			}
 		});
 		return size;
@@ -232,20 +239,20 @@ $.each( [ "Width", "Height" ], function( i, name ) {
 
 //Additional selectors
 function visible( element ) {
-	return !$(element).parents().andSelf().filter(function() {
+	return !$( element ).parents().andSelf().filter(function() {
 		return $.curCSS( this, "visibility" ) === "hidden" ||
 			$.expr.filters.hidden( this );
 	}).length;
 }
 
-$.extend($.expr[':'], {
-	data: function(elem, i, match) {
-		return !!$.data(elem, match[3]);
+$.extend( $.expr[ ":" ], {
+	data: function( elem, i, match ) {
+		return !!$.data( elem, match[ 3 ] );
 	},
 
-	focusable: function(element) {
+	focusable: function( element ) {
 		var nodeName = element.nodeName.toLowerCase(),
-			tabIndex = $.attr(element, 'tabindex');
+			tabIndex = $.attr( element, "tabindex" );
 		if ( "area" === nodeName ) {
 			var map = element.parentNode,
 				mapName = map.name,
@@ -256,19 +263,19 @@ $.extend($.expr[':'], {
 			img = $( "img[usemap=#" + mapName + "]" )[0];
 			return !!img && visible( img );
 		}
-		return (/input|select|textarea|button|object/.test(nodeName)
+		return ( /input|select|textarea|button|object/.test( nodeName )
 			? !element.disabled
-			: 'a' == nodeName
-				? element.href || !isNaN(tabIndex)
-				: !isNaN(tabIndex))
+			: "a" == nodeName
+				? element.href || !isNaN( tabIndex )
+				: !isNaN( tabIndex ))
 			// the element and all of its ancestors must be visible
 			&& visible( element );
 	},
 
-	tabbable: function(element) {
-		var tabIndex = $.attr(element, 'tabindex');
-		return (isNaN(tabIndex) || tabIndex >= 0) && $(element).is(':focusable');
+	tabbable: function( element ) {
+		var tabIndex = $.attr( element, "tabindex" );
+		return ( isNaN( tabIndex ) || tabIndex >= 0 ) && $( element ).is( ":focusable" );
 	}
 });
 
-})(jQuery);
+})( jQuery );
