dea2f8a7fc1d7c1d8fbf69aa50d7c1f28c3f35b6
jzaefferer <joern.zaefferer@gmail.com>
2010-08-31 14:20:56 +0200

    Autocomplete: Store term when selecting an item to prevent another search triggered before blur. Fixes #5591 - Home and End keys trigger search

--- ui/jquery.ui.autocomplete.js
+++ ui/jquery.ui.autocomplete.js
@@ -163,6 +163,7 @@ $.widget( "ui.autocomplete", {
 					}
 
 					if ( false !== self._trigger( "select", event, { item: item } ) ) {
+						self.term = item.value;
 						self.element.val( item.value );
 					}
 
