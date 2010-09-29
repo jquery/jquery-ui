b582c963319ce960086417a6a77e5982a0871287
Felix Nagel <info@felixnagel.com>
2010-09-30 01:41:50 +0200

    added: disable and ajax demo

---
+++ demos/selectmenu/ajax/get_data.php
@@ -0,0 +1,13 @@
+<?php
+// echo $_GET['get_data'];
+?>
+
+<label for="speedB">Select an Address:</label>
+<select name="speedB" id="speedB">
+  <option value="1">John Doe</option>
+  <option value="3">Joseph Doe</option>
+  <option value="4">Mad Doe Kiiid</option>
+</select>
+          
+
+        
