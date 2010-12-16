3d81302e70125e2a0303c5409195c8895899b192
Max Schnur <max.schnur@gmail.com>
2010-12-15 22:10:18 -0500

    Tabs: Modified anchor bindings to use event.preventDefault() instead of return false where appropriate

--- ui/jquery.ui.tabs.js
+++ ui/jquery.ui.tabs.js
@@ -333,7 +333,8 @@ $.widget( "ui.tabs", {
 			};
 
 		// attach tab event handler, unbind to avoid duplicates from former tabifying...
-		this.anchors.bind( o.event + ".tabs", function() {
+		this.anchors.bind( o.event + ".tabs", function(event) {
+      event.preventDefault();
 			var el = this,
 				$li = $(el).closest( "li" ),
 				$hide = self.panels.filter( ":not(.ui-tabs-hide)" ),
@@ -349,7 +350,7 @@ $.widget( "ui.tabs", {
 				self.panels.filter( ":animated" ).length ||
 				self._trigger( "select", null, self._ui( this, $show[ 0 ] ) ) === false ) {
 				this.blur();
-				return false;
+        return
 			}
 
 			o.selected = self.anchors.index( this );
@@ -370,7 +371,7 @@ $.widget( "ui.tabs", {
 					}).dequeue( "tabs" );
 
 					this.blur();
-					return false;
+					return;
 				} else if ( !$hide.length ) {
 					if ( o.cookie ) {
 						self._cookie( o.selected, o.cookie );
@@ -384,7 +385,7 @@ $.widget( "ui.tabs", {
 					self.load( self.anchors.index( this ) );
 
 					this.blur();
-					return false;
+					return;
 				}
 			}
 
@@ -418,8 +419,8 @@ $.widget( "ui.tabs", {
 		});
 
 		// disable click in any case
-		this.anchors.bind( "click.tabs", function(){
-			return false;
+		this.anchors.bind( "click.tabs", function(event){
+			event.preventDefault();
 		});
 	},
 
