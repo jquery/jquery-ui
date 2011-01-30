cfd47b95a9abf8ca655a1a098712c995e77e8483
jzaefferer <joern.zaefferer@gmail.com>
2011-01-30 17:51:13 +0100

    Added basic editing test.

--- tests/visual/grid/datasource.js
+++ tests/visual/grid/datasource.js
@@ -24,15 +24,21 @@
 			if ($.isArray( this.options.source ) ) {
 				// TODO pass this (as the datasource instance) instead of type?
 				store._populate( this.options.type, this.options.source );
-			} else if ( $.isFunction( this.options.source ) ) {
+			} else if ( $.type( this.options.source ) == "string" ) {
 				var that = this;
-				this.options.source( {}, function(data) {
+				$.getJSON( this.options.source, {}, function(data) {
 					store._populate( that.options.type, data );
 					// TODO replace this workaround with proper notifications or async handling 
 					$("table").grid("refresh");
 				});
+			} else if ( $.isFunction( this.options.source ) ) {
+				var that = this;
+				this.options.source( {}, function(data) {
+					store._populate( that.options.type, data );
+					// TODO see above 
+					$("table").grid("refresh");
+				});
 			}
-			// TODO typeof source == "string" => $.getJSON(source)?
 		},
 		save: function( items ) {
 			$.each( items, function( itemId, item ) {
