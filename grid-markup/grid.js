84b65f64b2f841d97077b3c0df9e2b214caffcfc
Richard Worth <rdworth@gmail.com>
2011-02-04 20:49:10 -0500

    Moved resize refresh code to grid refresh. Adjusted some borders, worked on some column alignment (still not perfect, depends on head and body borders matchin), fixed alignment of resize handles.

--- grid-markup/grid.js
+++ grid-markup/grid.js
@@ -7,8 +7,9 @@ $.widget( "ui.grid", {
 	options: {},
 	_create: function() {
 
-		var totalWidth = 0,
-			colWidths = this.element.find("td:eq(0)").parent("tr").children().map(function() {
+		var _this = this,
+			totalWidth = 0,
+			colWidths = this.element.find( "td:eq(0)" ).parent( "tr" ).children().map(function() {
 				var width = $(this).outerWidth();
 				totalWidth += width
 				return width;
@@ -36,45 +37,49 @@ $.widget( "ui.grid", {
 				.appendTo( uiGridBody );
 
 		// Move table CAPTION to grid head
-		uiGridBodyTable.find("caption")
+		uiGridBodyTable.find( "caption" )
 			.prependTo( uiGridHeadTable );
 
 		// Create COLGROUP and COLs if missing
 		if ( !uiGridBodyTable.find("colgroup").length ) {
 			// TODO: Consider adding support for existing COL elements not inside COLGROUP
 			// TODO: ... in the meantime, remove any that might exist
-			uiGridBodyTable.find("col").remove();
-			var colgroup = $("<colgroup></colgroup>").insertBefore( uiGridBodyTable.find("thead") );
-			uiGridBodyTable.find("tr:eq(0)").children().each(function(i) {
-				colgroup.append("<col>");
+			uiGridBodyTable.find( "col" ).remove();
+			var colgroup = $( "<colgroup></colgroup>" ).insertBefore( uiGridBodyTable.find("thead") );
+			uiGridBodyTable.find( "tr:eq(0)" ).children().each(function(i) {
+				colgroup.append( "<col>" );
 			});
 		}
 
 		// Auto-size columns based on relative widths of pre-grid table column widths
-		uiGridBody.find("colgroup").children().each(function(i) {
-			$(this).css("width", ( colWidths[i] / totalWidth * 100 ) + '%' );
+		uiGridBody.find( "colgroup" ).children().each(function(i) {
+			$(this).css( "width", ( colWidths[i] / totalWidth * 100 ) + '%' );
 		});
 
 		// Copy table COLGROUP to grid head
-		uiGridBodyTable.find("colgroup")
+		uiGridBodyTable.find( "colgroup" )
 			.clone()
 			.appendTo( uiGridHeadTable );
 
 		// Move table THEAD to grid head for fixed column headers
-		uiGridBodyTable.find("thead")
+		uiGridBodyTable.find( "thead" )
 			.appendTo( uiGridHeadTable );
 
 		// Give head rows a clickable state
-		uiGridHeadTable.find("tr").addClass("ui-state-default");
+		uiGridHeadTable.find( "tr" ).addClass( "ui-state-default" );
 
 		// Give head cells a clickable state
 		this._hoverable( uiGridHeadTable.find("th").addClass("ui-state-default") );
 
 		// Give body rows a clickable state
-		uiGridBodyTable.find("tr").addClass("ui-state-default");
+		uiGridBodyTable.find( "tr" ).addClass( "ui-state-default" );
 
 		// Give body cells a clickable state
-		uiGridBodyTable.find("td").addClass("ui-state-default");
+		uiGridBodyTable.find( "td" ).addClass( "ui-state-default" );
+
+		uiGrid.bind( "resize", function() {
+			_this.refresh();
+		});
 
 		this.refresh();
 
@@ -88,6 +93,15 @@ $.widget( "ui.grid", {
 		var gridHeight = this.uiGrid.height(),
 			headHeight = this.uiGridHead.height(),
 			footHeight = this.uiGridFoot.height();
+
+		if ( this.uiGrid.is(":ui-resizable") ) {
+			this.uiGrid.addClass( "ui-grid-resizable" );
+			if ( footHeight < 16) {
+				footHeight = this.uiGridFoot.height( 16 );
+			}
+		} else {
+			this.uiGrid.removeClass( "ui-grid-resizable" );
+		}
 
 		// Adjust body height to fill
 		this.uiGridBody.height( gridHeight - headHeight - footHeight );
