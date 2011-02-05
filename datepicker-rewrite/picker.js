cad5cd85d1e838136ebe34a8b75da78c14df1414
jzaefferer <joern.zaefferer@gmail.com>
2011-02-05 14:28:04 +0100

    Formatting and cleanup

--- datepicker-rewrite/picker.js
+++ datepicker-rewrite/picker.js
@@ -1,18 +1,7 @@
 /*
- * jQuery UI Datepicker Lite
- *
- * Copyright 2010 Marc Grabanski
- * Licensed under the MIT license
- *
- *
- * Depends:
- *	jquery.ui.core.js
- *	jquery.ui.widget.js
- *	jquery.ui.position.js
- *	jquery.ui.button.js
- *	jquery.date.js
- *	jquery.tmpl.js
- *  jquery.ui.datepicker.html
+ * Experimental datepicker rewrite to evaluate jquery-tmpl.
+ * 
+ * Based on Marc Grabanski's https://github.com/1Marc/jquery-ui-datepicker-lite
  */
 (function( $, undefined ) {
 
@@ -22,35 +11,36 @@ $.widget( "ui.datepicker", {
 	_create: function() {
 		var self = this;
 		this.date = $.date();
-		if (this.element.is("input")) {
+		if ( this.element.is( "input" ) ) {
 			self._bind( {
 				click: "open",
 				// TODO click on picker should not close
 				blur: "close"
 			});
-			this.picker = $("<div/>").insertAfter(this.element).hide();
-			this.picker.css({
+			this.picker = $( "<div/>" ).insertAfter( this.element ).hide();
+			this.picker.css( {
 				position: "absolute"
 			});
 		} else {
 			this.inline = true;
 			this.picker = this.element;
 		}
-		this.picker.delegate(".ui-datepicker-prev", "click", function() {
-			self.date.adjust("M", -1);
+		this.picker.delegate( ".ui-datepicker-prev", "click", function() {
+			self.date.adjust( "M", -1 );
 			self.refresh();
 		});
-		this.picker.delegate(".ui-datepicker-next", "click", function() {
-			self.date.adjust("M", +1)
+		this.picker.delegate( ".ui-datepicker-next", "click", function() {
+			self.date.adjust( "M", +1 )
 			self.refresh();
 		});
-		this.picker.delegate(".ui-datepicker-calendar a", "click", function( event ) {
-			self.date.setDay( +$(this).text() );
-			if (!self.inline) {
+		this.picker.delegate( ".ui-datepicker-calendar a", "click", function( event ) {
+			// TODO exclude clicks on lead days or handle them correctly
+			self.date.setDay( +$( this ).text() );
+			if ( !self.inline ) {
 				self.element.val( self.date.format() );
 				self.close();
 			}
-			self._trigger("select", event, {
+			self._trigger( "select", event, {
 				date: self.date.format(),
 			});
 		});
@@ -61,37 +51,29 @@ $.widget( "ui.datepicker", {
 		this.date.refresh();
 		this.picker.empty();
 
-		$("#ui-datepicker-tmpl").tmpl({
+		$( "#ui-datepicker-tmpl" ).tmpl({
 			date: this.date
-		}).appendTo(this.picker)
-			.find("button").button().end()
+		}).appendTo( this.picker )
+			.find( "button" ).button().end()
 		
 		// against display:none in datepicker.css
-		this.picker.find(".ui-datepicker").css("display", "block");
+		this.picker.find( ".ui-datepicker" ).css( "display", "block" );
 		this._hoverable( this.picker.find( ".ui-datepicker-header a" ) );
 		this._hoverable( this.picker.find( ".ui-datepicker-header a, .ui-datepicker-calendar a" ) );
 	},
-	_setOption: function( key, value ) {
-		$.Widget.prototype._setOption.apply( this, arguments );
-		if ( key === "" ) {
-		}
-	},
 	open: function( event ) {
 		this.picker.fadeIn( "fast" );
-		// would open ever get called for non-inline datepickers?
-		if (!this.inline) {
-			this.picker.position({
-				my: "left top",
-				at: "left bottom",
-				of: this.element
-			});
-		}
+		this.picker.position({
+			my: "left top",
+			at: "left bottom",
+			of: this.element
+		});
 	},
 	close: function( event ) {
 		this.picker.fadeOut();
 	},
-	destroy: function() {
-		if (!this.inline) {
+	_destroy: function() {
+		if ( !this.inline ) {
 			this.picker.remove();
 		}
 	},
