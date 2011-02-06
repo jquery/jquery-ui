42068d5b1e6ea89f2f10ae5dcb1fd1e2b4b9cc6a
William Kevin Manire <williamkmanire@gmail.com>
2011-02-05 22:21:04 -0800

    Widget: Removed accidental paste.

--- tests/unit/widget/widget_tickets.js
+++ tests/unit/widget/widget_tickets.js
@@ -88,44 +88,6 @@ test( "#6795 - Widget: handle array arguments to _trigger consistently", functio
 	.testWidget( "testEvent" );
 });
 
-test( "#5830 - Widget: Using inheritance overwrites the base classes options", function() {
-	$.widget( "ui.testWidgetBase", {
-		options: {
-			obj: {
-				key1: "foo",
-				key2: "bar"
-			},
-			arr: [ "testing" ]
-		}
-	});
-
-	$.widget( "ui.testWidgetExtension", $.ui.testWidgetBase, {
-		options: {
-			obj: {
-				key1: "baz"
-			},
-			arr: [ "alpha", "beta" ]
-		}
-	});
-
-	same( $.ui.testWidgetBase.prototype.options.obj, {
-		key1: "foo",
-		key2: "bar"
-	}, "base class option object not overridden");
-	same( $.ui.testWidgetBase.prototype.options.arr, [ "testing" ],
-		"base class option array not overridden");
-
-	same( $.ui.testWidgetExtension.prototype.options.obj, {
-		key1: "baz",
-		key2: "bar"
-	}, "extension class option object extends base");
-	same( $.ui.testWidgetExtension.prototype.options.arr, [ "alpha", "beta" ],
-		"extension class option array overwrites base");
-
-	delete $.ui.testWidgetBase;
-	delete $.ui.testWidgetExtension;
-});
-
 test( "#6947 - Widget: Attempt to access private member of widget returns jQuery object", function() {
 	$.widget( "ui.testWidget", {
 		_create: function() {},
