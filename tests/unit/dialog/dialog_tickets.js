772cb752cb5216777fe0d6e812984bb9b62a5281
Jay Merrifield <fracmak@gmail.com>
2011-03-01 12:08:17 -0500

    Added an extra series of tests to confirm that the dialog behavior still works as intended when noclose handler is added

--- tests/unit/dialog/dialog_tickets.js
+++ tests/unit/dialog/dialog_tickets.js
@@ -89,7 +89,8 @@ test("#6645: Missing element not found check in overlay", function(){
 });
 
 test("#6966: Escape key closes all dialogs, not the top one", function(){
-	expect(4);
+	expect(8);
+    // test with close function removing dialog
     d1 = $('<div title="dialog 1">Dialog 1</div>').dialog({modal: true});
     d2 = $('<div title="dialog 2">Dialog 2</div>').dialog({modal: true, close: function(){ d2.remove()}});
     ok(d1.dialog("isOpen"), 'first dialog is open');
@@ -99,6 +100,19 @@ test("#6966: Escape key closes all dialogs, not the top one", function(){
     d2.parent().trigger(e);
     ok(d1.dialog("isOpen"), 'first dialog still open');
     ok(!d2.data('dialog'), 'second dialog is closed');
+    d2.remove();
+    d1.remove();
+
+    // test without close function removing dialog
+    d1 = $('<div title="dialog 1">Dialog 1</div>').dialog({modal: true});
+    d2 = $('<div title="dialog 2">Dialog 2</div>').dialog({modal: true});
+    ok(d1.dialog("isOpen"), 'first dialog is open');
+    ok(d2.dialog("isOpen"), 'second dialog is open');
+    var e = jQuery.Event("keydown");
+    e.keyCode = $.ui.keyCode.ESCAPE;
+    d2.parent().trigger(e);
+    ok(d1.dialog("isOpen"), 'first dialog still open');
+    ok(!d2.dialog("isOpen"), 'second dialog is closed');
     d2.remove();
     d1.remove();
 });
