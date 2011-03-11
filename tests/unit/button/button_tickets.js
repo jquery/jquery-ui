893c54adf59ff3b13740e4a263fb5ba30f2953aa
ddstreet <ddstreet@ieee.org>
2011-03-11 10:36:48 -0500

    Button: test cases for finding associated label. Test #7092 - button creation that requires a matching label does not find label in all cases

--- tests/unit/button/button_tickets.js
+++ tests/unit/button/button_tickets.js
@@ -20,4 +20,26 @@ test( "#6262 - buttonset not applying ui-corner to invisible elements", function
 	ok( set.find( "label:eq(2)" ).is( ".ui-button.ui-corner-right" ) );
 });
 
+test( "#7092 - button creation that requires a matching label does not find label in all cases", function() {
+	var group = $( "<span><label for='t7092a'/><input type='checkbox' id='t7092a'/></span>" );
+	group.find( "input:checkbox" ).button();
+	ok( group.find( "label" ).is( ".ui-button" ) );
+
+	group = $( "<input type='checkbox' id='t7092b'/><label for='t7092b'/>" );
+	group.filter( "input:checkbox" ).button();
+	ok( group.filter( "label" ).is( ".ui-button" ) );
+
+	group = $( "<span><input type='checkbox' id='t7092c'/></span><label for='t7092c'/>" );
+	group.find( "input:checkbox" ).button();
+	ok( group.filter( "label" ).is( ".ui-button" ) );
+
+	group = $( "<span><input type='checkbox' id='t7092d'/></span><span><label for='t7092d'/></span>" );
+	group.find( "input:checkbox" ).button();
+	ok( group.find( "label" ).is( ".ui-button" ) );
+
+	group = $( "<input type='checkbox' id='t7092e'/><span><label for='t7092e'/></span>" );
+	group.filter( "input:checkbox" ).button();
+	ok( group.find( "label" ).is( ".ui-button" ) );
+});
+
 })( jQuery );
