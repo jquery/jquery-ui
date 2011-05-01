ec5aeb1b154a023e2630d21aa7982ca13a00f03e
gnarf <gnarf@gnarf.net>
2011-05-01 06:23:19 -0500

    Unit Tests: Adding some basic unit tests for effects.* modules

--- tests/unit/effects/effects_core.js
+++ tests/unit/effects/effects_core.js
@@ -1,10 +1,28 @@
 (function($) {
 
-module( "effects - Core Effects" );
+var animateTime = 15;
 
-test( "Empty Unit", function() {
-	expect( 1 );
-	equals( 0, 0, "Unit Tests work" );
+module( "effects.core" );
+
+$.each( $.effects.effect, function( effect ) {
+	if ( effect === "transfer" ) {
+		return;
+	}
+	QUnit.reset();
+	module( "effect."+effect );
+	test( "show/hide", function() {
+		var hidden = $( "div.hidden" );
+		expect( 3 );
+		stop();
+		hidden.show( effect, animateTime, function() {
+			equal( hidden.css("display"), "block", "Hidden is shown after .show(\"" +effect+ "\", time)" );
+		}).hide( effect, animateTime, function() {
+			equal( hidden.css("display"), "none", "Back to hidden after .hide(\"" +effect+ "\", time)" );
+		}).queue( function(next) {
+			deepEqual( hidden.queue(), ["inprogress"], "Only the inprogress sentinel remains")
+			start();
+		});
+	});
 });
 
 })(jQuery);
