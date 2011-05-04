4dcfeee8d54d81db4d5af8b2fd189b799cd9561e
gnarf <gnarf@gnarf.net>
2011-05-04 01:54:47 -0500

    Added some better queue order testing

--- tests/unit/effects/effects_core.js
+++ tests/unit/effects/effects_core.js
@@ -12,16 +12,34 @@ $.each( $.effects.effect, function( effect ) {
 	module( "effect."+effect );
 	test( "show/hide", function() {
 		var hidden = $( "div.hidden" );
-		expect( 3 );
+		expect( 8 );
 		stop();
-		hidden.show( effect, animateTime, function() {
+
+		var count = 0,
+			test = 0;
+
+		function queueTest( fn ) {
+			count++;
+			var point = count;
+			return function( next ) {
+				test++;
+				equals( point, test, "Queue function fired in order" );
+				if ( fn ) {
+					fn ()
+				} else {
+					setTimeout( next, animateTime );
+				}
+			}
+		}
+		
+		hidden.queue( queueTest() ).show( effect, animateTime, queueTest(function() {
 			equal( hidden.css("display"), "block", "Hidden is shown after .show(\"" +effect+ "\", time)" );
-		}).hide( effect, animateTime, function() {
+		})).queue( queueTest() ).hide( effect, animateTime, queueTest(function() {
 			equal( hidden.css("display"), "none", "Back to hidden after .hide(\"" +effect+ "\", time)" );
-		}).queue( function(next) {
+		})).queue( queueTest(function(next) {
 			deepEqual( hidden.queue(), ["inprogress"], "Only the inprogress sentinel remains")
 			start();
-		});
+		}));
 	});
 });
 
