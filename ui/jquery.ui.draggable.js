dddc256429b03b77258e6d8db68f85de9710f2cc
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2011-05-12 01:10:44 +0900

    Before add same document, we should remove "id" attribute from clone. Fixes #4564 - Draggable+Sortable Demo duplicates DOM Ids.

--- ui/jquery.ui.draggable.js
+++ ui/jquery.ui.draggable.js
@@ -263,8 +263,9 @@ $.widget("ui.draggable", $.ui.mouse, {
 		var o = this.options;
 		var helper = $.isFunction(o.helper) ? $(o.helper.apply(this.element[0], [event])) : (o.helper == 'clone' ? this.element.clone() : this.element);
 
-		if(!helper.parents('body').length)
-			helper.appendTo((o.appendTo == 'parent' ? this.element[0].parentNode : o.appendTo));
+		if ( !helper.parents('body').length ) {
+			helper.removeAttr('id').appendTo((o.appendTo == 'parent' ? this.element[0].parentNode : o.appendTo));
+		}
 
 		if(helper[0] != this.element[0] && !(/(fixed|absolute)/).test(helper.css("position")))
 			helper.css("position", "absolute");
@@ -580,7 +581,7 @@ $.ui.plugin.add("draggable", "connectToSortable", {
 					//Now we fake the start of dragging for the sortable instance,
 					//by cloning the list group item, appending it to the sortable and using it as inst.currentItem
 					//We can then fire the start event of the sortable with our passed browser event, and our own helper (so it doesn't create a new one)
-					this.instance.currentItem = $(self).clone().appendTo(this.instance.element).data("sortable-item", true);
+					this.instance.currentItem = $(self).clone().removeAttr('id').appendTo(this.instance.element).data("sortable-item", true);
 					this.instance.options._helper = this.instance.options.helper; //Store helper option to later restore it
 					this.instance.options.helper = function() { return ui.helper[0]; };
 
