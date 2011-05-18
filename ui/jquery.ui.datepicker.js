92b0f6702a9408f4bd7d71ccca7e0e851d0efc6b
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-05-18 17:30:26 +0200

    There's no guard against unparsed characters at the end of the date string, any extra characters are just ignored
    Fixes #7244 - Datepicker: parseDate() does not throw an exception for long years

--- ui/jquery.ui.datepicker.js
+++ ui/jquery.ui.datepicker.js
@@ -1082,6 +1082,9 @@ $.extend(Datepicker.prototype, {
 						checkLiteral();
 				}
 		}
+		if (iValue < value.length){
+			throw "Extra/unparsed characters found in date: " + value.substring(iValue);
+		}
 		if (year == -1)
 			year = new Date().getFullYear();
 		else if (year < 100)
