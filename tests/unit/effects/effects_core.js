2dd03e338441749d569c6b9678a27f272884532d
tomykaira <tomykaira@gmail.com>
2011-05-23 10:14:39 +0900

    whitespace

--- tests/unit/effects/effects_core.js
+++ tests/unit/effects/effects_core.js
@@ -55,14 +55,14 @@ $.each( $.effects.effect, function( effect ) {
 		}));
 	});
 
-  test( "toggle with % width", function() {
-	  var test = $("div.relWidth.relHeight"),
-		   width = test.width(), height = test.height();
+	test( "toggle with % width", function() {
+		var test = $("div.relWidth.relHeight"),
+			 width = test.width(), height = test.height();
 
-	  test.toggle( effect, minDuration );
-	  equal( test.width(), width, "Width is the same after animation started" );
-    equal( test.height(), height, "Height is the same after animation started" );
-  });
+		test.toggle( effect, minDuration );
+		equal( test.width(), width, "Width is the same after animation started" );
+		equal( test.height(), height, "Height is the same after animation started" );
+	});
 });
 
 module("animateClass");
