4e32a779d96133a94bae47ce493a73f6387a57ec
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-05-31 17:30:45 +0200

    Grid Editing: Update stored and master-detail, only refactorings

--- grid-editing/localstore.js
+++ grid-editing/localstore.js
@@ -23,16 +23,20 @@ $.widget( "demos.localstore", {
 		if (this.options.key === null ) {
 			throw "need to configure a key";
 		}
-		if ( this._fresh() && $.type( this.options.initial ) === "string") {
-			var that = this;
-			$.ajax({
-				dataType: "json",
-				url: this.options.initial,
-				async: false,
-				success: function( result ) {
-					that.save( result );
-				}
-			});
+		if ( this._fresh() ) {
+			if ($.type( this.options.initial ) === "string") {
+				var that = this;
+				$.ajax({
+					dataType: "json",
+					url: this.options.initial,
+					async: false,
+					success: function( result ) {
+						that.save( result );
+					}
+				});
+			} else {
+				this.save( this.options.initial );
+			}
 		}
 	},
 	_fresh: function() {
@@ -40,16 +44,21 @@ $.widget( "demos.localstore", {
 		return !stored || stored === this.options.initial;
 	},
 	load: function() {
-		var stored = localStorageSupport
-			? JSON.parse( localStorage.getItem( this.options.key ) )
-			: this.data;
+		var stored = [];
+		if ( localStorageSupport ) {
+			stored = localStorage.getItem( this.options.key );
+			if ( stored ) {
+				stored = jQuery.parseJSON( stored );
+			}
+		} else {
+			stored = this.data;
+		}
 		if (!stored) {
 			return this.options.intial;
 		}
 		return stored;
 	},
 	save: function( data ) {
-		console.log("save", data)
 		localStorageSupport
 			? localStorage.setItem( this.options.key, JSON.stringify( data ) )
 			: this.data = data;
