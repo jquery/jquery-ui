81b665c91719a3bdec6f1f8d8f96dc4aea02d668
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-06-22 00:27:47 +0200

    Grid Editing: Lazy-apply inline editor via gridEditor wrapper widget

---
+++ grid-editing/grid-editor.js
@@ -0,0 +1,34 @@
+/*
+ * Inline Editor
+ *
+ * Depends on:
+ * widget
+ *
+ * Optional:
+ * spinner
+ */
+(function( $ ) {
+
+$.widget( "ui.gridEditor", {
+	_create: function() {
+		this._bind({
+			click: function( event ) {
+				var that = this;
+				var target = $( event.target ).closest( this.options.items );
+				if ( target.length && !target.data( "editor" ) ) {
+					target.editor({
+						type: this.options.type( target ),
+						submit: function( event, ui ) {
+							that._trigger( "submit", event, $.extend( {
+								item: target
+							}, ui ));
+						}
+					}).editor("start");
+				}
+			}
+		});
+	}
+});
+
+
+})( jQuery );
