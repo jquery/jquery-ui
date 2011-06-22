2e7b48813057d1a364b616f485cae9baa957b538
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-06-22 14:39:39 +0200

    Grid SPF: Extract nested datasource and fix a few bugs, like filter not getting forwarded properly

---
+++ grid-spf/datasource-nested.js
@@ -0,0 +1,65 @@
+(function ($, undefined ) {
+
+// TODO cache should be per-instance?
+var cache = {};
+$.widget( "ui.nestedDatasource", $.ui.datasource, {
+	widgetEventPrefix: "datasource",
+	options: {
+		remote: null
+	},
+
+	_create: function() {
+		var options = this.options;
+		options.source = function( request, response ) {
+			var i, length,
+				cached = true,
+				tags = request.filter,
+				pageSize = request.paging.limit,
+				page = request.page,
+				start = ( page - 1 ) * pageSize;
+
+			if ( !cache[ tags ] ) {
+				cache[ tags ] = [];
+			}
+
+			// check if we have cached data
+			for ( i = start; i < start + pageSize; i++ ) {
+				if ( !cache[ tags ][ i ] ) {
+					cached = false;
+					break;
+				}
+			}
+			if ( cached ) {
+				response( cache[ tags ].slice( start, start + pageSize ), cache[ tags ].total );
+				return;
+			}
+
+			options.remote.refresh(function() {
+				var total = cache[ tags ].total = options.remote.totalCount;
+				var data = options.remote.toArray();
+				for ( i = 0, length = data.length; i < length; i++ ) {
+					cache[ tags ][ start + i ] = data[ i ];
+				}
+				response( data.slice( 0, pageSize ), total );
+			});
+		};
+		this._super( "_create" );
+	},
+
+	_setOptions: function( options ) {
+		if ( "filter" in options ) {
+			this.options.remote.option( "filter", options.filter );
+		}
+		if ( "paging" in options ) {
+			this.options.remote.option( "paging", {
+				limit: ( "limit" in options.paging ?
+					options.paging.limit : this.options.paging.limit ) * 5,
+				offset: ( "offset" in options.paging ?
+					options.paging.offset : this.options.paging.offset )
+			});
+		}
+		this._super( "_setOptions", options );
+	}
+});
+
+})(jQuery);
