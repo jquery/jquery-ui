d40039507efb910349cc70e37b4bfcc98f71fb46
Corey Frang <gnarf@gnarf.net>
2011-07-31 05:39:41 -0500

    Mask: Adding a unit test for refresh method

--- tests/unit/mask/mask_methods.js
+++ tests/unit/mask/mask_methods.js
@@ -2,4 +2,15 @@
 
 module( "mask: methods" );
 
+test( "refresh", function() {
+	expect( 1 );
+
+	var input = $( "#mask1" ).mask({
+		mask: "99/99/99"
+	});
+
+	input.val( "123456" ).mask( "refresh" );
+	equal( input.val(), "12/34/56", "Refresh re-parsed the value of the input" );
+});
+
 }( jQuery ) );
