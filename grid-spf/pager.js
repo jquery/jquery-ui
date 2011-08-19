5f0e108c82b4db0b42f1b4ee1dec6cbfd6c7d68d
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-08-19 12:34:53 +0200

    Pager: Remove unused pageSize option; whitespace

--- grid-spf/pager.js
+++ grid-spf/pager.js
@@ -1,7 +1,6 @@
 $.widget( "spf.pager", {
 	options: {
-		source: null,
-		pageSize: 2
+		source: null
 	},
 	_create: function() {
 		var that = this;
@@ -10,7 +9,7 @@ $.widget( "spf.pager", {
 		$( this.options.source ).bind( "datasourceresponse", function() {
 			that.refresh();
 		});
-		
+
 		this.buttons = this.element.delegate("button", "click", function() {
 			var method = $(this).data("page");
 			that[method]();
@@ -22,9 +21,9 @@ $.widget( "spf.pager", {
 			that.options.source.refresh();
 		});
 	},
-	refresh: function() {	
+	refresh: function() {
 		this.buttons.button("enable");
-		
+
 		var source = this.options.source;
 		if (!source.options.paging.offset) {
 			this.buttons.slice(0, 3).button("disable")
@@ -36,35 +35,35 @@ $.widget( "spf.pager", {
 		this.element.find(".total").text(this.totalPages());
 		this.element.find(".totalRecords").text(source.totalCount);
 	},
-	
+
 	totalPages: function() {
 		return this.options.source.totalPages();
 	},
-	
+
 	page: function(pageIndex) {
 		return this.options.source.page(pageIndex);
 	},
-	
+
 	first: function() {
 		this.page(1);
 	},
-	
+
 	prevStep: function() {
 		this.options.source.options.paging.offset -= 1;
 	},
-	
+
 	prev: function() {
 		this.page( this.page() - 1 )
 	},
-	
+
 	next: function() {
 		this.page( this.page() + 1 )
 	},
-	
+
 	nextStep: function() {
 		this.options.source.options.paging.offset += 1;
 	},
-	
+
 	last: function() {
 		this.page( this.totalPages() );
 	}
