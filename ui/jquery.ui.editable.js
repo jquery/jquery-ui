1792c6a21ff2ccdb1c953fcf5bf6d24551b794df
Rafael Xavier de Souza <rxaviers@gmail.com>
2011-09-13 09:42:46 -0300

    Editable: added options for 1) editor type, 2) button position and type; editors and buttons are extensible by augmenting $.ui.editable.editors, $.ui.editable.saveButtons, and $.ui.editable.cancelButtons respectivelly; simplified event bindings; included a new demo.

--- ui/jquery.ui.editable.js
+++ ui/jquery.ui.editable.js
@@ -10,181 +10,258 @@
  * Depends:
  *	jquery.ui.core.js
  *	jquery.ui.widget.js
+ *	jquery.ui.button.js
  */
 (function( $, undefined ) {
 
-var uiEditableClasses = 'ui-editable-content ui-widget ui-widget-content ui-corner-all',
-    buttonClass = 'ui-editable-button',
-    cancelClass = 'ui-editable-cancel',
-    inputClass = 'ui-editable-input',
-	placeholderClass = 'ui-editable-placeholder',
-	saveClass = 'ui-editable-save',
-    cancelIconClass = 'ui-icon ui-icon-cancel',
-    saveIconClass = 'ui-icon ui-icon-disk',
-    defaultStateClass = 'ui-state-default',
-	highlightStateClass = 'ui-state-highlight',
-    hoverStateClass = 'ui-state-hover';
+var editableClass = "ui-editable",
+	formClass = "ui-editable-form",
+	buttonClass = "ui-editable-button",
+	cancelClass = "ui-editable-cancel",
+	inputClass = "ui-editable-input",
+	placeholderClass = "ui-editable-placeholder",
+	saveClass = "ui-editable-save",
+	hoverableClass = "ui-widget-content ui-state-default ui-corner-all",
+
+	cancelIconClass = "ui-icon-cancel",
+	saveIconClass = "ui-icon-disk",
+
+	activeStateClass = "ui-state-active",
+	highlightStateClass = "ui-state-highlight"
 
 $.widget( "ui.editable", {
-    version: "@VERSION",
+	version: "@VERSION",
 	widgetEventPrefix: "edit",
 
 	options: {
-	    value: '',
-	    saveButton: 'Save',
-		cancelButton: 'Cancel',
-		placeholder: 'Click to edit'
+		value: "",
+		editor: "text",
+		buttons: "outside",
+		save: {
+			type: "button",
+			text: "Save"
+		},
+		cancel: {
+			type: "button",
+			text: "Cancel"
+		},
+		placeholder: "Click to edit"
 	},
 
-	_init: function() {
-        if (!this.value($.trim(this.element.text()) || this.options.value)) {
-            this._show();
+	start: function() {
+		this._edit();
+	},
+
+	submit: function() {
+		$( "form", this.element ).submit();
+	},
+
+	cancel: function() {
+		this._cancel();
+	},
+
+	_create: function() {
+		if ( !this.value( $.trim( this.element.text() ) || this.options.value ) ) {
+			this._show();
 		}
-		this._delegate();
+		this._bind( this._events );
+		this.element.addClass( editableClass );
 	},
 
-	_delegate: function() {
-        var self = this;
+	_events: {
+		click: function( event ) {
+			var $this = $( event.target );
+			
+			if ( $this.hasClass( saveClass ) || $this.parent().hasClass( saveClass ) ) {
+				this.submit();
+				return;
+			}
 
-		this.element
-			.bind('click', function(event) {
-				var $this = $(event.target);
-	            
-				if ($this.hasClass(saveClass) || $this.parent().hasClass(saveClass)) {
-					self._save(event, self.element.find('.' + inputClass).val());
-					return;
-	            }
+			if ( $this.hasClass( cancelClass ) || $this.parent().hasClass( cancelClass ) ) {
+				this._cancel( event );
+				return false;
+			}
 
-				if ($this.hasClass(cancelClass) || $this.parent().hasClass(cancelClass)) {
-					self._cancel(event);
-					return;
-	            }
-								
-				if ($this.hasClass('ui-editable') || $this.hasClass(placeholderClass)) {
-					self._edit();
-					return;
-	            }
-	        })
-			.bind('mouseover', function(event) {
-				var $this = $(event.target);
-				
-				self.element.removeClass(highlightStateClass);
-				self.element.find('.' + buttonClass).removeClass(hoverStateClass);
-				
-				if ($this.hasClass(saveClass) || $this.parent().hasClass(saveClass)) {
-					self.element.find('.' + saveClass).addClass(hoverStateClass);
-					return;
-	            }
-				
-				if ($this.hasClass('ui-editable') || $this.hasClass(placeholderClass)) {
-					self.element.addClass(highlightStateClass);
-					return;
-	            }
-			})
-			.bind('mouseout', function(event) {
-				var $this = $(event.target);
-				
-				if ($this.hasClass(saveClass) || $this.parent().hasClass(saveClass)) {
-					self.element.find('.' + saveClass).removeClass(hoverStateClass);
-					return;
-	            }
-				
-				if ($this.hasClass('ui-editable') || $this.hasClass(placeholderClass)) {
-					self.element.removeClass(highlightStateClass);
-					return;
-	            }
-			})
-			.addClass( 'ui-editable' );
+			if ( $this.is( this.element ) || $this.hasClass( placeholderClass ) ) {
+				this._edit();
+				return;
+			}
+		},
+		mouseover: function( event ) {
+			var $this = $( event.target );
+
+			this.element.removeClass( highlightStateClass );
+
+			if ( $this.is( this.element ) || $this.hasClass( placeholderClass ) ) {
+				this.element.addClass( highlightStateClass );
+				return;
+			}
+		},
+		mouseout: function( event ) {
+			var $this = $( event.target );
+
+			if ( $this.is( this.element ) || $this.hasClass( placeholderClass ) ) {
+				this.element.removeClass( highlightStateClass );
+				return;
+			}
+		}
 	},
 
-    _show: function() {
-        this.element.html( this.value() || this._placeholder() );
-    },
+	_show: function() {
+		this.element.html( this.value() || this._placeholder() );
+	},
 
 	_edit: function() {
-        this.element
-			.html( this._form() );
-		this._formDelegate();
+		this.element.html( this._form() );
+		this._formEvents();
 	},
 
 	_placeholder: function() {
-        return $( "<span></span>" )
-            .addClass( placeholderClass )
-            .html(this.options.placeholder);
-    },
+		return $( "<span></span>" )
+			.addClass( placeholderClass )
+			.html( this.options.placeholder );
+	},
 
 	_form: function() {
-		return $( "<form></form>" )
-            .addClass( uiEditableClasses )
-            .append( $( "<input/>" )
-                .attr( "type", "text" )
-                .attr( "value", this.value() )
-                .addClass( inputClass ))
-            .append( this._saveButton() )
-            .append( this._cancelButton() );
+		var editor = $.ui.editable.editors[ this.options.editor ],
+            form = $( "<form></form>" )
+			.addClass( formClass )
+			.append( $( "<span></span>" )
+				.append( editor.element( this )));
+		if ( this.options.buttons == "inside" ) {
+			this.frame = form;
+		}
+		else {
+			this.frame = $( "> span" , form );
+		}
+		this._hoverable( this.frame.addClass( hoverableClass ) );
+		if( this.options.buttons && this.options.save ) {
+			form.append( this._saveButton() );
+		}
+		if( this.options.buttons && this.options.cancel ) {
+			form.append( this._cancelButton() );
+		}
+		return form;
 	},
 
-    _saveButton: function() {
-        return $( "<a></a>" )
-            .attr( "href", "#" )
-            .attr( "title", this.options.saveButton )
-            .addClass( saveClass + " " + buttonClass + " " + defaultStateClass )
-            .append( $("<span></span>")
-                .addClass( saveIconClass )
-                .html( this.options.saveButton ));
-    },
+	_saveButton: function() {
+		return $.ui.editable.saveButtons[ this.options.save.type ]( this ).addClass( saveClass );
+	},
 
-    _cancelButton: function() {
-        return $( "<a></a>" )
-            .attr( "href", "#" )
-            .attr( "title", this.options.cancelButton )
-            .addClass( cancelClass + " " + buttonClass + " " + defaultStateClass )
-            .append( $("<span></span>")
-                .addClass( cancelIconClass )
-                .html( this.options.cancelButton ));
-    },
+	_cancelButton: function() {
+		return $.ui.editable.cancelButtons[ this.options.cancel.type ]( this ).addClass( cancelClass );
+	},
 
-    _formDelegate: function() {
-		var self = this;
-		this.element.find('form')
-			.submit(function(event) {
-				self._save(event, $('input', this).val());
-				$('input', this).blur();
+	_formEvents: function() {
+		var self = this,
+            editor = $.ui.editable.editors[ self.options.editor ];
+		$( "form", this.element )
+			.submit( function( event ) {
+				self._save.call( self, event, editor.value( self, this ) );
 				return false;
 			});
-        this.element.find('input')
-            .bind('blur', function() {
-                if (self.timer) {
-                    window.clearTimeout(self.timer);
-                }
-                self.timer = window.setTimeout(function() {
-                    self._show.call(self);
-                    self.element.removeClass(highlightStateClass);
-                }, 200);
-            })
-            .focus();
+		editor.bind( this );
 	},
 
-	_save: function(event, newValue) {
-        var hash = {
-            value: newValue
-        };
-        
-        if (this._trigger('save', event, hash) !== false) {
-            this.value(newValue);
-        }
-	},
+	_save: function( event, newValue ) {
+		var hash = {
+			value: newValue
+		};
 
-	_cancel: function(event) {
-		this._trigger('cancel', event);
-	},
-	
-	value: function(newValue) {
-		if (arguments.length) {
-			this.options.value = $(newValue).hasClass(placeholderClass) ? '' : newValue;
+		if ( this._trigger( "submit", event, hash ) !== false && this.value() !== newValue && this._trigger( "change", event, hash ) !== false ) {
+			this.value( newValue );
 		}
-		return this.options.value;
+
+		this._show();
+	},
+
+	_cancel: function( event ) {
+		this._show();
+		this._trigger( "cancel", event );
+	},
+
+	value: function( newValue ) {
+		if ( arguments.length ) {
+			this._value = newValue;
+		}
+		return this._value;
 	}
 });
 
-})(jQuery);
+$.ui.editable.saveButtons = {
+	button: function(editable) {
+		return $( "<button></button>" )
+			.html( editable.options.save.text )
+			.button({
+				icons: {
+					primary: saveIconClass
+				},
+				text: false
+			});
+	},
+	submit: function(editable) {
+		return $( "<input/>" )
+			.attr( "type", "submit" )
+			.val( editable.options.save.text )
+			.button();
+	}
+};
+
+$.ui.editable.cancelButtons = {
+	button: function(editable) {
+		return $( "<button></button>" )
+			.html( editable.options.cancel.text )
+			.button({
+				icons: {
+					primary: cancelIconClass
+				},
+				text: false
+			});
+	},
+	link: function(editable) {
+		return $( "<a></a>" )
+			.attr( "href", "#" )
+			.attr( "title", editable.options.cancel.text )
+			.append( $( "<span></span>" )
+				.addClass( cancelIconClass )
+				.html( editable.options.cancel.text ));
+	}
+};
+
+$.ui.editable.editors = {
+	text: {
+		element:function(editable) {
+			return $( "<input/>" )
+				.attr( "type", "text" )
+				.val( editable.value() )
+				.addClass( inputClass );
+		},
+		bind: function( editable ) {
+            var self = editable;
+			$( "input", editable.element )
+				.focus(function() {
+					self.frame.addClass( activeStateClass );
+				})
+				.blur(function() {
+					self.frame.removeClass( activeStateClass );
+				})
+				.bind( "keydown", function( event ) {
+					var keyCode = $.ui.keyCode;
+					switch ( event.keyCode ) {
+					case keyCode.ESCAPE:
+						self._cancel.call( self );
+						return true;
+					}
+				})
+				.focus();
+		},
+		value: function( editable, form ) {
+			return $( "input", form ).val();
+		}
+	},
+	textarea: $.noop,
+	select: $.noop,
+	spinner: $.noop 
+};
+
+})( jQuery );
