4bff6f564b132c70950c0a4100301a49bd802717
igor milla <igor.fsp.milla@gmail.com>
2011-10-11 02:09:08 +0300

    jquery.ui.datepicker-uk.js : Corrected translation of the weekHeader.(cherry picked from commit f25937b215b1302368a71d69368d4782abdb7bac)

--- ui/i18n/jquery.ui.datepicker-uk.js
+++ ui/i18n/jquery.ui.datepicker-uk.js
@@ -1,5 +1,6 @@
 /* Ukrainian (UTF-8) initialisation for the jQuery UI date picker plugin. */
 /* Written by Maxim Drogobitskiy (maxdao@gmail.com). */
+/* Corrected by Igor Milla (igor.fsp.milla@gmail.com). */
 jQuery(function($){
 	$.datepicker.regional['uk'] = {
 		closeText: 'Закрити',
@@ -13,7 +14,7 @@ jQuery(function($){
 		dayNames: ['неділя','понеділок','вівторок','середа','четвер','п’ятниця','субота'],
 		dayNamesShort: ['нед','пнд','вів','срд','чтв','птн','сбт'],
 		dayNamesMin: ['Нд','Пн','Вт','Ср','Чт','Пт','Сб'],
-		weekHeader: 'Не',
+		weekHeader: 'Тиж',
 		dateFormat: 'dd/mm/yy',
 		firstDay: 1,
 		isRTL: false,
