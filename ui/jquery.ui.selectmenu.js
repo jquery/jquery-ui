4649f04279f4121b5f27ccb6c622b6ace55206e1
Felix Nagel <info@felixnagel.com>
2011-10-18 01:18:11 +0200

    id handling (follow-up), thx to @thg2k, see #176

--- ui/jquery.ui.selectmenu.js
+++ ui/jquery.ui.selectmenu.js
@@ -38,8 +38,8 @@ $.widget("ui.selectmenu", {
 		var self = this, o = this.options;
 
 		// set a default id value, generate a new random one if not set by developer
-		var selectmenuId = this.element.attr( 'id' ).replace(':', '\\:') || 'ui-selectmenu-' + Math.random().toString( 16 ).slice( 2, 10 );
-
+		var selectmenuId = (this.element.attr( 'id' ) || 'ui-selectmenu-' + Math.random().toString( 16 ).slice( 2, 10 )).replace(':', '\\:');
+		
 		// quick array of button and menu id's
 		this.ids = [ selectmenuId, selectmenuId + '-button', selectmenuId + '-menu' ];
 
