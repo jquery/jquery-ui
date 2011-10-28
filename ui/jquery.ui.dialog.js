4f627d89fe32ff709c4f03d8871f36f943a385b9
jvivs <james@kohort.com>
2011-10-28 17:14:33 -0300

    revert for better formatted commit message

--- ui/jquery.ui.dialog.js
+++ ui/jquery.ui.dialog.js
@@ -592,12 +592,7 @@ $.widget("ui.dialog", {
 			case "title":
 				// convert whatever was passed in o a string, for html() to not throw up
 				$( ".ui-dialog-title", self.uiDialogTitlebar )
-					.html($( ".ui-dialog-title", self.uiDialogTitlebar )
-					.html( value 
-                        			? ( $(value).size()
-                            				? value
-                            				: value.toString() )
-                        			: "&#160;" );
+					.html( "" + ( value || "&#160;" ) );
 				break;
 		}
 
