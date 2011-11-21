80ba251a9be532eb9de33aa8540c9238928c0d69
Felix Nagel <info@felixnagel.com>
2011-11-21 21:06:20 +0100

    Selectmenu: remove markup unit test

--- tests/unit/selectmenu/selectmenu_core.js
+++ tests/unit/selectmenu/selectmenu_core.js
@@ -2,23 +2,6 @@
 
 module( "selectmenu: core" );
 
-
-test("markup structure", function () {
-	expect(5);	
-	var element = $('#speed').selectmenu();
-	var widget = element.selectmenu("widget");
-	var button = widget.filter(".ui-selectmenu-button");
-	var menu = widget.filter(".ui-selectmenu-menu");
-
-	equals( button.length, 1, "button wrapper found");	
-	equals( button.children("a").length, 1, "button link found");
-		
-	equals( menu.length, 1, "menu wrapper found");
-	equals( menu.children("ul").length, 1, "menu found");	
-	
-	equals( menu.find("ul li.ui-menu-item").length, element.find("option").length, "menu li's found");
-});
-
 test("accessibility", function () {
 	var element = $('#speed').selectmenu();
 	var widget = element.selectmenu("widget");
