c8b4be1381fa36126acad72b6327487bcf20da6d
Richard D. Worth <rdworth@gmail.com>
2012-01-26 11:14:57 -0500

    Interaction: added missing file header

--- ui/jquery.ui.interaction.js
+++ ui/jquery.ui.interaction.js
@@ -1,3 +1,15 @@
+/*
+ * jQuery UI Interaction @VERSION
+ *
+ * Copyright 2011, AUTHORS.txt (http://jqueryui.com/about)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * http://docs.jquery.com/UI/Interaction
+ *
+ * Depends:
+ *	jquery.ui.widget.js
+ */
 (function( $, undefined ) {
 
 var interaction; // = $.ui.interaction
