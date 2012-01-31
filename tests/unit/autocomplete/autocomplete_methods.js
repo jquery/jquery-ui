62a70da959e71a2d5c8ecc27548ea38733ec3128
Scott González <scott.gonzalez@gmail.com>
2012-01-31 14:21:18 -0500

    Autocomplete: Added test for close method.

--- tests/unit/autocomplete/autocomplete_methods.js
+++ tests/unit/autocomplete/autocomplete_methods.js
@@ -9,15 +9,19 @@ test( "destroy", function() {
 	});
 });
 
-test( "search", function() {
-	expect( 3 );
+test( "search, close", function() {
+	expect( 6 );
 	var data = [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby", "python", "c", "scala", "groovy", "haskell", "perl" ],
 		element = $( "#autocomplete" ).autocomplete({
 			source: data,
 			minLength: 0
 		}),
 		menu = element.autocomplete( "widget" );
+
+	ok( menu.is( ":hidden" ), "menu is hidden on init" );
+
 	element.autocomplete( "search" );
+	ok( menu.is( ":visible" ), "menu is visible after search" );
 	equal( menu.find( ".ui-menu-item" ).length, data.length, "all items for a blank search" );
 
 	element.val( "has" ).autocomplete( "search" );
@@ -25,6 +29,9 @@ test( "search", function() {
 
 	element.autocomplete( "search", "ja" );
 	equal( menu.find( ".ui-menu-item" ).length, 2, "only java and javascript for 'ja'" );
+
+	element.autocomplete( "close" );
+	ok( menu.is( ":hidden" ), "menu is hidden after close" );
 });
 
 }( jQuery ) );
