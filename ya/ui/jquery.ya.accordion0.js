0225d3a0b9cb86bd5a58d0579c8a9843fc11941a
Q 13 <qsimon13@gmail.com>
2012-02-10 13:32:52 +0800

    添加accordion包装

---
+++ ya/ui/jquery.ya.accordion0.js
@@ -0,0 +1,13 @@
+/**
+ * @author 13
+ */
+(function($){
+	var Base=$.ui.accordion;
+	$.widget('ya.accordion0',Base,{
+		_create:function(){
+			Base.prototype._create.call(this);
+		}
+		
+	});
+}(jQuery));
+
