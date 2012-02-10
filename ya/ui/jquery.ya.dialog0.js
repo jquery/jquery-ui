86525c38e311165171db84f4affb691164d7d208
Q 13 <qsimon13@gmail.com>
2012-02-10 13:06:11 +0800

    dialog更改

--- ya/ui/jquery.ya.dialog0.js
+++ ya/ui/jquery.ya.dialog0.js
@@ -6,15 +6,6 @@
 	$.widget('ya.dialog0',Base,{
 		_create:function(){
 			Base.prototype._create.call(this);
-			var uiDialog=this.uiDialog,
-				options=this.options;
-			$('<div class="ui-dialog-bg"></div>').css(_.extend({
-				width:"100%",
-				height:"100%",
-				position:"absolute",
-				top:"0px",
-				left:"0px"
-			},options.uiBgCss||{})).prependTo(uiDialog);
 		}
 		
 	});
