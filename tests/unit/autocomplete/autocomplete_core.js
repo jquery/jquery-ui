f5b52a89a735082d06d6f6ea950efbccff0a227f
Scott González <scott.gonzalez@gmail.com>
2012-02-11 15:43:38 -0500

    Autocomplete: Removed the timeout for the change event. Fixes #7550 - Autocomplete change event not triggered in time.
    Thanks spekary for finding a workaround for IE.

--- tests/unit/autocomplete/autocomplete_core.js
+++ tests/unit/autocomplete/autocomplete_core.js
@@ -2,45 +2,6 @@
 
 module( "autocomplete: core" );
 
-asyncTest( "close-on-blur is properly delayed", function() {
-	expect( 3 );
-	var element = $( "#autocomplete" )
-			.autocomplete({
-				source: [ "java", "javascript" ]
-			})
-			.val( "ja" )
-			.autocomplete( "search" ),
-		menu = element.autocomplete( "widget" );
-
-	ok( menu.is( ":visible" ) );
-	element.blur();
-	ok( menu.is( ":visible" ) );
-	setTimeout(function() {
-		ok( menu.is( ":hidden") );
-		start();
-	}, 200 );
-});
-
-asyncTest( "close-on-blur is cancelled when starting a search", function() {
-	expect( 3 );
-	var element = $( "#autocomplete" )
-			.autocomplete({
-				source: [ "java", "javascript" ]
-			})
-			.val( "ja" )
-			.autocomplete( "search" ),
-		menu = element.autocomplete( "widget" );
-
-	ok( menu.is( ":visible" ) );
-	element.blur();
-	ok( menu.is( ":visible" ) );
-	element.autocomplete( "search" );
-	setTimeout(function() {
-		ok( menu.is( ":visible" ) );
-		start();
-	}, 200 );
-});
-
 test( "prevent form submit on enter when menu is active", function() {
 	expect( 2 );
 	var event,
