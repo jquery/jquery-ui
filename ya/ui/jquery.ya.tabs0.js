7a4b7b9fb69e33ba272fe69809cd5880a1e9ee45
Q 13 <qsimon13@gmail.com>
2012-02-14 17:59:56 +0800

    其他示例调整

--- ya/ui/jquery.ya.tabs0.js
+++ ya/ui/jquery.ya.tabs0.js
@@ -4,7 +4,23 @@
 (function($){
 	var Base=$.ui.tabs;
 	$.widget('ya.tabs0',Base,{
+		options: {
+			closable:false
+		},
 		_create:function(){
+			var self=this;
+			var element=self.element,
+				options=this.options;
+			if(options.closable){
+				options.tabTemplate='<li><a href="#{href}">#{label}</a><span class="ui-icon ui-icon-close">Remove Tab</span>';
+				element.on('click','.ui-icon-close',function(){
+					var thisJq=$(this);
+					var headerListJq=self.lis,
+						curHeaderJq=thisJq.parent();
+					var index = headerListJq.index(curHeaderJq);
+					self.remove(index);
+				});
+			}
 			Base.prototype._create.call(this);
 		}
 		
