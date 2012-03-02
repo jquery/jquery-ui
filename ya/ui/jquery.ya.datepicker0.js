35620fad81740e60e601aba26c26dd9c5c0992b2
Q 13 <qsimon13@gmail.com>
2012-03-02 16:33:34 +0800

    添加datepicker组件

---
+++ ya/ui/jquery.ya.datepicker0.js
@@ -0,0 +1,32 @@
+/**
+ * @author 13
+ */
+(function($,root){
+	var ya=root.ya,
+		sl=ya.sl,
+		uihelper=ya.uihelper,
+		Solution=sl.Solution;
+	$.widget('ya.datepicker0',{
+		options: {
+			advancedTheme:{	//设置widget的高级主题效果
+				shadow:false,	//设置背景阴影，默认存在
+				corner:false
+			}
+		},
+		_create:function(){
+			var self=this,
+				options=self.options,
+				advancedTheme=options.advancedTheme;
+			self.element.datepicker(options);
+			if(advancedTheme){
+				for(var n in advancedTheme){
+					if(advancedTheme[n]){
+						uihelper.advancedThemeH(self,n,'#ui-datepicker-div');
+					}		
+				}
+			}
+			
+		}
+		
+	});
+}(jQuery,this));
