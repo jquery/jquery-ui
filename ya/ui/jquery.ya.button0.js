0648065ed29330cf5aa19bf957b78ad559c16255
Q 13 <qsimon13@gmail.com>
2012-03-05 11:03:46 +0800

    dialog示例调整，按钮组件化

---
+++ ya/ui/jquery.ya.button0.js
@@ -0,0 +1,45 @@
+/**
+ * @author 13
+ */
+(function($,root){
+	var ya=root.ya,
+		sl=ya.sl,
+		regx=ya.regx,
+		uihelper=ya.uihelper,
+		Solution=sl.Solution;
+	var Base=$.ui.button;
+	$.widget('ya.button0',Base,{
+		options: {
+			advancedTheme:{	//设置widget的高级主题效果
+				shadow:false,	//设置背景阴影，默认存在
+				corner:false
+			},
+			cls:null
+		},
+		_create:function(){
+			var self=this,
+				options=self.options,
+				cls=options.cls,
+				advancedTheme=options.advancedTheme;
+			Base.prototype._create.call(this);
+			
+			//为buttonElement提供特殊的class
+			if(cls){
+				var textCls="";
+				cls.replace(regx.rword,function(v){
+					textCls+=v+"-text ";
+				});
+			}
+			self.buttonElement.addClass(cls);
+			$('.ui-button-text',self.buttonElement).addClass(textCls);
+			if(advancedTheme){
+				for(var n in advancedTheme){
+					if(advancedTheme[n]){
+						uihelper.advancedThemeH(self,n,$('.ui-button-text',self.buttonElement));
+					}		
+				}
+			}
+		}
+		
+	});
+}(jQuery,this));
--- ya/ui/jquery.ya.dialog0.js
+++ ya/ui/jquery.ya.dialog0.js
@@ -7,11 +7,23 @@
 		uihelper=ya.uihelper,
 		Solution=sl.Solution;
 	var Base=$.ui.dialog;
+	
+	var attrFn = $.attrFn || {
+		val: true,
+		css: true,
+		html: true,
+		text: true,
+		data: true,
+		width: true,
+		height: true,
+		offset: true,
+		click: true
+	};
 	$.widget('ya.dialog0',Base,{
 		options: {
 			advancedTheme:{	//设置widget的高级主题效果
-				shadow:true,	//设置背景阴影，默认存在
-				corner:true
+				shadow:false,	//设置背景阴影，默认存在
+				corner:false
 			}
 		},
 		_create:function(){
@@ -27,11 +39,60 @@
 					}		
 				}
 			}
-			//按钮添加圆角
-			var buttonJq=$('.ui-dialog-buttonset .ui-button span',self.uiDialog);	//buttons
-			uihelper.advancedThemeH(self,'corner',buttonJq);
+			//按钮添加圆角,移到button组件中实现
+			//var buttonJq=$('.ui-dialog-buttonset .ui-button span',self.uiDialog);	//buttons
+			//uihelper.advancedThemeH(self,'corner',buttonJq);
 			//去除虚线
 			uihelper.removeOutline(self,self.uiDialog);
+		},
+		_createButtons: function(buttons) {
+			var self = this,
+				hasButtons = false,
+				uiDialogButtonPane = $('<div></div>')
+					.addClass(
+						'ui-dialog-buttonpane ' +
+						'ui-widget-content ' +
+						'ui-helper-clearfix'
+					),
+				uiButtonSet = $( "<div></div>" )
+					.addClass( "ui-dialog-buttonset" )
+					.appendTo( uiDialogButtonPane );
+	
+			// if we already have a button pane, remove it
+			self.uiDialog.find('.ui-dialog-buttonpane').remove();
+	
+			if (typeof buttons === 'object' && buttons !== null) {
+				$.each(buttons, function() {
+					return !(hasButtons = true);
+				});
+			}
+			if (hasButtons) {
+				$.each(buttons, function(name, props) {
+					props = $.isFunction( props ) ?
+						{ click: props, text: name } :
+						props;
+					var button = $('<button type="button"></button>')
+						.click(function() {
+							props.click.apply(self.element[0], arguments);
+						})
+						.appendTo(uiButtonSet);
+					// can't use .attr( props, true ) with jQuery 1.3.2.
+					$.each( props, function( key, value ) {
+						if ( key === "click" ) {
+							return;
+						}
+						if ( key in attrFn ) {
+							button[ key ]( value );
+						} else if(key!="options"){
+							button.attr( key, value );
+						}
+					});
+					if ($.fn.button0) {
+						button.button0(props.options||{});	//替换为button0
+					}
+				});
+				uiDialogButtonPane.appendTo(self.uiDialog);
+			}
 		}
 		
 	});
