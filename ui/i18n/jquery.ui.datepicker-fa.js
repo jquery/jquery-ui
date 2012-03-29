ddbb57e058003304d0d47a56d592b1dc04616cfc
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2012-03-29 13:29:21 +0300

    Persian localization corrections.
    1. Changed ي to ی according to a bug report in MediaWiki ( https://bugzilla.wikimedia.org/show_bug.cgi?id=35579 ).
    
    2. Put short day names in a separate variable, because they are the same for Short and Min.
    
    3. Added line breaks, because otherwise reading and editing right-to-left is very hard.

--- ui/i18n/jquery.ui.datepicker-fa.js
+++ ui/i18n/jquery.ui.datepicker-fa.js
@@ -2,17 +2,46 @@
 /* Javad Mowlanezhad -- jmowla@gmail.com */
 /* Jalali calendar should supported soon! (Its implemented but I have to test it) */
 jQuery(function($) {
+	var dayNamesShort = [
+		'ی',
+		'د',
+		'س',
+		'چ',
+		'پ',
+		'ج', 
+		'ق'
+	];
 	$.datepicker.regional['fa'] = {
 		closeText: 'بستن',
-		prevText: '&#x3C;قبلي',
-		nextText: 'بعدي&#x3E;',
+		prevText: '&#x3C;قبلی',
+		nextText: 'بعدی&#x3E;',
 		currentText: 'امروز',
-		monthNames: ['فروردين','ارديبهشت','خرداد','تير','مرداد','شهريور',
-		'مهر','آبان','آذر','دي','بهمن','اسفند'],
+		monthNames: [
+			'فروردين',
+			'ارديبهشت',
+			'خرداد',
+			'تير',
+			'مرداد',
+			'شهريور',
+			'مهر',
+			'آبان',
+			'آذر',
+			'دي',
+			'بهمن',
+			'اسفند'
+		],
 		monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
-		dayNames: ['يکشنبه','دوشنبه','سه‌شنبه','چهارشنبه','پنجشنبه','جمعه','شنبه'],
-		dayNamesShort: ['ي','د','س','چ','پ','ج', 'ش'],
-		dayNamesMin: ['ي','د','س','چ','پ','ج', 'ش'],
+		dayNames: [
+			'يکشنبه',
+			'دوشنبه',
+			'سه‌شنبه',
+			'چهارشنبه',
+			'پنجشنبه',
+			'جمعه',
+			'شنبه'
+		],
+		dayNamesShort: dayNamesShort,
+		dayNamesMin: dayNamesShort,
 		weekHeader: 'هف',
 		dateFormat: 'yy/mm/dd',
 		firstDay: 6,
