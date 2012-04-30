fc3fc6ec2e9d8cf245c67e90668c1b1ee83f6d24
Scott González <scott.gonzalez@gmail.com>
2012-04-30 08:42:01 -0400

    Effects tests: Added TODO for disabled test.

--- tests/unit/effects/effects_core.js
+++ tests/unit/effects/effects_core.js
@@ -31,7 +31,7 @@ test( "Immediate Return Conditions", function() {
 	equal( ++count, 3, "Both Functions worked properly" );
 });
 
-/* Disabled - Can't figure out why this is failing in IE 6/7
+/* TODO: Disabled - Can't figure out why this is failing in IE 6/7
 test( "createWrapper and removeWrapper retain focused elements (#7595)", function() {
 	expect( 2 );
 	var test = $( "div.hidden" ).show(),
