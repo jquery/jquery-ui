d6f99ea3245da2f46577634b6cc0e5f64dccea57
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-05-07 12:17:45 +0200

    Grunt: Cleanup testswarm task, increase timeout to 20 minutes (until BrowserStack queue is increased)

--- grunt.js
+++ grunt.js
@@ -345,42 +345,42 @@ grunt.registerTask( "testswarm", function( commit, configFile ) {
 		testswarm = require( "testswarm" ),
 		testBase = "http://swarm.jquery.org/git/jquery-ui/" + commit + "/tests/unit/",
 		testUrls = [],
-		config = grunt.file.readJSON( configFile );
 		tests = {
-		"Accordion": "accordion/accordion.html",
-		"Accordion_deprecated": "accordion/accordion_deprecated.html",
-		"Autocomplete": "autocomplete/autocomplete.html",
-		"Button": "button/button.html",
-		"Core": "core/core.html",
-		//"datepicker/datepicker.html",
-		//"dialog/dialog.html",
-		//"draggable/draggable.html",
-		//"droppable/droppable.html",
-		"Effects": "effects/effects.html",
-		"Menu": "menu/menu.html",
-		"Position": "position/position.html",
-		"Position_deprecated": "position/position_deprecated.html",
-		"Progressbar": "progressbar/progressbar.html",
-		//"resizable/resizable.html",
-		//"selectable/selectable.html",
-		//"slider/slider.html",
-		//"sortable/sortable.html",
-		"Spinner": "spinner/spinner.html",
-		"Tabs": "tabs/tabs.html",
-		"Tabs_deprecated": "tabs/tabs_deprecated.html",
-		"Tooltip": "tooltip/tooltip.html",
-		"Widget": "widget/widget.html"
-	};
-	for (test in tests) {
+			"Accordion": "accordion/accordion.html",
+			"Accordion_deprecated": "accordion/accordion_deprecated.html",
+			"Autocomplete": "autocomplete/autocomplete.html",
+			"Button": "button/button.html",
+			"Core": "core/core.html",
+			//"datepicker/datepicker.html",
+			//"dialog/dialog.html",
+			//"draggable/draggable.html",
+			//"droppable/droppable.html",
+			"Effects": "effects/effects.html",
+			"Menu": "menu/menu.html",
+			"Position": "position/position.html",
+			"Position_deprecated": "position/position_deprecated.html",
+			"Progressbar": "progressbar/progressbar.html",
+			//"resizable/resizable.html",
+			//"selectable/selectable.html",
+			//"slider/slider.html",
+			//"sortable/sortable.html",
+			"Spinner": "spinner/spinner.html",
+			"Tabs": "tabs/tabs.html",
+			"Tabs_deprecated": "tabs/tabs_deprecated.html",
+			"Tooltip": "tooltip/tooltip.html",
+			"Widget": "widget/widget.html"
+		};
+	for ( test in tests ) {
 		testUrls.push( testBase + tests[ test ] + "?nojshint=true" );
 	}
 	testswarm({
 		url: "http://swarm.jquery.org/",
 		pollInterval: 10000,
+		timeout: 1000 * 60 * 20,
 		done: this.async()
 	}, {
 		authUsername: "jqueryui",
-		authToken: config.jqueryui.authToken,
+		authToken: grunt.file.readJSON( configFile ).jqueryui.authToken,
 		jobName: 'jQuery UI commit #<a href="https://github.com/jquery/jquery-ui/commit/' + commit + '">' + commit + '</a>',
 		runMax: 4,
 		"runNames[]": Object.keys(tests),
