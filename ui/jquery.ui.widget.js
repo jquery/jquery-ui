f0d4da890356c68f4e29c7dbc1c0349bf2181c1c
Scott González <scott.gonzalez@gmail.com>
2012-06-11 13:30:03 -0400

    Widget: Work around bug in jQuery <1.6.3 where .removeData( dashed-name ) doesn't work.

--- ui/jquery.ui.widget.js
+++ ui/jquery.ui.widget.js
@@ -251,7 +251,10 @@ $.Widget.prototype = {
 			// 1.9 BC for #7810
 			// TODO remove dual storage
 			.removeData( this.widgetName )
-			.removeData( this.widgetFullName );
+			.removeData( this.widgetFullName )
+			// support: jquery <1.6.3
+			// http://bugs.jquery.com/ticket/9413
+			.removeData( $.camelCase( this.widgetFullName ) );
 		this.widget()
 			.unbind( "." + this.widgetName )
 			.removeAttr( "aria-disabled" )
