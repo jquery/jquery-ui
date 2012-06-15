b65747317822149ea8c9e45f4e6f2189efed7258
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-06-15 23:51:05 +0200

    Grunt: Refactor testswarm task and add an experimental testswarm-multi-jquery task for testing across jQuery versions

--- build/tasks/testswarm.js
+++ build/tasks/testswarm.js
@@ -1,36 +1,38 @@
+/*jshint node: true */
 module.exports = function( grunt ) {
 
-grunt.registerTask( "testswarm", function( commit, configFile ) {
+var tests = {
+	"Accordion": "accordion/accordion.html",
+	"Accordion_deprecated": "accordion/accordion_deprecated.html",
+	"Autocomplete": "autocomplete/autocomplete.html",
+	"Button": "button/button.html",
+	"Core": "core/core.html",
+	//"datepicker/datepicker.html",
+	//"dialog/dialog.html",
+	//"draggable/draggable.html",
+	//"droppable/droppable.html",
+	"Effects": "effects/effects.html",
+	"Menu": "menu/menu.html",
+	"Position": "position/position.html",
+	"Position_deprecated": "position/position_deprecated.html",
+	"Progressbar": "progressbar/progressbar.html",
+	//"resizable/resizable.html",
+	//"selectable/selectable.html",
+	//"slider/slider.html",
+	//"sortable/sortable.html",
+	"Spinner": "spinner/spinner.html",
+	"Tabs": "tabs/tabs.html",
+	"Tabs_deprecated": "tabs/tabs_deprecated.html",
+	"Tooltip": "tooltip/tooltip.html",
+	"Widget": "widget/widget.html"
+};
+
+function submit( commit, tests, configFile ) {
 	var test,
 		testswarm = require( "testswarm" ),
 		config = grunt.file.readJSON( configFile ).jqueryui,
 		testBase = config.testUrl + commit + "/tests/unit/",
-		testUrls = [],
-		tests = {
-			"Accordion": "accordion/accordion.html",
-			"Accordion_deprecated": "accordion/accordion_deprecated.html",
-			"Autocomplete": "autocomplete/autocomplete.html",
-			"Button": "button/button.html",
-			"Core": "core/core.html",
-			//"datepicker/datepicker.html",
-			//"dialog/dialog.html",
-			//"draggable/draggable.html",
-			//"droppable/droppable.html",
-			"Effects": "effects/effects.html",
-			"Menu": "menu/menu.html",
-			"Position": "position/position.html",
-			"Position_deprecated": "position/position_deprecated.html",
-			"Progressbar": "progressbar/progressbar.html",
-			//"resizable/resizable.html",
-			//"selectable/selectable.html",
-			//"slider/slider.html",
-			//"sortable/sortable.html",
-			"Spinner": "spinner/spinner.html",
-			"Tabs": "tabs/tabs.html",
-			"Tabs_deprecated": "tabs/tabs_deprecated.html",
-			"Tooltip": "tooltip/tooltip.html",
-			"Widget": "widget/widget.html"
-		};
+		testUrls = [];
 	for ( test in tests ) {
 		testUrls.push( testBase + tests[ test ] + "?nojshint=true" );
 	}
@@ -40,7 +42,7 @@ grunt.registerTask( "testswarm", function( commit, configFile ) {
 		timeout: 1000 * 60 * 30,
 		done: this.async()
 	}, {
-		authUsername: "jqueryui",
+		authUsername: config.authUsername,
 		authToken: config.authToken,
 		jobName: 'jQuery UI commit #<a href="https://github.com/jquery/jquery-ui/commit/' + commit + '">' + commit.substr( 0, 10 ) + '</a>',
 		runMax: config.runMax,
@@ -48,6 +50,22 @@ grunt.registerTask( "testswarm", function( commit, configFile ) {
 		"runUrls[]": testUrls,
 		"browserSets[]": ["popular"]
 	});
+}
+
+grunt.registerTask( "testswarm", function( commit, configFile ) {
+	submit( commit, tests, configFile );
+});
+
+grunt.registerTask( "testswarm-multi-jquery", function( commit, configFile ) {
+	var test,
+		allTests = {};
+	for ( test in tests ) {
+		allTests[ test + "-1.6.3" ] = tests[ test ] + "?nojshint=true&jquery=1.6.3";
+	}
+	for ( test in tests ) {
+		allTests[ test + "-1.7.2" ] = tests[ test ] + "?nojshint=true&jquery=1.7.2";
+	}
+	submit( commit, allTests, configFile );
 });
 
 };
