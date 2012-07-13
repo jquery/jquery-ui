be0be892d3a2658f1f4518a7d3346ef356aa89c6
kborchers <kris.borchers@gmail.com>
2012-07-12 23:01:13 -0500

    Menu: Fix issue with missing active item when clicking a menu item more than once

--- ui/jquery.ui.menu.js
+++ ui/jquery.ui.menu.js
@@ -583,7 +583,8 @@ $.widget( "ui.menu", {
 	select: function( event ) {
 		// Save active reference before collapseAll triggers blur
 		var ui = {
-			item: this.active
+			// Selecting a menu item removes the active item causing multiple clicks to be missing an item
+			item: this.active || $( event.target ).closest( ".ui-menu-item" )
 		};
 		this.collapseAll( event, true );
 		this._trigger( "select", event, ui );
