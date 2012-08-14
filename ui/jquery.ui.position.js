04df2b95421bc6c440b4205d88c6a56e84fdee30
Scott González <scott.gonzalez@gmail.com>
2012-08-14 17:00:07 -0400

    Position: Create curCSS alias if it doesn't exist. Fixes #8501 - 1.8 Compat; Position still uses $.curCSS.

--- ui/jquery.ui.position.js
+++ ui/jquery.ui.position.js
@@ -257,6 +257,11 @@ if ( !$.offset.setOffset ) {
 	};
 }
 
+// jQuery <1.4.3 uses curCSS, in 1.4.3 - 1.7.2 curCSS = css, 1.8+ only has css
+if ( !$.curCSS ) {
+	$.curCSS = $.css;
+}
+
 // fraction support test (older versions of jQuery don't support fractions)
 (function () {
 	var body = document.getElementsByTagName( "body" )[ 0 ], 
