cbe7fc32eb9bb60507b6161767c6de9a29d66bf5
Dale Kocian <dale.kocian@gmail.com>
2012-10-15 17:27:27 -0400

    Button: added replace on radio name to escape quotes. Fixed #7505 - Button: Buttonset not applied to radio group with quotation/apostrophe in name

--- ui/jquery.ui.button.js
+++ ui/jquery.ui.button.js
@@ -25,7 +25,7 @@ var lastActive, startXPos, startYPos, clickDragged,
 		}, 1 );
 	},
 	radioGroup = function( radio ) {
-		var name = radio.name,
+		var name = radio.name.replace( /'/g, "\\'" ),
 			form = radio.form,
 			radios = $( [] );
 		if ( name ) {
