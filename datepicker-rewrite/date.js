69ab148171e3c8fcfd304810d0ca5f324d0fa49c
Sean Cady <sean.cady@ge.com>
2012-10-15 13:25:18 -0400

    Date: Added Hour, minute, second to date creation; Added logic to handle day overflow on year or month change; Fixed naming of monthname. Added tests for everything. Finish merge from master.

--- datepicker-rewrite/date.js
+++ datepicker-rewrite/date.js
@@ -6,14 +6,14 @@
  */
 (function( $, undefined ) {
 
-$.date = function ( datestring, formatstring ) {
+$.date = function( datestring, formatstring ) {
 	//TODO: Need to refactor $.date to be a constructor, move the methods to a prototype.
 	var calendar = Globalize.culture().calendar,
 		format = formatstring ? formatstring : calendar.patterns.d,
-		date = datestring ? Globalize.parseDate(datestring, format) : new Date();
+		date = datestring ? Globalize.parseDate( datestring, format ) : new Date();
 
 	if ( !date ) {
-		date = new Date()
+		date = new Date();
 	}
 
 	return {
@@ -35,18 +35,30 @@ $.date = function ( datestring, formatstring ) {
 			return this;
 		},
 		setDay: function( day ) {
-			date = new Date( date.getFullYear(), date.getMonth(), day );
+			date = new Date( date.getFullYear(), date.getMonth(), day, date.getHours(), date.getMinutes(), date.getSeconds() );
 			return this;
 		},
 		setMonth: function( month ) {
-			//TODO: Should update this to keep the time component (hour, min, sec) intact. Same for setYear and setFullDate.
-			//TODO: Do we want to do any special handling when switching to a month that causes the days to overflow?
-			date = new Date( date.getFullYear(), month, date.getDate());
+			// Overflow example:  Month is October 31 (yeah Halloween) and month is changed to April with 30 days,
+			// the new date will me May 1.  We will honor the month the user wants to set and if and overflow
+			// occurs, set to last day of month.
+			var days = date.getDay(), year = date.getFullYear();
+			if ( days > this.daysInMonth( year, month ) ) {
+				// Overflow
+				days = this.daysInMonth( year, month );
+			}
+			date = new Date( year, month, days, date.getHours(), date.getMinutes(), date.getSeconds() );
 			return this;
 		},
 		setYear: function( year ) {
-			//TODO: Same question as setMonth, but I suppose only for leap years.
-			date = new Date( year, date.getMonth(), date.getDate() );
+			var day = date.getDate(),
+				month = date.getMonth();
+			// Check if Leap, and February and day is 29th
+			if ( this.isLeapYear( year ) && month == 1 && day == 29 ) {
+				// set day to last day of February
+				day = this.daysInMonth( year, month );
+			}
+			date = new Date( year, month, day, date.getHours(), date.getMinutes(), date.getSeconds() );
 			return this;
 		},
 		setFullDate: function( year, month, day ) {
@@ -57,11 +69,11 @@ $.date = function ( datestring, formatstring ) {
 			var day = period == "D" ? date.getDate() + offset : date.getDate(),
 				month = period == "M" ? date.getMonth() + offset : date.getMonth(),
 				year = period == "Y" ? date.getFullYear() + offset : date.getFullYear();
-
+			// If not day, update the day to the new month and year
 			if ( period != "D" ) {
-				day = Math.max(1, Math.min( day, this.daysInMonth( year, month ) ) );
+				day = Math.max( 1, Math.min( day, this.daysInMonth( year, month ) ) );
 			}
-			date = new Date( year, month, day );
+			date = new Date( year, month, day, date.getHours(), date.getMinutes(), date.getSeconds() );
 			return this;
 		},
 		daysInMonth: function( year, month ) {
@@ -69,7 +81,7 @@ $.date = function ( datestring, formatstring ) {
 			month = month || date.getMonth();
 			return 32 - new Date( year, month, 32 ).getDate();
 		},
-		monthname: function() {
+		monthName: function() {
 			return calendar.months.names[ date.getMonth() ];
 		},
 		day: function() {
@@ -81,12 +93,16 @@ $.date = function ( datestring, formatstring ) {
 		year: function() {
 			return date.getFullYear();
 		},
+		isLeapYear: function( year ) {
+			year = year || date.getFullYear();
+			return new Date( year, 1, 29 ).getMonth() == 1;
+
+		},
 		weekdays: function() {
-			// TODO take firstDay into account
 			var result = [];
 			for ( var dow = 0; dow < 7; dow++ ) {
 				var day = ( dow + calendar.firstDay ) % 7;
-				result.push( {
+				result.push({
 					shortname: calendar.days.namesShort[ day ],
 					fullname: calendar.days.names[ day ]
 				});
@@ -98,7 +114,7 @@ $.date = function ( datestring, formatstring ) {
 				today = $.date(),
 				firstDayOfMonth = new Date( this.year(), date.getMonth(), 1 ).getDay(),
 				leadDays = ( firstDayOfMonth - calendar.firstDay + 7 ) % 7,
-				rows = Math.ceil( ( leadDays + this.daysInMonth() ) / 7),
+				rows = Math.ceil( ( leadDays + this.daysInMonth() ) / 7 ),
 				printDate = new Date( this.year(), date.getMonth(), 1 - leadDays );
 			for ( var row = 0; row < rows; row++ ) {
 				var week = result[ result.length ] = {
@@ -114,33 +130,34 @@ $.date = function ( datestring, formatstring ) {
 						today: today.equal( printDate )
 					};
 					day.render = day.selectable = !day.lead;
-					this.eachDay( day );
+					// TODO undefined in picker demos, fix it
+					// this.eachDay( day );
 					// TODO use adjust("D", 1)?
 					printDate.setDate( printDate.getDate() + 1 );
 				}
 			}
 			return result;
 		},
-		// specialzed for multi-month template, could be used in general
+		// specialized for multi-month template, could be used in general
 		months: function( add ) {
 			var result = [],
-				current = date.getMonth(),
-				self = this;
+				current = date.getMonth();
 			for ( var i = 0; i < add + 1; i++ ) {
 				result.push( this.clone() );
 				this.adjust( "M", 1 );
 			}
-			result[0].first = true;
-			result[result.length - 1].last = true;
-			date.setMonth(current);
+			result[ 0 ].first = true;
+			result[ result.length - 1 ].last = true;
+			date.setMonth( current );
 			return result;
 		},
-		iso8601Week: function( date ) {
+		iso8601Week: function(date) {
 			var checkDate = new Date( date.getTime() );
 			// Find Thursday of this week starting on Monday
 			checkDate.setDate( checkDate.getDate() + 4 - ( checkDate.getDay() || 7 ) );
 			var time = checkDate.getTime();
-			checkDate.setMonth( 0 ); // Compare with Jan 1
+			// Compare with Jan 1
+			checkDate.setMonth( 0 );
 			checkDate.setDate( 1 );
 			return Math.floor( Math.round( ( time - checkDate ) / 86400000) / 7 ) + 1;
 		},
@@ -148,18 +165,17 @@ $.date = function ( datestring, formatstring ) {
 			this.selected = this.clone();
 			return this;
 		},
-		// TODO create new Date with year, month, day instead
 		clone: function() {
-			var result = $.date( this.format(), format );
-			result.eachDay = this.eachDay;
-			return result;
+			return $.date( new Date(date.getFullYear(), date.getMonth(),
+				date.getDate(), date.getHours(),
+				date.getMinutes(), date.getSeconds()), formatstring );
 		},
 		// TODO compare year, month, day each for better performance
 		equal: function( other ) {
 			function format( date ) {
 				return Globalize.format( date, "d" );
 			}
-			return format( date ) == format( other );
+			return format( date ) === format( other );
 		},
 		date: function() {
 			return date;
@@ -174,7 +190,7 @@ $.date = function ( datestring, formatstring ) {
 			}
 			return calendar;
 		}
-	}
-}
+	};
+};
 
 }( jQuery ));
