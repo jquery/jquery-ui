11528ffcc2aef495fd31263229c46fc11e81b093
David Petersen <public@petersendidit.com>
2012-10-19 15:39:37 -0500

    Tabs: use the _classes method.
    Fixes #7053

--- tests/unit/tabs/tabs_common.js
+++ tests/unit/tabs/tabs_common.js
@@ -1,6 +1,16 @@
 TestHelpers.commonWidgetTests( "tabs", {
 	defaults: {
 		active: null,
+		classes: {
+			"ui-tabs": "ui-corner-all",
+			"ui-tabs-collapsible": null,
+			"ui-tabs-active": null,
+			"ui-tabs-nav": "ui-corner-all",
+			"ui-tab": "ui-corner-top",
+			"ui-tabs-anchor": null,
+			"ui-tabs-panel": "ui-corner-bottom",
+			"ui-tabs-loading": null
+		},
 		collapsible: false,
 		disabled: false,
 		event: "click",
