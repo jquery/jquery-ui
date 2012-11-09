00e92e16b4f4a9f30cb12bbdc23aeba97d17203d
Mike Sherov <mike.sherov@gmail.com>
2012-11-09 17:26:12 -0500

    Dev: remove all-active test runner as now all test suites can run cleanly!

--- grunt.js
+++ grunt.js
@@ -281,7 +281,7 @@ grunt.initConfig({
 		files: grunt.file.expandFiles( "tests/unit/**/*.html" ).filter(function( file ) {
 			// disabling everything that doesn't (quite) work with PhantomJS for now
 			// TODO except for all|index|test, try to include more as we go
-			return !( /(all|all-active|index|test|dialog|tabs|tooltip)\.html$/ ).test( file );
+			return !( /(all|index|test|dialog|tabs|tooltip)\.html$/ ).test( file );
 		})
 	},
 	lint: {
