9d18aee2a1dd60fccc2755cb6dcca297b98ea587
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-11-16 20:24:57 +0100

    Dialog: Extend autofocus, starting with [autofocus], then :tabbable content, then buttonpane, then close button, then dialog. Fixes #4731 - Dialog: Add option to set which element gains focus on open

--- tests/unit/dialog/dialog_core.js
+++ tests/unit/dialog/dialog_core.js
@@ -17,6 +17,8 @@ test("title id", function() {
 	el.remove();
 });
 
+// TODO test for aria-describedby
+// add only when the attribute isn't anywhere yet
 test("ARIA", function() {
 	expect(4);
 
@@ -40,6 +42,46 @@ test("widget method", function() {
 	expect( 1 );
 	var dialog = $("<div>").appendTo("#main").dialog();
 	deepEqual(dialog.parent()[0], dialog.dialog("widget")[0]);
+});
+
+test( "focus tabbable", function() {
+	expect( 5 );
+	var el,
+		options = {
+			buttons: [{
+				text: "Ok",
+				click: $.noop
+			}]
+		};
+
+	// 1. first element inside the dialog matching [autofocus]
+	el = $( "<div><input><input autofocus></div>" ).dialog( options );
+	equal( document.activeElement, el.find( "input" )[ 1 ] );
+	el.remove();
+
+	// 2. tabbable element inside the content element
+	el = $( "<div><input><input></div>" ).dialog( options );
+	equal( document.activeElement, el.find( "input" )[ 0 ] );
+	el.remove();
+
+	// 3. tabbable element inside the buttonpane
+	el = $( "<div>text</div>" ).dialog( options );
+	equal( document.activeElement, el.dialog( "widget" ).find( ".ui-dialog-buttonpane button" )[ 0 ] );
+	el.remove();
+
+	// 4. the close button
+	el = $( "<div>text</div>" ).dialog();
+	equal( document.activeElement, el.dialog( "widget" ).find( ".ui-dialog-titlebar .ui-dialog-titlebar-close" )[ 0 ] );
+	el.remove();
+
+	// 5. the dialog itself
+	el = $( "<div>text</div>" ).dialog({
+		autoOpen: false
+	});
+	el.dialog( "widget" ).find( ".ui-dialog-titlebar-close" ).hide();
+	el.dialog( "open" );
+	equal( document.activeElement, el.parent()[ 0 ] );
+	el.remove();
 });
 
 })(jQuery);
