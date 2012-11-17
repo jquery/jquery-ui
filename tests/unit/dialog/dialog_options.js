5d251b477cb7c4f3e86fe09a689c41e6b6ad0bb0
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-11-17 12:10:26 +0100

    Dialog: Move array notation support for position option to backCompat check. Fixes #8824 - Deprecate array notation for position option.

--- tests/unit/dialog/dialog_options.js
+++ tests/unit/dialog/dialog_options.js
@@ -331,16 +331,6 @@ if ( !$.ui.ie ) {
 		el.remove();
 	});
 
-	test("position, right bottom on window w/array", function() {
-		expect( 2 );
-		var el = $('<div></div>').dialog({ position: ["right", "bottom"] }),
-			dialog = el.dialog('widget'),
-			offset = dialog.offset();
-		closeEnough(offset.left, $(window).width() - dialog.outerWidth() + $(window).scrollLeft(), 1);
-		closeEnough(offset.top, $(window).height() - dialog.outerHeight() + $(window).scrollTop(), 1);
-		el.remove();
-	});
-
 	test("position, right bottom at right bottom via ui.position args", function() {
 		expect( 2 );
 		var el = $('<div></div>').dialog({
@@ -358,16 +348,6 @@ if ( !$.ui.ie ) {
 	});
 
 }
-
-test("position, offset from top left w/array", function() {
-	expect( 2 );
-	var el = $('<div></div>').dialog({ position: [10, 10] }),
-		dialog = el.dialog('widget'),
-		offset = dialog.offset();
-	closeEnough(offset.left, 10 + $(window).scrollLeft(), 1);
-	closeEnough(offset.top, 10 + $(window).scrollTop(), 1);
-	el.remove();
-});
 
 test("position, at another element", function() {
 	expect( 4 );
