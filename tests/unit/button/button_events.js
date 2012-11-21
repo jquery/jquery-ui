c7271393d4198fc07254f70427d277914d1cdb46
Mike Sherov <mike.sherov@gmail.com>
2012-11-21 08:33:43 -0500

    Button: add blur event to buttons. Fixes #8559 - Button: Button doesn't remove active state when using keyboard.

--- tests/unit/button/button_events.js
+++ tests/unit/button/button_events.js
@@ -13,4 +13,14 @@ test("buttonset works with single-quote named elements (#7505)", function() {
 	}).click();
 });
 
+test( "when button loses focus, ensure active state is removed (#8559)", function() {
+	expect( 1 );
+
+	$("#button").button().keypress( function() {
+		$("#button").blur( function() {
+			ok( !$("#button").is(".ui-state-active"), "button loses active state appropriately" );
+		}).blur();
+	}).focus().simulate( "keydown", { keyCode: $.ui.keyCode.ENTER } ).simulate( "keypress", { keyCode: $.ui.keyCode.ENTER } );
+});
+
 })(jQuery);
