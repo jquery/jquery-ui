425a3c89e89564d62a787d4256089a64717b2f96
Matthieu Penant <thieum22@hotmail.com>
2012-12-05 13:21:13 -0800

    i18n: Added datepicker local. for Canadian French
    i18n: Added datepicker localization for Canadian French

---
+++ ui/i18n/jquery.ui.datepicker-fr-CA.js
@@ -0,0 +1,23 @@
+/* Canadian-French initialisation for the jQuery UI date picker plugin. */
+jQuery(function ($) {
+    $.datepicker.regional['fr-CA'] = {
+        closeText: 'Fermer',
+        prevText: 'Précédent',
+        nextText: 'Suivant',
+        currentText: 'Aujourd\'hui',
+        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+		'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin',
+		'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+        dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+        weekHeader: 'Sem.',
+        dateFormat: 'yy-mm-dd',
+        firstDay: 0,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['fr-CA']);
+});
