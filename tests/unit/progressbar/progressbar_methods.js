9d0df536240f1b5e84d1f772ef95117280acd4d8
Scott González <scott.gonzalez@gmail.com>
2012-12-06 09:50:33 -0500

    Progress tests: Cleanup.

--- tests/unit/progressbar/progressbar_methods.js
+++ tests/unit/progressbar/progressbar_methods.js
@@ -12,7 +12,7 @@ test( "value", function() {
 
 	var element = $( "<div>" ).progressbar({ value: 20 });
 	equal( element.progressbar( "value" ), 20, "correct value as getter" );
-	equal( element.progressbar( "value", 30), element, "chainable as setter" );
+	strictEqual( element.progressbar( "value", 30 ), element, "chainable as setter" );
 	equal( element.progressbar( "option", "value" ), 30, "correct value after setter" );
 });
 
