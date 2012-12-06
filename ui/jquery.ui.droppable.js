fb38c20763c637e4d05ca2a48e25db4b380754be
Scott González <scott.gonzalez@gmail.com>
2012-12-06 14:43:35 -0500

    Droppable: Added missing semicolon.

--- ui/jquery.ui.droppable.js
+++ ui/jquery.ui.droppable.js
@@ -207,7 +207,7 @@ $.ui.intersect = function(draggable, droppable, toleranceMode) {
 		case 'pointer':
 			draggableLeft = ((draggable.positionAbs || draggable.position.absolute).left + (draggable.clickOffset || draggable.offset.click).left);
 			draggableTop = ((draggable.positionAbs || draggable.position.absolute).top + (draggable.clickOffset || draggable.offset.click).top);
-			return isOverAxis( draggableTop, t, droppable.proportions.height ) && isOverAxis( draggableLeft, l, droppable.proportions.width )
+			return isOverAxis( draggableTop, t, droppable.proportions.height ) && isOverAxis( draggableLeft, l, droppable.proportions.width );
 		case 'touch':
 			return (
 				(y1 >= t && y1 <= b) ||	// Top edge touching
