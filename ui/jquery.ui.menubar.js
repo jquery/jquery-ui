c57319c0948e9e3a7c15879fb96fa9b0779a1740
Steven G. Harms <sharms@shopittome.com>
2012-12-08 15:16:23 -0800

    Revert "menubar: decompose _create"
    This reverts commit ec67b65fd46ec82d4162650a4de327fd09cb4548.
    
    This is not *quite* ready for pull request.

--- ui/jquery.ui.menubar.js
+++ ui/jquery.ui.menubar.js
@@ -31,85 +31,42 @@ $.widget( "ui.menubar", {
 		}
 	},
 	_create: function() {
-		// Top-level items, typically li
+		var that = this, subMenus;
 		this.menuItems = this.element.children( this.options.items );
-
-		// (Typically) menuItem contained, event-able used to launch a sub-menu
 		this.items = this.menuItems.children( "button, a" );
 
-		// Keep track of open submenus
-		this.openSubmenus = 0;
+		this.menuItems
+			.addClass( "ui-menubar-item" )
+			.attr( "role", "presentation" );
+		// let only the first item receive focus
+		this.items.slice(1).attr( "tabIndex", -1 );
 
-		// (Typically) a ul
-		var subMenus = this.items.siblings( this.options.menuElement );
-
-		this._prepareElement(this.element);
-		this._prepareMenuItems(this.menuItems);
-		this._prepareItems(this.items);
-		this._prepareSubMenus(subMenus);
-	},
-	_prepareElement: function(element) {
-		element
+		this.element
 			.addClass( "ui-menubar ui-widget-header ui-helper-clearfix" )
 			.attr( "role", "menubar" );
-	  this._bindPluginElementEvents(element);
-	},
-	_prepareMenuItems: function(menuItems) {
-		menuItems.addClass( "ui-menubar-item" )
-			.attr( "role", "presentation" );
-	},
-	_prepareItems: function(menubarTopLevelItems) {
-		// let only the first item receive focus
-		menubarTopLevelItems.slice(1).attr( "tabIndex", -1 );
-
-		this._focusable( menubarTopLevelItems );
-		this._hoverable( menubarTopLevelItems );
-		this._applyItemsEventBehavior( menubarTopLevelItems );
-	},
-	_applyItemsEventBehavior: function($topLevelItems) {
-		var menubar = this;
-		$topLevelItems.each(function() {
-			var input = $(this),
-				// TODO menu var is only used on two places, doesn't quite justify the .each
-				menu = input.next( menubar.options.menuElement ),
-				isNonmenuButton = menu.length;
-
-			if ( isNonmenuButton ) {
-				menubar._applyItemsKeyboardEvents(menubar, input, menu);
-				menubar._applyItemsMouseEvents(menubar, input, menu);
-				menubar._prepareMenuIcon(menubar, input);
-
-				input.attr( "aria-haspopup", "true" );
-			} else {
-				menubar._applyNonMenuButtonEvents(menubar, input);
-			}
-
-			menubar._prepareInputItem(menubar, input);
-		});
-	},
-	_prepareSubMenus: function(subMenus) {
-		var menubar = this;
-
-		subMenus.menu({
-			position: {
-				within: this.options.position.within
-			},
-			select: function( event, ui ) {
-				ui.item.parents( "ul.ui-menu:last" ).hide();
-				menubar._close();
-				// TODO what is this targetting? there's probably a better way to access it
-				$(event.target).prev().focus();
-				menubar._trigger( "select", event, ui );
-			},
-			menus: this.options.menuElement
-		})
-		.hide()
-		.attr({
-			"aria-hidden": "true",
-			"aria-expanded": "false"
-		});
-
+		this._focusable( this.items );
+		this._hoverable( this.items );
+		subMenus = this.items.siblings( this.options.menuElement )
+			.menu({
+				position: {
+					within: this.options.position.within
+				},
+				select: function( event, ui ) {
+					ui.item.parents( "ul.ui-menu:last" ).hide();
+					that._close();
+					// TODO what is this targetting? there's probably a better way to access it
+					$(event.target).prev().focus();
+					that._trigger( "select", event, ui );
+				},
+				menus: this.options.menuElement
+			})
+			.hide()
+			.attr({
+				"aria-hidden": "true",
+				"aria-expanded": "false"
+			});
 		this._on( subMenus, {
+
 			"keydown": function(event) {
 				var menu = $( this );
 				if ( menu.is( ":hidden" ) ) {
@@ -127,117 +84,124 @@ $.widget( "ui.menubar", {
 				}
 			}
 		});
-	},
-	_bindPluginElementEvents: function(myElement) {
-		var menubar = this;
-		menubar._on( myElement, {
+		this.items.each(function() {
+			var input = $(this),
+				// TODO menu var is only used on two places, doesn't quite justify the .each
+				menu = input.next( that.options.menuElement ),
+				mouseBehaviorCallback, keyboardBehaviorCallback;
+
+			mouseBehaviorCallback = function( event ) {
+				// ignore triggered focus event
+				if ( event.type === "focus" && !event.originalEvent ) {
+					return;
+				}
+				event.preventDefault();
+				// TODO can we simplify or extractthis check? especially the last two expressions
+				// there's a similar active[0] == menu[0] check in _open
+				if ( event.type === "click" && menu.is( ":visible" ) && this.active && this.active[0] === menu[0] ) {
+					this._close();
+					return;
+				}
+				if ( ( this.open && event.type === "mouseenter" ) || event.type === "click" || this.options.autoExpand ) {
+					if( this.options.autoExpand ) {
+						clearTimeout( this.closeTimer );
+					}
+
+					this._open( event, menu );
+				}
+			};
+
+			keyboardBehaviorCallback = function( event ) {
+				switch ( event.keyCode ) {
+				case $.ui.keyCode.SPACE:
+				case $.ui.keyCode.UP:
+				case $.ui.keyCode.DOWN:
+					this._open( event, $( this ).next() );
+					event.preventDefault();
+					break;
+				case $.ui.keyCode.LEFT:
+					this.previous( event );
+					event.preventDefault();
+					break;
+				case $.ui.keyCode.RIGHT:
+					this.next( event );
+					event.preventDefault();
+					break;
+				}
+			};
+
+			// might be a non-menu button
+			if ( menu.length ) {
+				that._on(input, {
+					"click": mouseBehaviorCallback,
+					"focus": mouseBehaviorCallback,
+					"mouseenter": mouseBehaviorCallback,
+					"keydown": keyboardBehaviorCallback
+				});
+
+				input.attr( "aria-haspopup", "true" );
+
+				// TODO review if these options (menuIcon and buttons) are a good choice, maybe they can be merged
+				if ( that.options.menuIcon ) {
+					input.addClass( "ui-state-default" ).append( "<span class='ui-button-icon-secondary ui-icon ui-icon-triangle-1-s'></span>" );
+					input.removeClass( "ui-button-text-only" ).addClass( "ui-button-text-icon-secondary" );
+				}
+			} else {
+				that._on(input, {
+					click: function(event) {
+						this._close();
+					},
+
+					mouseenter: function(event) {
+						if (this.open){
+							this._close();
+						}
+					}
+				});
+			}
+
+			input
+				.addClass( "ui-button ui-widget ui-button-text-only ui-menubar-link" )
+				.attr( "role", "menuitem" )
+				.wrapInner( "<span class='ui-button-text'></span>" );
+
+			if ( that.options.buttons ) {
+				input.removeClass( "ui-menubar-link" ).addClass( "ui-state-default" );
+			}
+		});
+		that._on( {
 			keydown: function( event ) {
-				if ( event.keyCode === $.ui.keyCode.ESCAPE && menubar.active && menubar.active.menu( "collapse", event ) !== true ) {
-					var active = menubar.active;
-					menubar.active.blur();
-					menubar._close( event );
+				if ( event.keyCode === $.ui.keyCode.ESCAPE && that.active && that.active.menu( "collapse", event ) !== true ) {
+					var active = that.active;
+					that.active.blur();
+					that._close( event );
 					active.prev().focus();
 				}
 			},
 			focusin: function( event ) {
-				clearTimeout( menubar.closeTimer );
+				clearTimeout( that.closeTimer );
 			},
 			focusout: function( event ) {
-				menubar.closeTimer = setTimeout( function() {
-					menubar._close( event );
+				that.closeTimer = setTimeout( function() {
+					that._close( event );
 				}, 150);
 			},
 			"mouseleave .ui-menubar-item": function( event ) {
-				if ( menubar.options.autoExpand ) {
-					menubar.closeTimer = setTimeout( function() {
-						menubar._close( event );
+				if ( that.options.autoExpand ) {
+					that.closeTimer = setTimeout( function() {
+						that._close( event );
 					}, 150);
 				}
 			},
 			"mouseenter .ui-menubar-item": function( event ) {
-				clearTimeout( menubar.closeTimer );
+				clearTimeout( that.closeTimer );
 			}
 		});
-	},
-	_prepareInputItem: function(menubar, $input) {
-		$input
-			.addClass( "ui-button ui-widget ui-button-text-only ui-menubar-link" )
-			.attr( "role", "menuitem" )
-			.wrapInner( "<span class='ui-button-text'></span>" );
 
-		if ( menubar.options.buttons ) {
-			$input.removeClass( "ui-menubar-link" ).addClass( "ui-state-default" );
-		}
+		// Keep track of open submenus
+		this.openSubmenus = 0;
 	},
-	_applyItemsKeyboardEvents: function(menubar, $input) {
-		var keyboardBehaviorCallback = function( event ) {
-			switch ( event.keyCode ) {
-			case $.ui.keyCode.SPACE:
-			case $.ui.keyCode.UP:
-			case $.ui.keyCode.DOWN:
-				this._open( event, $( this ).next() );
-				event.preventDefault();
-				break;
-			case $.ui.keyCode.LEFT:
-				this.previous( event );
-				event.preventDefault();
-				break;
-			case $.ui.keyCode.RIGHT:
-				this.next( event );
-				event.preventDefault();
-				break;
-			}
-		};
-		menubar._on($input, {
-			"keydown": keyboardBehaviorCallback
-		});
-	},
-	_applyItemsMouseEvents: function(menubar, $input, menu) {
-		var mouseBehaviorCallback = function( event ) {
-			// ignore triggered focus event
-			if ( event.type === "focus" && !event.originalEvent ) {
-				return;
-			}
-			event.preventDefault();
-			// TODO can we simplify or extractthis check? especially the last two expressions
-			// there's a similar active[0] == menu[0] check in _open
-			if ( event.type === "click" && menu.is( ":visible" ) && this.active && this.active[0] === menu[0] ) {
-				this._close();
-				return;
-			}
-			if ( ( this.open && event.type === "mouseenter" ) || event.type === "click" || this.options.autoExpand ) {
-				if( this.options.autoExpand ) {
-					clearTimeout( this.closeTimer );
-				}
 
-				this._open( event, menu );
-			}
-		};
-		menubar._on($input, {
-			"click": mouseBehaviorCallback,
-			"focus": mouseBehaviorCallback,
-			"mouseenter": mouseBehaviorCallback
-		});
-	},
-	_applyNonMenuButtonEvents: function(menubar, input) {
-		menubar._on(input, {
-			click: function(event) {
-				this._close();
-			},
-			mouseenter: function(event) {
-				if (this.open){
-					this._close();
-				}
-			}
-		});
-	},
-	_prepareMenuIcon: function(menubar, $input) {
-		// TODO review if these options (menuIcon and buttons) are a good choice, maybe they can be merged
-		if ( menubar.options.menuIcon ) {
-			$input.addClass( "ui-state-default" ).append( "<span class='ui-button-icon-secondary ui-icon ui-icon-triangle-1-s'></span>" );
-			$input.removeClass( "ui-button-text-only" ).addClass( "ui-button-text-icon-secondary" );
-		}
-	},
 	_destroy : function() {
 		this.menuItems
 			.removeClass( "ui-menubar-item" )
