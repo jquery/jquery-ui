5229615753f9652cf01be84e011d846de0ea5785
Mike Sherov <mike.sherov@gmail.com>
2012-12-22 15:46:57 -0500

    Dialog Tests: Fix test fails due to window measurements in FF and IE7

--- tests/unit/dialog/dialog_deprecated.js
+++ tests/unit/dialog/dialog_deprecated.js
@@ -1,26 +1,36 @@
 module("dialog (deprecated): position option with string and array");
 
-if ( !$.ui.ie ) {
-	test("position, right bottom on window w/array", function() {
-		expect( 2 );
-		var el = $('<div></div>').dialog({ position: ["right", "bottom"] }),
-			dialog = el.dialog('widget'),
-			offset = dialog.offset();
-		closeEnough(offset.left, $(window).width() - dialog.outerWidth() + $(window).scrollLeft(), 1);
-		closeEnough(offset.top, $(window).height() - dialog.outerHeight() + $(window).scrollTop(), 1);
-		el.remove();
-	});
+test( "position, right bottom on window w/array", function() {
+	expect( 2 );
 
-	test("position, right bottom on window", function() {
-		expect( 2 );
-		var el = $('<div></div>').dialog({ position: "right bottom" }),
-			dialog = el.dialog('widget'),
-			offset = dialog.offset();
-		closeEnough(offset.left, $(window).width() - dialog.outerWidth() + $(window).scrollLeft(), 1);
-		closeEnough(offset.top, $(window).height() - dialog.outerHeight() + $(window).scrollTop(), 1);
-		el.remove();
-	});
-}
+	// dialogs alter the window width and height in FF and IE7
+	// so we collect that information before creating the dialog
+	// Support: FF, IE7
+	var winWidth = $( window ).width(),
+		winHeight = $( window ).height(),
+		el = $("<div></div>").dialog({ position: [ "right", "bottom" ] }),
+		dialog = el.dialog("widget"),
+		offset = dialog.offset();
+	closeEnough( offset.left, winWidth - dialog.outerWidth() + $( window ).scrollLeft(), 1, "offset left of right bottom on window w/array" );
+	closeEnough( offset.top, winHeight - dialog.outerHeight() + $( window ).scrollTop(), 1, "offset top of right bottom on window w/array" );
+	el.remove();
+});
+
+test( "position, right bottom on window", function() {
+	expect( 2 );
+
+	// dialogs alter the window width and height in FF and IE7
+	// so we collect that information before creating the dialog
+	// Support: FF, IE7
+	var winWidth = $( window ).width(),
+		winHeight = $( window ).height(),
+		el = $("<div></div>").dialog({ position: "right bottom" }),
+		dialog = el.dialog("widget"),
+		offset = dialog.offset();
+	closeEnough( offset.left, winWidth - dialog.outerWidth() + $( window ).scrollLeft(), 1, "offset left of right bottom on window" );
+	closeEnough( offset.top, winHeight - dialog.outerHeight() + $( window ).scrollTop(), 1, "offset top of right bottom on window" );
+	el.remove();
+});
 
 test("position, offset from top left w/array", function() {
 	expect( 2 );
