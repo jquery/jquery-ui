351cd16d8bce96ff69db365350c408d21c76a989
Mike Petrovich <michael.c.petrovich@gmail.com>
2012-12-26 08:35:42 -0500

    Clip rewrite: Work in progress

--- ui/jquery.ui.effect-clip.js
+++ ui/jquery.ui.effect-clip.js
@@ -15,49 +15,92 @@
 
 $.effects.effect.clip = function( o, done ) {
 	// Create element
-	var el = $( this ),
-		props = [ "position", "top", "bottom", "left", "right", "height", "width" ],
+	var end, delta,
+		el = $( this ),
+		props = [ "display", "position", "clip" ],
 		mode = $.effects.setMode( el, o.mode || "hide" ),
 		show = mode === "show",
 		direction = o.direction || "vertical",
 		vert = direction === "vertical",
 		size = vert ? "height" : "width",
-		position = vert ? "top" : "left",
-		animation = {},
-		wrapper, animate, distance;
+		position = el.css("position"),
+		display = el.css("display"),
+		width = el.outerWidth(),
+		height = el.outerHeight(),
+		regex = /^rect\((\d+px|\d+%|auto),? (\d+px|\d+%|auto),? (\d+px|\d+%|auto),? (\d+px|\d+%|auto)\)$/,
+		start = regex.exec( el.css("clip") ) || ["", 0, width, height, 0],
+		shadow = null;
 
 	// Save & Show
 	$.effects.save( el, props );
-	el.show();
 
-	// Create Wrapper
-	wrapper = $.effects.createWrapper( el ).css({
-		overflow: "hidden"
-	});
-	animate = ( el[0].tagName === "IMG" ) ? wrapper : el;
-	distance = animate[ size ]();
+	start = {
+		top: start[ 1 ] === "auto" ? 0 : parseInt( start[ 1 ], 10 ),
+		right: start[ 2 ] === "auto" ? width : parseInt( start[ 2 ], 10 ),
+		bottom: start[ 3 ] === "auto" ? height : parseInt( start[ 3 ], 10 ),
+		left: start[ 4 ] === "auto" ? 0 : parseInt( start[ 4 ], 10 ),
+	};
 
-	// Shift
-	if ( show ) {
-		animate.css( size, 0 );
-		animate.css( position, distance / 2 );
+	end = {
+		top: (start.bottom - start.top) / 2,
+		right: (start.right - start.left) / 2,
+		bottom: (start.bottom - start.top) / 2,
+		left: (start.right - start.left) / 2
+	};
+
+	delta = {
+		top: end.top - start.top,
+		right: end.right - start.right,
+		bottom: end.bottom - start.bottom,
+		left: end.left - start.left,
+	};
+
+	if (
+		display === "inline" || display === "inline-block" || 
+		display === "inline-table" || display === "ruby" || 
+		display === "run-in" || display === "compact"
+	) {
+		display = "inline-block";
+	} else {
+		display = "block";
 	}
 
-	// Create Animation Object:
-	animation[ size ] = show ? distance : 0;
-	animation[ position ] = show ? 0 : distance / 2;
+	if (position === "static" || position === "relative") {
+		shadow = $("<div>").css({
+			display: display,
+			visibility: "hidden",
+			width: width,
+			height: height
+		}).insertAfter( el );
+	}
+
+	el.css({
+		position: "absolute",
+		left: el.offset().left,
+		top: el.offset().top
+	});
 
 	// Animate
-	animate.animate( animation, {
+	$({ a : 0 }).animate( { a : 1 }, {
 		queue: false,
 		duration: o.duration,
 		easing: o.easing,
+		step: function( now ) {
+			var top = start.top + (now * delta.top),
+				right = start.right + (now * delta.right),
+				bottom = start.bottom + (now * delta.bottom),
+				left = start.left + (now * delta.left);
+			
+			el.css( "clip", "rect(" + top + "px " + right + "px " + bottom + "px " + left + "px)" );
+		},
 		complete: function() {
 			if ( !show ) {
 				el.hide();
 			}
+			if ( shadow ) {
+				shadow.remove();
+			}
 			$.effects.restore( el, props );
-			$.effects.removeWrapper( el );
 			done();
 		}
 	});
