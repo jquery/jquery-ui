8e41313c18734b4a2f3d0b19ab60d33f520c5bcf
TJ VanToll <tj.vantoll@gmail.com>
2012-12-30 11:50:08 -0500

    Coding standards.

--- tests/unit/selectable/selectable_events.js
+++ tests/unit/selectable/selectable_events.js
@@ -38,25 +38,28 @@ test( "stop", function() {
 test( "mousedown: initial position of helper", function() {
 	expect( 2 );
 
-	var el = $( "#selectable1" ).selectable();
-	var contentToForceScroll = $( "<div></div>" ).css({
+	var el = $("#selectable1").selectable(),
+		contentToForceScroll,
+		helper;
+
+	contentToForceScroll = $("<div></div>").css({
 		height: "10000px",
 		width: "10000px"
 	});
 
-	$( "body" ).append( contentToForceScroll );
+	$("body").append( contentToForceScroll );
 	$( window ).scrollTop( 1 ).scrollLeft( 1 );
 	el.simulate( "mousedown", {
 		clientX: 10,
 		clientY: 10
 	});
 
-	var helper = $( ".ui-selectable-helper" );
+	helper = $(".ui-selectable-helper");
 	equal( helper.css( "top" ), "11px", "Scrolling should be accounted for, see #8915." );
 	equal( helper.css( "left" ), "11px", "Scrolling left should also be accounted for." );
 
 	// Cleanup
-	el.simulate( "mouseup" );
+	el.simulate("mouseup");
 	contentToForceScroll.remove();
 	$( window ).scrollTop( 0 ).scrollLeft( 0 );
 });
