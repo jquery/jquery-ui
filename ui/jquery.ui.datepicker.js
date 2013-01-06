f0dcbc8db825f07b9a550633f3f6417dc613f831
schettino72 <schettino72@gmail.com>
2013-01-06 15:28:23 +0800

    Datepicker: focus on next element after selecting date. Fixed #7765 - Datepicker: Focus not placed back into textbox after date selection

--- ui/jquery.ui.datepicker.js
+++ ui/jquery.ui.datepicker.js
@@ -1014,12 +1014,8 @@ $.extend(Datepicker.prototype, {
 		if (inst.inline){
 			this._updateDatepicker(inst);
 		} else {
+			inst.input.focus();
 			this._hideDatepicker();
-			this._lastInput = inst.input[0];
-			if (typeof(inst.input[0]) !== "object") {
-				inst.input.focus(); // restore focus
-			}
-			this._lastInput = null;
 		}
 	},
 
