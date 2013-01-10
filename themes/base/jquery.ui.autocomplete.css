58fc441ea5001b8fa4f3238b4f2ff2fd8b0950b9
Scott González <scott.gonzalez@gmail.com>
2013-01-10 08:52:20 -0500

    Update copyright year to 2013.

--- themes/base/jquery.ui.autocomplete.css
+++ themes/base/jquery.ui.autocomplete.css
@@ -2,7 +2,7 @@
  * jQuery UI Autocomplete @VERSION
  * http://jqueryui.com
  *
- * Copyright 2012 jQuery Foundation and other contributors
+ * Copyright 2013 jQuery Foundation and other contributors
  * Released under the MIT license.
  * http://jquery.org/license
  *
