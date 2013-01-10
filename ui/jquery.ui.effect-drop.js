83cbf979788f22ba3bd1668507623c0dd6b57041
Scott González <scott.gonzalez@gmail.com>
2013-01-10 08:52:20 -0500

    Update copyright year to 2013.

--- ui/jquery.ui.effect-drop.js
+++ ui/jquery.ui.effect-drop.js
@@ -2,7 +2,7 @@
  * jQuery UI Effects Drop @VERSION
  * http://jqueryui.com
  *
- * Copyright 2012 jQuery Foundation and other contributors
+ * Copyright 2013 jQuery Foundation and other contributors
  * Released under the MIT license.
  * http://jquery.org/license
  *
