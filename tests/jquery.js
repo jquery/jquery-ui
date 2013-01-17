34409e7ddd58b3ccfed8175f7febd896ffea9e82
Kris Borchers <kris.borchers@gmail.com>
2013-01-17 00:28:41 -0600

    Updating to jQuery 1.9.0

--- tests/jquery.js
+++ tests/jquery.js
@@ -18,7 +18,7 @@ for ( ; i < length; i++ ) {
 if ( version === "git" ) {
 	url = "http://code.jquery.com/jquery-git.js";
 } else {
-	url = "../../jquery-" + ( version || "1.8.3" ) + ".js";
+	url = "../../jquery-" + ( version || "1.9.0" ) + ".js";
 }
 
 document.write( "<script src='" + url + "'></script>" );
