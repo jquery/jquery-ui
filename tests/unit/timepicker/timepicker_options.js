1502235a560ac5d6cc7069226fba533c1c314193
andekande <andekande@live.de>
2013-01-17 13:54:24 +0100

    Tests clearEmpty and disabled Options

--- tests/unit/timepicker/timepicker_options.js
+++ tests/unit/timepicker/timepicker_options.js
@@ -40,4 +40,50 @@ test( "ampm", function() {
 
 });
 
+test( "clearEmpty", function () {
+    expect( 4 );
+
+    var input = $( "#timepicker1" ).val(" 1:30:00 PM").timepicker({
+        clearEmpty: true
+    });
+
+    equal( input.val(), " 1:30:00 PM", "Sanity Check" );
+
+    input.timepicker( "value", "" );
+
+    equal( input.val(), "", "Deleted value by setting empty string" );
+
+    input.timepicker( "value", "1:30:00" );
+
+    equal( input.val(), " 1:30:00 AM", "Restored value" );
+
+    TestHelpers.downup( input, $.ui.keyCode.BACKSPACE );
+    TestHelpers.downup( input, $.ui.keyCode.BACKSPACE );
+    TestHelpers.downup( input, $.ui.keyCode.BACKSPACE );
+    TestHelpers.downup( input, $.ui.keyCode.BACKSPACE );
+    TestHelpers.downup( input, $.ui.keyCode.BACKSPACE );
+
+    equal( input.val(), "__:__:__ __", "Deleted value by using Backspace Key, mask not yet refreshed (will onBlur)" );
+
+});
+
+test( "disabled", function () {
+    expect( 3 );
+
+    var input = $("#timepicker1").val(" 1:30:00 PM").timepicker({
+        disabled: true
+    });
+
+    equal( input.val(), " 1:30:00 PM", "Sanity Check" );
+
+    input.timepicker( "value", "14:20:00" );
+
+    equal( input.val(), " 2:20:00 PM", "Can set value when disabled" );
+
+    TestHelpers.downup( input, $.ui.keyCode.UP );
+
+    equal( input.val(), " 2:20:00 PM", "After up keypress value does not change" );
+
+});
+
 }( jQuery ) );
