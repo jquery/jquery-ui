51f68d7abfe021d305218b9278a572df53144f85
Thomas Jaggi <thomas.jaggi@gmail.com>
2013-01-28 15:55:37 +0100

    Revert "Add rounded corners to slider range area"
    This reverts commit d84d985582d3f9b4a7f89332a3f8b508c3ae8fc5.

--- ui/jquery.ui.slider.js
+++ ui/jquery.ui.slider.js
@@ -81,8 +81,7 @@ $.widget( "ui.slider", $.ui.mouse, {
 				// note: this isn't the most fittingly semantic framework class for this element,
 				// but worked best visually with a variety of themes
 				" ui-widget-header" +
-				( ( o.range === "min" || o.range === "max" ) ? " ui-slider-range-" + o.range : "" ) +
-				" ui-corner-all");
+				( ( o.range === "min" || o.range === "max" ) ? " ui-slider-range-" + o.range : "" ) );
 		}
 
 		handleCount = ( o.values && o.values.length ) || 1;
