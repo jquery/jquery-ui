26da178c376995228b0b3d128954c97e21f9aeac
Thomas Jaggi <thomas.jaggi@gmail.com>
2013-01-28 15:31:09 +0100

    Fix formatting

--- ui/jquery.ui.slider.js
+++ ui/jquery.ui.slider.js
@@ -80,9 +80,8 @@ $.widget( "ui.slider", $.ui.mouse, {
 				.addClass( "ui-slider-range" +
 				// note: this isn't the most fittingly semantic framework class for this element,
 				// but worked best visually with a variety of themes
-				" ui-widget-header" +
-				( ( o.range === "min" || o.range === "max" ) ? " ui-slider-range-" + o.range : "" ) +
-				" ui-corner-all");
+				" ui-widget-header ui-corner-all" +
+				( ( o.range === "min" || o.range === "max" ) ? " ui-slider-range-" + o.range : "" ) );
 		}
 
 		handleCount = ( o.values && o.values.length ) || 1;
