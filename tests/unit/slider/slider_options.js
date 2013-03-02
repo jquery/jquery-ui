6d3a1e1fe8cc21f385456ea26075f3909136a589
Scott González <scott.gonzalez@gmail.com>
2013-03-02 15:31:38 -0500

    Slider tests: Use ui-slider not uiSlider for data key.

--- tests/unit/slider/slider_options.js
+++ tests/unit/slider/slider_options.js
@@ -180,13 +180,13 @@ test( "values", function() {
 		document.createElement( "div" ),
 		document.createElement( "div" )
 	]).slider({
-		range:  true,
+		range: true,
 		values: [ 25, 75 ]
 	});
 
 	notStrictEqual(
-		ranges.eq( 0 ).data( "uiSlider" ).options.values,
-		ranges.eq( 1 ).data( "uiSlider" ).options.values,
+		ranges.eq( 0 ).data( "ui-slider" ).options.values,
+		ranges.eq( 1 ).data( "ui-slider" ).options.values,
 		"multiple range sliders should not have a reference to the same options.values array"
 	);
 
