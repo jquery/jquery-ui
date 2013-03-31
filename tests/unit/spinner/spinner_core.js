efe8da8e6e9b50547fe9a632b267c7f8479b6686
Mike Sherov <mike.sherov@gmail.com>
2013-03-31 12:55:57 -0400

    Spinner Tests: Fix IE test failures by accounting for the async nature of focus/blur.

--- tests/unit/spinner/spinner_core.js
+++ tests/unit/spinner/spinner_core.js
@@ -94,6 +94,9 @@ asyncTest( "blur input while spinning with UP", function() {
 	function step2() {
 		value = element.val();
 		ok( value > 11, "repeating while key is down" );
+		setTimeout( function() {
+			value = element.val();
+		});
 		element[0].blur();
 		setTimeout( step3, 250 );
 	}
