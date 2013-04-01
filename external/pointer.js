227ec13e75cecb8311702eb62fa7d455abd1c7a6
Kris Borchers <kris.borchers@gmail.com>
2013-03-31 23:20:46 -0500

    Pointer: More specific event property transfer and cleanup

--- external/pointer.js
+++ external/pointer.js
@@ -5,42 +5,71 @@ var POINTER_TYPE_UNAVAILABLE = "1",
     POINTER_TYPE_MOUSE = "4";
 
 function processEvent( event, pointerType ) {
-    var prop,
-        orig = event;
+    var propLength,
+        i = 0,
+        orig = event,
+        mouseProps = $.event.mouseHooks.props;
 
-    event = $.Event( pointerType );
+    event = $.Event( pointerType, {
+        bubbles: true,
+        cancelable: true,
+        pointerId: 0,
+        width: 0,
+        height: 0,
+        pressure: 0,
+        tiltX: 0,
+        tiltY: 0,
+        pointerType: "",
+        // TODO: properly determine isPrimary
+        isPrimary: true
+    });
 
-    for ( prop in orig ) {
-        if ( !( prop in event ) ) {
-            event[ prop ] = orig[ prop ];
-        }
+    for ( propLength = mouseProps.length; i < propLength; i++ ) {
+        event[ mouseProps[ i ] ] = orig[ mouseProps[ i ] ];
     }
 
-    event.pageX = event.pageX || orig.originalEvent.pageX;
-    event.pageY = event.pageY || orig.originalEvent.pageY;
-
-    // TODO - Actually determine if primary
-    event.isPrimary = true;
-
-    if ( orig.type.indexOf("mouse") != -1 ) {
+    if ( orig.type.indexOf("mouse") !== -1 ) {
         event.pointerId = 1;
         event.pointerType = POINTER_TYPE_MOUSE;
         // TODO: Don't assume left click
         event.button = 0;
         event.buttons = 1;
-    } else if ( orig.type.indexOf("touch") != -1 ) {
-        event.pointerId = orig.identifier || 2;
+    } else if ( orig.type.indexOf("touch") !== -1 ) {
+        event.pointerId = orig.pointerId;
         event.pointerType = POINTER_TYPE_TOUCH;
         event.button = 0;
         event.buttons = 1;
-    } else if ( orig.type.indexOf("Pointer") != -1 ) {
+        event.originalEvent = orig;
+    } else if ( orig.type.indexOf("Pointer") !== -1 ) {
         event.pointerId = orig.originalEvent.pointerId;
         event.pointerType = orig.originalEvent.pointerType || POINTER_TYPE_UNAVAILABLE;
         event.button = orig.originalEvent.button;
         event.buttons = orig.originalEvent.buttons;
+        event.width = orig.originalEvent.width;
+        event.height = orig.originalEvent.height;
+        event.pressure = orig.originalEvent.pressure;
+        event.tiltX = orig.originalEvent.tiltX;
+        event.tiltY = orig.originalEvent.tiltY;
     }
 
     return event;
+}
+
+function pointerEventHandler( event, type ) {
+    var touch;
+
+    if ( event.type.indexOf("touch") !== -1 ) {
+        touch = event.originalEvent.changedTouches[ 0 ];
+        event.pageX = touch.pageX;
+        event.pageY = touch.pageY;
+        event.screenX = touch.screenX;
+        event.screenY = touch.screenY;
+        event.clientX = touch.clientX;
+        event.clientY = touch.clientY;
+        event.pointerId = touch.identifier;
+    }
+
+    $( event.target ).trigger( processEvent( event, type ) );
 }
 
 $.event.special.pointerdown = {
@@ -51,8 +80,7 @@ $.event.special.pointerdown = {
         $( this ).off( "mousedown touchstart MSPointerDown", $.event.special.pointerdown.handler );
     },
     handler: function( event ) {
-        event = processEvent( event, "pointerdown" );
-        $( event.target ).trigger( event );
+        pointerEventHandler( event, "pointerdown" );
     }
 };
 
@@ -64,8 +92,7 @@ $.event.special.pointerup = {
         $( this ).off( "mouseup touchend MSPointerUp", $.event.special.pointerup.handler );
     },
     handler: function( event ) {
-        event = processEvent( event, "pointerup" );
-        $( event.target ).trigger( event );
+        pointerEventHandler( event, "pointerup" );
     }
 };
 
@@ -77,8 +104,7 @@ $.event.special.pointermove = {
         $( this ).off( "mousemove touchmove MSPointerMove", $.event.special.pointermove.handler );
     },
     handler: function( event ) {
-        event = processEvent( event, "pointermove" );
-        $( event.target ).trigger( event );
+        pointerEventHandler( event, "pointermove" );
     }
 };
 })( jQuery );
