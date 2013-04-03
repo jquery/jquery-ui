f15731072bd76dd5d208ec71d2077c745f4180d2
Scott González <scott.gonzalez@gmail.com>
2013-04-03 16:06:15 -0400

    Datepicker: Fixed date format for Serbian locales. Fixes #7347 - Datepicker: Wrong date format for Serbian localization. (cherry picked from commit 504b100a1a9213186968c654dd915b92bb9ee15b)

--- ui/i18n/jquery.ui.datepicker-sr-SR.js
+++ ui/i18n/jquery.ui.datepicker-sr-SR.js
@@ -14,7 +14,7 @@ jQuery(function($){
 		dayNamesShort: ['Ned','Pon','Uto','Sre','Čet','Pet','Sub'],
 		dayNamesMin: ['Ne','Po','Ut','Sr','Če','Pe','Su'],
 		weekHeader: 'Sed',
-		dateFormat: 'dd/mm/yy',
+		dateFormat: 'dd.mm.yy',
 		firstDay: 1,
 		isRTL: false,
 		showMonthAfterYear: false,
