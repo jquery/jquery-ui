86548c9f0b7bb1e5134957404a4c9d87b2747b32
David Petersen <public@petersendidit.com>
2013-04-23 15:10:29 -0500

    Accordion: use the _classes method.
    Fixes #7053

--- tests/unit/accordion/accordion_common.js
+++ tests/unit/accordion/accordion_common.js
@@ -2,6 +2,16 @@ TestHelpers.commonWidgetTests( "accordion", {
 	defaults: {
 		active: 0,
 		animate: {},
+		classes: {
+			"ui-accordion": null,
+			"ui-accordion-header": "ui-corner-top",
+			"ui-accordion-header-active": null,
+			"ui-accordion-header-collapsed": "ui-corner-all",
+			"ui-accordion-content": "ui-corner-bottom",
+			"ui-accordion-content-active": null,
+			"ui-accordion-header-icon": null,
+			"ui-accordion-icons": null
+		},
 		collapsible: false,
 		disabled: false,
 		event: "click",
