5045c1c46b811a0c42f90c414924872db84f4e5c
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-06-06 15:11:26 +0200

    Datepicker Rewrite: Inline popup logic. Some bug fixes. Add event handlers to 'Today' and 'Done' buttons.

--- datepicker-rewrite/picker.js
+++ datepicker-rewrite/picker.js
@@ -5,16 +5,26 @@
  */
 (function( $, undefined ) {
 
-var idIncrement = 0;
+var idIncrement = 0,
+	suppressExpandOnFocus = false;
+
 $.widget( "ui.datepicker", {
 	options: {
+		appendTo: null,
 		eachDay: $.noop,
 		tmpl: "#ui-datepicker-tmpl",
 		gridTmpl: "#ui-datepicker-grid-tmpl",
 		position: {
 			my: "left top",
 			at: "left bottom"
-		}
+		},
+		show: true,
+		hide: true,
+
+		// callbacks
+		close: null,
+		open: null,
+		select: null
 	},
 	_create: function() {
 		var that = this;
@@ -23,108 +33,223 @@ $.widget( "ui.datepicker", {
 		this.id = "ui-datepicker-" + idIncrement;
 		idIncrement++;
 		if ( this.element.is( "input" ) ) {
-			this.picker = $( "<div>" ).insertAfter( this.element ).popup({
-				managed: true,
-				expandOnFocus: true,
-				open: function( event, ui ) {
-					that.open( event );
-				},
-				focusPopup: function( event, ui ) {
-					that.grid.focus( 1 );
-				}
-			});
+			this._createPicker();
 		} else {
 			this.inline = true;
 			this.picker = this.element;
 		}
-		this.picker.delegate( ".ui-datepicker-prev", "click", function( event ) {
-			event.preventDefault();
-			that.date.adjust( "M", -1 );
-			that.refresh();
-		});
-		this.picker.delegate( ".ui-datepicker-next", "click", function( event ) {
-			event.preventDefault();
-			that.date.adjust( "M", 1 );
-			that.refresh();
-		});
-		this.picker.delegate( ".ui-datepicker-calendar a", "mousedown", function( event ) {
-			event.preventDefault();
-			// TODO exclude clicks on lead days or handle them correctly
-			// TODO store/read more then just date, also required for multi month picker
-			that.select( event, $( this ).data( "timestamp" ) );
-			that.grid.focus( 1 );
+		this._on( this.picker, {
+			"click .ui-datepicker-prev": function( event ) {
+				event.preventDefault();
+				this.date.adjust( "M", -1 );
+				this.refresh();
+			},
+			"click .ui-datepicker-next": function( event ) {
+				event.preventDefault();
+				this.date.adjust( "M", 1 );
+				this.refresh();
+			},
+			"click .ui-datepicker-current": function( event ) {
+				event.preventDefault();
+				this.select( event, new Date().getTime() );
+			},
+			"click .ui-datepicker-close": function( event ) {
+				event.preventDefault();
+				this.close( event );
+			},
+			"mousedown .ui-datepicker-calendar a": function( event ) {
+				event.preventDefault();
+				// TODO exclude clicks on lead days or handle them correctly
+				// TODO store/read more then just date, also required for multi month picker
+				this.select( event, $( event.currentTarget ).data( "timestamp" ) );
+				if ( this.inline ) {
+					this.grid.focus( 1 );
+				}
+			},
+			"keydown .ui-datepicker-calendar": "_handleKeydown"
 		});
 
+		// TODO use hoverable (no delegation support)? convert to _on?
 		this.picker.delegate( ".ui-datepicker-header a, .ui-datepicker-calendar a", "mouseenter.datepicker mouseleave.datepicker", function() {
 			$( this ).toggleClass( "ui-state-hover" );
 		});
 
-		this.picker.delegate( ".ui-datepicker-calendar", "keydown.datepicker", function(event) {
-			if (jQuery.inArray(event.keyCode, [ 13, 33, 34, 35, 36, 37, 38, 39, 40]) == -1) {
-				//only interested navigation keys
+		this._createTmpl();
+	},
+	_handleKeydown: function( event ) {
+		if ( jQuery.inArray( event.keyCode, [ 13, 33, 34, 35, 36, 37, 38, 39, 40 ] ) === -1 ) {
+			//only interested navigation keys
+			return;
+		}
+		event.preventDefault();
+
+		var activeCell = $( "#" + this.grid.attr( "aria-activedescendant" ) ),
+			oldMonth = this.date.month(),
+			oldYear = this.date.year();
+
+		switch ( event.keyCode ) {
+			case $.ui.keyCode.ENTER:
+				activeCell.children( "a:first" ).mousedown();
+				return;
+			case $.ui.keyCode.PAGE_UP:
+				this.date.adjust( event.altKey ? "Y" : "M", 1 );
+				break;
+			case $.ui.keyCode.PAGE_DOWN:
+				this.date.adjust( event.altKey ? "Y" : "M", -1 );
+				break;
+			case $.ui.keyCode.END:
+				this.date.setDay( this.date.daysInMonth() );
+				break;
+			case $.ui.keyCode.HOME:
+				this.date.setDay( 1 );
+				break;
+			case $.ui.keyCode.LEFT:
+				this.date.adjust( "D", -1 );
+				break;
+			case $.ui.keyCode.UP:
+				this.date.adjust( "D", -7 );
+				break;
+			case $.ui.keyCode.RIGHT:
+				this.date.adjust( "D", 1 );
+				break;
+			case $.ui.keyCode.DOWN:
+				this.date.adjust( "D", 7 );
+				break;
+			default:
+				return;
+		}
+
+		if ( this.date.month() != oldMonth || this.date.year() != oldYear ) {
+			this.refresh();
+			this.grid.focus( 1 );
+		}
+		else {
+			var newId = this.id + "-" + this.date.day(),
+				newCell = $("#" + newId);
+
+			if ( !newCell.length ) {
 				return;
 			}
-			event.stopPropagation();
-			event.preventDefault();
+			this.grid.attr("aria-activedescendant", newId);
 
-			var activeCell = $( "#" + that.grid.attr( "aria-activedescendant" ) ),
-				oldMonth = that.date.month(),
-				oldYear = that.date.year();
+			activeCell.children("a").removeClass("ui-state-focus");
+			newCell.children("a").addClass("ui-state-focus");
+		}
+	},
+	_createPicker: function() {
+		this.picker = $( "<div>" )
+			.addClass( "ui-front" )
+			// TODO add a ui-datepicker-popup class, move position:absolte to that
+			.css( "position", "absolute" )
+			.uniqueId()
+			.hide();
+		this._setHiddenPicker();
+		// implement proper ui-front insert logic
+		this.picker.appendTo( this._appendTo() )
 
-			switch ( event.keyCode ) {
-				case $.ui.keyCode.ENTER:
-					activeCell.children( "a:first" ).mousedown();
-					return;
-					break;
-				case $.ui.keyCode.PAGE_UP:
-					that.date.adjust( event.altKey ? "Y" : "M", 1 );
-					break;
-				case $.ui.keyCode.PAGE_DOWN:
-					that.date.adjust( event.altKey ? "Y" : "M", -1 );
-					break;
-				case $.ui.keyCode.END:
-					that.date.setDay( that.date.daysInMonth() );
-					break;
-				case $.ui.keyCode.HOME:
-					that.date.setDay( 1 );
-					break;
-				case $.ui.keyCode.LEFT:
-					that.date.adjust( "D", -1 );
-					break;
-				case $.ui.keyCode.UP:
-					that.date.adjust( "D", -7 );
-					break;
-				case $.ui.keyCode.RIGHT:
-					that.date.adjust( "D", 1 );
-					break;
-				case $.ui.keyCode.DOWN:
-					that.date.adjust( "D", 7 );
-					break;
-				default:
-					return;
-			}
+		this.element
+			.attr( "aria-haspopup", "true" )
+			.attr( "aria-owns", this.picker.attr( "id" ) );
 
-			if ( that.date.month() != oldMonth || that.date.year() != oldYear ) {
-				that.refresh();
-				that.grid.focus( 1 );
-			}
-			else {
-				var newId = that.id + "-" + that.date.day(),
-					newCell = $("#" + newId);
-
-				if ( !newCell.length ) {
+		this._on({
+			keydown: function( event ) {
+				switch ( event.keyCode ) {
+					case $.ui.keyCode.TAB:
+						// Waiting for close() will make popup hide too late, which breaks tab key behavior
+						this.picker.hide();
+						this.close( event );
+						break;
+					case $.ui.keyCode.ESCAPE:
+						if ( this.isOpen ) {
+							this.close( event );
+						}
+						break;
+					case $.ui.keyCode.DOWN:
+					case $.ui.keyCode.UP:
+						clearTimeout( this.closeTimer );
+						this._delay(function() {
+							this.open( event );
+							this.grid.focus( 1 );
+						}, 1);
+						break;
+				}
+			},
+			mousedown: function( event ) {
+				if (this.isOpen) {
+					suppressExpandOnFocus = true;
+					this.close();
 					return;
 				}
-				that.grid.attr("aria-activedescendant", newId);
-
-				activeCell.children("a").removeClass("ui-state-focus");
-				newCell.children("a").addClass("ui-state-focus");
+				this.open( event );
+				clearTimeout( this.closeTimer );
+			},
+			focus: function( event ) {
+				if ( !suppressExpandOnFocus ) {
+					this._delay( function() {
+						if ( !this.isOpen ) {
+							this.open( event );
+						}
+					}, 1);
+				}
+				this._delay( function() {
+					suppressExpandOnFocus = false;
+				}, 100);
+			},
+			blur: function( event ) {
+				suppressExpandOnFocus = false;
 			}
 		});
 
-		this.createTmpl();
+		this._on( this.picker, {
+			focusout: function( event ) {
+				// use a timer to allow click to clear it and letting that
+				// handle the closing instead of opening again
+				this.closeTimer = this._delay( function() {
+					this.close( event );
+				}, 150);
+			},
+			focusin: function( event ) {
+				clearTimeout( this.closeTimer );
+			},
+			mouseup: function( event ) {
+				clearTimeout( this.closeTimer );
+			},
+			keyup: function( event ) {
+				if ( event.keyCode === $.ui.keyCode.ESCAPE && this.picker.is( ":visible" ) ) {
+					this.close( event );
+					this._focusTrigger();
+				}
+			}
+		});
+
+		this._on( this.document, {
+			click: function( event ) {
+				if ( this.isOpen && !$( event.target ).closest( this.element.add( this.picker ) ).length ) {
+					this.close( event );
+				}
+			}
+		});
 	},
-	createTmpl: function() {
+	_appendTo: function() {
+		var element = this.options.appendTo;
+
+		if ( element ) {
+			element = element.jquery || element.nodeType ?
+				$( element ) :
+				this.document.find( element ).eq( 0 );
+		}
+
+		if ( !element ) {
+			element = this.element.closest( ".ui-front" );
+		}
+
+		if ( !element.length ) {
+			element = this.document[0].body;
+		}
+
+		return element;
+	},
+	_createTmpl: function() {
 		this.date.refresh();
 
 		$( this.options.tmpl ).tmpl( {
@@ -143,6 +268,10 @@ $.widget( "ui.datepicker", {
 		// against display:none in datepicker.css
 		this.picker.find( ".ui-datepicker" ).css( "display", "block" );
 		this.grid = this.picker.find( ".ui-datepicker-calendar" );
+	},
+	_focusTrigger: function( event ) {
+		suppressExpandOnFocus = true;
+		this.element.focus();
 	},
 	refresh: function() {
 		//determine which day gridcell to focus after refresh
@@ -164,23 +293,59 @@ $.widget( "ui.datepicker", {
 		this.grid = newGrid;
 	},
 	open: function( event ) {
-		if ( !this.inline ) {
-			this.date = $.date( this.element.val() );
-			this.date.eachDay = this.options.eachDay;
-			this.date.select();
-			this.refresh();
+		if ( this.inline || this.isOpen ) {
+			return;
 		}
+
+		this.date = $.date( this.element.val() );
+		this.date.eachDay = this.options.eachDay;
+		this.date.select();
+		this.refresh();
+
+		var position = $.extend( {}, {
+			of: this.element
+		}, this.options.position );
+
+		this.picker
+			.attr( "aria-hidden", "false" )
+			.attr( "aria-expanded", "true" )
+			.show()
+			.position( position )
+			.hide();
+
+		this._show( this.picker, this.options.show );
+
+		// take trigger out of tab order to allow shift-tab to skip trigger
+		this.element.attr( "tabindex", -1 );
+		this.isOpen = true;
+
+		this._trigger( "open", event );
 	},
 	close: function( event ) {
-		this.picker.popup( "close" );
+		if ( this.inline ) {
+			return;
+		}
+
+		this._setHiddenPicker();
+		this._hide( this.picker, this.options.hide );
+
+		this.element.attr( "tabindex" , 0 );
+
+		this.isOpen = false;
+		this._trigger( "close", event );
+	},
+	_setHiddenPicker: function() {
+		this.picker
+			.attr( "aria-hidden", "true" )
+			.attr( "aria-expanded", "false" );
 	},
 	select: function( event, time ) {
 		this.date.setTime( time ).select();
 		this.refresh();
 		if ( !this.inline ) {
 			this.element.val( this.date.format() );
-			this.picker.popup( "focusTrigger", event );
 			this.close();
+			this._focusTrigger();
 		}
 		this._trigger( "select", event, {
 			date: this.date.format()
@@ -189,6 +354,9 @@ $.widget( "ui.datepicker", {
 	_destroy: function() {
 		if ( !this.inline ) {
 			this.picker.remove();
+			this.element
+				.removeAttr( "aria-haspopup" )
+				.removeAttr( "aria-owns" );
 		}
 	},
 	widget: function() {
