47a84f57e55db40956afe10cf1e293df9d8d6269
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-07-06 17:32:28 +0300

    Widget factory: Make the methods enable() and disable() act via _setOptions() instead of _setOption().

--- ui/jquery.ui.widget.js
+++ ui/jquery.ui.widget.js
@@ -358,10 +358,10 @@ $.Widget.prototype = {
 	},
 
 	enable: function() {
-		return this._setOption( "disabled", false );
+		return this._setOptions( { "disabled": false } );
 	},
 	disable: function() {
-		return this._setOption( "disabled", true );
+		return this._setOptions( { "disabled": true } );
 	},
 
 	_on: function( suppressDisabledCheck, element, handlers ) {
