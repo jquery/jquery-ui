d0b9f01ede6e19daa565aaf7cbef5338498cc726
Felix Nagel <info@felixnagel.com>
2013-09-07 17:06:32 +0200

    updated: copyright year

--- ui/jquery.ui.selectmenu.js
+++ ui/jquery.ui.selectmenu.js
@@ -2,7 +2,7 @@
  * jQuery UI Selectmenu version 1.4.0pre
  *
  * Copyright (c) 2009-2010 filament group, http://filamentgroup.com
- * Copyright (c) 2010-2012 Felix Nagel, http://www.felixnagel.com
+ * Copyright (c) 2010-2013 Felix Nagel, http://www.felixnagel.com
  * Licensed under the MIT (MIT-LICENSE.txt)
  *
  * https://github.com/fnagel/jquery-ui/wiki/Selectmenu
