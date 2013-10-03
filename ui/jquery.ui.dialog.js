ed09ee5b75064c7e6aa221e834cd5843d7c16260
Scott González <scott.gonzalez@gmail.com>
2013-10-03 12:09:50 -0400

    Dialog: Added note explaining that _on() is safe to use for shared handlers because of stacking.

--- ui/jquery.ui.dialog.js
+++ ui/jquery.ui.dialog.js
@@ -757,6 +757,8 @@ $.widget( "ui.dialog", {
 		if ( !this.document.data( "ui-dialog-overlays" ) ) {
 
 			// Prevent use of anchors and inputs
+			// Using _on() for an event handler shared across many instances is
+			// safe because the dialogs stack and must be closed in reverse order
 			this._on( this.document, {
 				focusin: function( event ) {
 					if ( isOpening ) {
