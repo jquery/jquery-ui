32b716d0f1155eec4d0827fcffb27bf1050305d5
Stephen Mathieson <smath23@gmail.com>
2013-10-08 09:45:18 -0400

    [tooltip] prevent adding the same id twice in the `aria-describedby` attribute
    fixes an edge case where an element is already described by the current id

--- ui/jquery.ui.tooltip.js
+++ ui/jquery.ui.tooltip.js
@@ -19,7 +19,9 @@ var increments = 0;
 
 function addDescribedBy( elem, id ) {
 	var describedby = (elem.attr( "aria-describedby" ) || "").split( /\s+/ );
-	describedby.push( id );
+	if ( $.inArray(id, describedby ) === -1 ) {
+		describedby.push( id );
+	}
 	elem
 		.data( "ui-tooltip-id", id )
 		.attr( "aria-describedby", $.trim( describedby.join( " " ) ) );
