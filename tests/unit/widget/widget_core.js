6cdebe84f8fb20374bb9dd32b62cba1b7f8dfd13
Jyoti Deka <dekajp@gmail.com>
2013-10-14 23:43:06 -0400

    Widget: option-method should work as getter only when argument length is 1. Fixes #9601 - Widget: calling _setOption with undefined as 3rd argument makes it a getter (cherry picked from commit ecde7cd41a59e9c3ff07f56baeeaec2147cca779)

--- tests/unit/widget/widget_core.js
+++ tests/unit/widget/widget_core.js
@@ -535,7 +535,7 @@ test( ".option() - delegate to ._setOptions()", function() {
 });
 
 test( ".option() - delegate to ._setOption()", function() {
-	expect( 2 );
+	expect( 3 );
 	var div,
 		calls = [];
 	$.widget( "ui.testWidget", {
@@ -555,6 +555,11 @@ test( ".option() - delegate to ._setOption()", function() {
 		"_setOption called for single option" );
 
 	calls = [];
+	div.testWidget( "option", "foo", undefined );
+	deepEqual( calls, [{ key: "foo", val: undefined }],
+		"_setOption called for single option where value is undefined" );
+
+	calls = [];
 	div.testWidget( "option", {
 		bar: "qux",
 		quux: "quuux"
@@ -566,9 +571,9 @@ test( ".option() - delegate to ._setOption()", function() {
 });
 
 test( ".option() - deep option setter", function() {
-	expect( 6 );
+	expect( 9 );
 	$.widget( "ui.testWidget", {} );
-	var div = $( "<div>" ).testWidget();
+	var result, div = $( "<div>" ).testWidget();
 	function deepOption( from, to, msg ) {
 		div.data( "ui-testWidget" ).options.foo = from;
 		$.ui.testWidget.prototype._setOption = function( key, value ) {
@@ -579,6 +584,12 @@ test( ".option() - deep option setter", function() {
 
 	deepOption( { bar: "baz" }, { bar: "qux" }, "one deep" );
 	div.testWidget( "option", "foo.bar", "qux" );
+
+	deepOption( { bar: "baz" }, { bar: undefined }, "one deep - value = undefined" );
+
+	result = div.testWidget( "option", "foo.bar", undefined );
+
+	deepEqual ( result, div, "option should return widget on successful set operation" );
 
 	deepOption( null, { bar: "baz" }, "null" );
 	div.testWidget( "option", "foo.bar", "baz" );
