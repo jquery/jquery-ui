369f89a81e0b609aca89affdeed8d56f78d9d530
Andrei Picus <office.nightcrawler@gmail.com>
2013-10-29 15:48:51 +0200

    Droppable: update ddmanager when scope changes through setOption. Fixed #9287 - droppable: Scope option cannot be changed after initialization.

--- tests/unit/droppable/droppable_options.js
+++ tests/unit/droppable/droppable_options.js
@@ -35,16 +35,54 @@ test("{ addClasses: false }", function() {
 	ok(!el.is(".ui-droppable"), "'ui-droppable' class not added");
 	el.droppable("destroy");
 });
+
+test( "scope", function() {
+	expect( 4 );
+	var droppableOffset, draggableOffset, oldDraggableOffset, dx, dy,
+			draggable1 = $("<div></div>").appendTo( "#qunit-fixture" ).draggable({ revert: "invalid" }),
+			draggable2 = $("<div></div>").appendTo( "#qunit-fixture" ).droppable(),
+			droppable = $("<div></div>").appendTo( "#qunit-fixture" ).droppable(),
+			newScope = "test";
+
+	draggable1.draggable( "option", "scope", newScope );
+	droppable.droppable( "option", "scope", newScope );
+
+	// Test that droppable accepts draggable with new scope.
+	droppableOffset = droppable.offset();
+	draggableOffset = draggable1.offset();
+	dx = droppableOffset.left - draggableOffset.left;
+	dy = droppableOffset.top - draggableOffset.top;
+
+	draggable1.simulate( "drag", {
+		dx: dx,
+		dy: dy
+	});
+
+	draggableOffset = draggable1.offset();
+	equal( draggableOffset.left, droppableOffset.left );
+	equal( draggableOffset.top, droppableOffset.top );
+
+	// Test that droppable doesn't accept draggable with old scope.
+	draggableOffset = draggable2.offset();
+	dx = droppableOffset.left - draggableOffset.left;
+	dy = droppableOffset.top - draggableOffset.top;
+	oldDraggableOffset = draggableOffset;
+
+	draggable2.simulate( "drag", {
+		dx: dx,
+		dy: dy
+	});
+
+	draggableOffset = draggable2.offset();
+	equal( draggableOffset.left, oldDraggableOffset.left );
+	equal( draggableOffset.top, oldDraggableOffset.top );
+});
 /*
 test("greedy", function() {
 	ok(false, 'missing test - untested code is broken code');
 });
 
 test("hoverClass", function() {
-	ok(false, 'missing test - untested code is broken code');
-});
-
-test("scope", function() {
 	ok(false, 'missing test - untested code is broken code');
 });
 
