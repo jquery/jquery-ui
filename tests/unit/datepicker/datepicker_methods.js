c68d3e538f5770580de6b8298b2f6b36e30e239d
TJ VanToll <tj.vantoll@gmail.com>
2013-11-06 08:54:19 -0500

    Datepicker: Updating enable()/disable() tests.

--- tests/unit/datepicker/datepicker_methods.js
+++ tests/unit/datepicker/datepicker_methods.js
@@ -1,6 +1,3 @@
-/*
- * datepicker_methods.js
- */
 (function( $ ) {
 
 module( "datepicker: methods" );
@@ -27,63 +24,30 @@ test( "destroy", function() {
 	// ok( inl.children().length === 0, "inline picker no longer has children" );
 });
 
-test("enableDisable", function() {
-	expect( 33 );
-	var inl, dp,
-		inp = TestHelpers.datepicker.init("#inp");
-	ok(!inp.datepicker("isDisabled"), "Enable/disable - initially marked as enabled");
-	ok(!inp[0].disabled, "Enable/disable - field initially enabled");
-	inp.datepicker("disable");
-	ok(inp.datepicker("isDisabled"), "Enable/disable - now marked as disabled");
-	ok(inp[0].disabled, "Enable/disable - field now disabled");
-	inp.datepicker("enable");
-	ok(!inp.datepicker("isDisabled"), "Enable/disable - now marked as enabled");
-	ok(!inp[0].disabled, "Enable/disable - field now enabled");
-	inp.datepicker("destroy");
-	// With a button
-	inp = TestHelpers.datepicker.init("#inp", {showOn: "button"});
-	ok(!inp.datepicker("isDisabled"), "Enable/disable button - initially marked as enabled");
-	ok(!inp[0].disabled, "Enable/disable button - field initially enabled");
-	ok(!inp.next("button")[0].disabled, "Enable/disable button - button initially enabled");
-	inp.datepicker("disable");
-	ok(inp.datepicker("isDisabled"), "Enable/disable button - now marked as disabled");
-	ok(inp[0].disabled, "Enable/disable button - field now disabled");
-	ok(inp.next("button")[0].disabled, "Enable/disable button - button now disabled");
-	inp.datepicker("enable");
-	ok(!inp.datepicker("isDisabled"), "Enable/disable button - now marked as enabled");
-	ok(!inp[0].disabled, "Enable/disable button - field now enabled");
-	ok(!inp.next("button")[0].disabled, "Enable/disable button - button now enabled");
-	inp.datepicker("destroy");
-	// With an image button
-	inp = TestHelpers.datepicker.init("#inp", {showOn: "button", buttonImageOnly: true,
-		buttonImage: "images/calendar.gif"});
-	ok(!inp.datepicker("isDisabled"), "Enable/disable image - initially marked as enabled");
-	ok(!inp[0].disabled, "Enable/disable image - field initially enabled");
-	ok(parseFloat(inp.next("img").css("opacity")) === 1, "Enable/disable image - image initially enabled");
-	inp.datepicker("disable");
-	ok(inp.datepicker("isDisabled"), "Enable/disable image - now marked as disabled");
-	ok(inp[0].disabled, "Enable/disable image - field now disabled");
-	ok(parseFloat(inp.next("img").css("opacity")) !== 1, "Enable/disable image - image now disabled");
-	inp.datepicker("enable");
-	ok(!inp.datepicker("isDisabled"), "Enable/disable image - now marked as enabled");
-	ok(!inp[0].disabled, "Enable/disable image - field now enabled");
-	ok(parseFloat(inp.next("img").css("opacity")) === 1, "Enable/disable image - image now enabled");
-	inp.datepicker("destroy");
+test( "enable / disable", function() {
+	expect( 6 );
+	var inl,
+		inp = TestHelpers.datepicker.init( "#inp" ),
+		dp = inp.datepicker( "widget" );
+
+	ok( !inp.datepicker( "option", "disabled" ), "initially enabled" );
+	ok( !dp.hasClass( "ui-datepicker-disabled" ), "does not have disabled class name" );
+
+	inp.datepicker( "disable" );
+	ok( inp.datepicker( "option", "disabled" ), "disabled option is set" );
+	ok( dp.hasClass( "ui-datepicker-disabled" ), "datepicker has disabled class name" );
+
+	inp.datepicker( "enable" );
+	ok( !inp.datepicker( "option", "disabled" ), "enabled after enable() call" );
+	ok( !dp.hasClass( "ui-datepicker-disabled" ), "no longer has disabled class name" );
+
 	// Inline
-	inl = TestHelpers.datepicker.init("#inl", {changeYear: true});
-	dp = $(".ui-datepicker-inline", inl);
-	ok(!inl.datepicker("isDisabled"), "Enable/disable inline - initially marked as enabled");
-	ok(!dp.children().is(".ui-state-disabled"), "Enable/disable inline - not visually disabled initially");
-	ok(!dp.find("select").prop("disabled"), "Enable/disable inline - form element enabled initially");
-	inl.datepicker("disable");
-	ok(inl.datepicker("isDisabled"), "Enable/disable inline - now marked as disabled");
-	ok(dp.children().is(".ui-state-disabled"), "Enable/disable inline - visually disabled");
-	ok(dp.find("select").prop("disabled"), "Enable/disable inline - form element disabled");
-	inl.datepicker("enable");
-	ok(!inl.datepicker("isDisabled"), "Enable/disable inline - now marked as enabled");
-	ok(!dp.children().is(".ui-state-disabled"), "Enable/disable inline - not visiually disabled");
-	ok(!dp.find("select").prop("disabled"), "Enable/disable inline - form element enabled");
-	inl.datepicker("destroy");
+	inl = TestHelpers.datepicker.init( "#inl" );
+	dp = inl.datepicker( "instance" );
+
+	// TODO: Disabling inline pickers does not work.
+	// TODO: When changeMonth and changeYear options are implemented ensure their dropdowns
+	// are properly disabled when in an inline picker.
 });
 
 })( jQuery );
