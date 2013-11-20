6458bab2ad02b2f718b1baa76078168c901dc202
TJ VanToll <tj.vantoll@gmail.com>
2013-11-20 08:53:02 -0500

    Datepicker: Add test to ensure the `<input>`'s value is preselected

--- tests/unit/datepicker/datepicker_core.js
+++ tests/unit/datepicker/datepicker_core.js
@@ -1,16 +1,23 @@
-/*
- * datepicker_core.js
- */
+(function( $ ) {
 
-(function($) {
-
-module( "datepicker: core", {
-	setup: function() {
-		$( "body" ).focus();
-	}
-});
+module( "datepicker: core" );
 
 TestHelpers.testJshint( "datepicker" );
+
+test( "input's value determines starting date", function() {
+	expect( 3 );
+
+	var input = $( "#datepicker" ).val( "1/1/2014" ).datepicker(),
+		picker = input.datepicker( "widget" );
+
+	input.datepicker( "open" );
+
+	equal( picker.find( ".ui-datepicker-month" ).html(), "January", "correct month displayed" );
+	equal( picker.find( ".ui-datepicker-year" ).html(), "2014", "correct year displayed" );
+	equal( picker.find( ".ui-state-focus" ).html(), "1", "correct day highlighted" );
+
+	input.val( "" ).datepicker( "destroy" );
+});
 
 asyncTest( "baseStructure", function() {
 	expect( 42 );
@@ -485,4 +492,4 @@ test( "mouse", function() {
 		"Mouse click inline - next" );
 });
 
-})(jQuery);
+})( jQuery );
