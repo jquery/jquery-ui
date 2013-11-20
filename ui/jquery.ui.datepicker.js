dfad85fd801ba02efd7e9c0025dbb7628898bd15
TJ VanToll <tj.vantoll@gmail.com>
2013-11-20 08:37:24 -0500

    Datepicker: Allow dateFormat to be changed after init. Add dateFormat tests.

--- ui/jquery.ui.datepicker.js
+++ ui/jquery.ui.datepicker.js
@@ -617,6 +617,13 @@ $.widget( "ui.datepicker", {
 			this.refresh();
 		}
 
+		if ( key === "dateFormat" ) {
+			this.date.setFormat( this.options.dateFormat );
+			if ( !this.inline ) {
+				this.element.val( this.date.format() );
+			}
+		}
+
 		if ( key === "showWeek" ) {
 			this.refresh();
 		}
