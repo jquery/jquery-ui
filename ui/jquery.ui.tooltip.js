af3ae966f38a02628402ee1e745695d74ae094ae
Daniel DeGroff <djdegroff@gmail.com>
2013-12-02 22:03:55 -0600

    Tooltip: on close and destroy only set title if empty or undefined. Fixed #8925 - Changes to title attribute are reverted on close
    Ticket #8925 states that changes to the title attribute while the
    tooltip is open are lost on tooltip close.
    
    In the close and destroy functions, the title attribute is always
    written if a value was stored in the element on open. It is possible
    the attribute has changed and restoring the initial value may overwrite
    the current value.
    
    If the value is empty or undefined as set in open, do not set the title
    attribute.
    
    This fix has the limitation that if the user removed the title
    attribute or set the value to an empty string the original title value
    would be restored on close and destroy. Test cases 3 and 4 demonstrate
    this limitation.

--- ui/jquery.ui.tooltip.js
+++ ui/jquery.ui.tooltip.js
@@ -305,7 +305,8 @@ $.widget( "ui.tooltip", {
 	close: function( event ) {
 		var that = this,
 			target = $( event ? event.currentTarget : this.element ),
-			tooltip = this._find( target );
+			tooltip = this._find( target ),
+			title = target.attr( "title" );
 
 		// disabling closes the tooltip, so we need to track when we're closing
 		// to avoid an infinite loop in case the tooltip becomes disabled on close
@@ -317,7 +318,8 @@ $.widget( "ui.tooltip", {
 		clearInterval( this.delayedShow );
 
 		// only set title if we had one before (see comment in _open())
-		if ( target.data( "ui-tooltip-title" ) ) {
+		// if the title attribute has changed since open(), don't restore
+		if ( target.data( "ui-tooltip-title" ) && ( title === "" || title === undefined ) ) {
 			target.attr( "title", target.data( "ui-tooltip-title" ) );
 		}
 
@@ -380,7 +382,7 @@ $.widget( "ui.tooltip", {
 		// close open tooltips
 		$.each( this.tooltips, function( id, element ) {
 			// Delegate to close method to handle common cleanup
-			var event = $.Event( "blur" );
+			var event = $.Event( "blur" ), title;
 			event.target = event.currentTarget = element[0];
 			that.close( event, true );
 
@@ -390,7 +392,11 @@ $.widget( "ui.tooltip", {
 
 			// Restore the title
 			if ( element.data( "ui-tooltip-title" ) ) {
-				element.attr( "title", element.data( "ui-tooltip-title" ) );
+				// if the title attribute has changed since open(), don't restore
+				title = element.attr( "title" );
+				if ( title === "" || title === undefined ) {
+					element.attr( "title", element.data( "ui-tooltip-title" ) );
+				}
 				element.removeData( "ui-tooltip-title" );
 			}
 		});
