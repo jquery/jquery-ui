9e151c5a1691f7a8b29b2f088c13b1686f9237b0
Brian J. Dowling <bjd-dev@simplicity.net>
2014-01-17 17:01:59 +0000

    Formatting: Fixed tabbing in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -179,15 +179,15 @@ grunt.initConfig({
 		})
 	},
 	qunit: {
-	        files: expandFiles( "tests/unit/**/*.html" ).filter(function( file ) {
-		       // TODO except for all|index|test, try to include more as we go
-		       return !( /(all|index|test)\.html$/ ).test( file );
+		files: expandFiles( "tests/unit/**/*.html" ).filter(function( file ) {
+			// TODO except for all|index|test, try to include more as we go
+			return !( /(all|index|test)\.html$/ ).test( file );
 		}),
 		options: {
-		       page : {
-		       	    viewportSize : { width: 700, height: 500 }
-		      }
-	        }
+			page: {
+				viewportSize: { width: 700, height: 500 }
+			}
+		}
 	},
 	jshint: {
 		options: {
