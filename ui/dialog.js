0ba36159f9cf7de5cc98d667313d38f90138bd2b
Alexander Schmitz <arschmitz@gmail.com>
2014-03-28 10:48:18 -0400

    Dialog: Updating dialog and tests for new button API

--- ui/dialog.js
+++ ui/dialog.js
@@ -416,10 +416,8 @@ $.widget( "ui.dialog", {
 		this.uiDialogTitlebarClose = $( "<button type='button'></button>" )
 			.button({
 				label: this.options.closeText,
-				icons: {
-					primary: "ui-icon-closethick"
-				},
-				text: false
+				icon: "ui-icon-closethick",
+				showLabel: false
 			})
 			.addClass( this._classes( "ui-dialog-titlebar-close" ) )
 			.appendTo( this.uiDialogTitlebar );
@@ -485,11 +483,13 @@ $.widget( "ui.dialog", {
 				click.apply( that.element[ 0 ], arguments );
 			};
 			buttonOptions = {
-				icons: props.icons,
-				text: props.showText
+				icon: props.icon,
+				showLabel: props.showLabel,
+				iconPosition: props.iconPosition
 			};
-			delete props.icons;
-			delete props.showText;
+			delete props.icon;
+			delete props.showLabel;
+			delete props.iconPosition;
 			$( "<button></button>", props )
 				.button( buttonOptions )
 				.appendTo( that.uiButtonSet );
