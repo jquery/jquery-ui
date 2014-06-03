4a6d8203d5940299bcd44567b3ee6658cf66832d
Felix Nagel <info@felixnagel.com>
2014-06-04 00:08:03 +0200

    Calendar: remove datepicker functionality, options and methods

--- ui/calendar.js
+++ ui/calendar.js
@@ -27,30 +27,18 @@
 }(function( $ ) {
 
 // TODO use uniqueId, if possible
-var idIncrement = 0,
-	// TODO move this to the instance
-	suppressExpandOnFocus = false;
+var idIncrement = 0;
 
 return $.widget( "ui.calendar", {
 	options: {
-		appendTo: null,
 		dateFormat: { date: "short" },
 		// TODO review
 		eachDay: $.noop,
 		numberOfMonths: 1,
-		position: {
-			my: "left top",
-			at: "left bottom"
-		},
 		showWeek: false,
-		show: true,
-		hide: true,
 		value: null,
 
 		// callbacks
-		beforeOpen: null,
-		close: null,
-		open: null,
 		select: null
 	},
 
@@ -58,20 +46,10 @@ return $.widget( "ui.calendar", {
 		this.id = "ui-calendar-" + idIncrement;
 		idIncrement++;
 
-		if ( this.element.is( "input" ) ) {
-			if ( !this.options.value && this.isValid() ) {
-				this.options.value = this._getParsedValue();
-			}
-			this._createPicker();
-		} else {
-			this.inline = true;
-			this.picker = this.element;
-		}
-
 		this.date = $.date( this.options.value, this.options.dateFormat ).select();
 		this.date.eachDay = this.options.eachDay;
 
-		this._on( this.picker, {
+		this._on( this.element, {
 			"click .ui-calendar-prev": function( event ) {
 				event.preventDefault();
 				this.date.adjust( "M", -this.options.numberOfMonths );
@@ -86,24 +64,18 @@ return $.widget( "ui.calendar", {
 				event.preventDefault();
 				this._select( event, new Date().getTime() );
 			},
-			"click .ui-calendar-close": function( event ) {
-				event.preventDefault();
-				this.close( event );
-			},
 			"mousedown .ui-calendar-calendar a": function( event ) {
 				event.preventDefault();
 				// TODO exclude clicks on lead days or handle them correctly
 				// TODO store/read more then just date, also required for multi month picker
 				this._select( event, $( event.currentTarget ).data( "timestamp" ) );
-				if ( this.inline ) {
-					this.grid.focus( 1 );
-				}
+				this.grid.focus( 1 );
 			},
 			"keydown .ui-calendar-calendar": "_handleKeydown"
 		});
 
 		// TODO use hoverable (no delegation support)? convert to _on?
-		this.picker.delegate( ".ui-calendar-header a, .ui-calendar-calendar a", "mouseenter.calendar mouseleave.calendar", function() {
+		this.element.delegate( ".ui-calendar-header a, .ui-calendar-calendar a", "mouseenter.calendar mouseleave.calendar", function() {
 			$( this ).toggleClass( "ui-state-hover" );
 		});
 
@@ -174,162 +146,13 @@ return $.widget( "ui.calendar", {
 		}
 	},
 
-	_createPicker: function() {
-		this.picker = $( "<div>" )
-			.addClass( "ui-front" )
-			// TODO add a ui-calendar-popup class, move position:absolute to that
-			.css( "position", "absolute" )
-			.uniqueId()
-			.hide();
-		this._setHiddenPicker();
-		this.picker.appendTo( this._appendTo() );
-
-		this.element
-			.attr( "aria-haspopup", "true" )
-			.attr( "aria-owns", this.picker.attr( "id" ) );
-
-		this._on({
-			keydown: function( event ) {
-				switch ( event.keyCode ) {
-					case $.ui.keyCode.TAB:
-						// Waiting for close() will make popup hide too late, which breaks tab key behavior
-						this.picker.hide();
-						this.close( event );
-						break;
-					case $.ui.keyCode.ESCAPE:
-						if ( this.isOpen ) {
-							this.close( event );
-						}
-						break;
-					case $.ui.keyCode.ENTER:
-						this._handleKeydown( event );
-						break;
-					case $.ui.keyCode.DOWN:
-					case $.ui.keyCode.UP:
-						clearTimeout( this.closeTimer );
-						this._delay( function() {
-							this.open( event );
-							this.grid.focus( 1 );
-						}, 1 );
-						break;
-					case $.ui.keyCode.HOME:
-						if ( event.ctrlKey ) {
-							this.date.setTime( new Date() );
-							event.preventDefault();
-							if ( this.isOpen ) {
-								this.refresh();
-							} else {
-								this.open( event );
-							}
-						}
-						break;
-					// TODO this is not in specs, keep?
-					case $.ui.keyCode.END:
-						if ( event.ctrlKey ) {
-							this.element.val( "" );
-							event.preventDefault();
-							if ( this.isOpen ) {
-								this.close( event );
-							}
-						}
-						break;
-				}
-			},
-			keyup: function() {
-				if ( this.isValid() && !this.inline ) {
-					this.date.setTime( this._getParsedValue() ).select();
-					this.refresh();
-				}
-			},
-			mousedown: function( event ) {
-				if ( this.isOpen ) {
-					suppressExpandOnFocus = true;
-					this.close();
-					return;
-				}
-				this.open( event );
-				clearTimeout( this.closeTimer );
-			},
-			focus: function( event ) {
-				if ( !suppressExpandOnFocus ) {
-					this._delay( function() {
-						if ( !this.isOpen ) {
-							this.open( event );
-						}
-					}, 1);
-				}
-				this._delay( function() {
-					suppressExpandOnFocus = false;
-				}, 100 );
-			},
-			blur: function() {
-				suppressExpandOnFocus = false;
-			}
-		});
-
-		this._on( this.picker, {
-			focusout: function( event ) {
-				// use a timer to allow click to clear it and letting that
-				// handle the closing instead of opening again
-				// also allows tabbing inside the calendar without it closing
-				this.closeTimer = this._delay( function() {
-					this.close( event );
-				}, 150 );
-			},
-			focusin: function() {
-				clearTimeout( this.closeTimer );
-			},
-			mouseup: function() {
-				clearTimeout( this.closeTimer );
-			},
-			// TODO on TAB (or shift TAB), make sure it ends up on something useful in DOM order
-			keyup: function( event ) {
-				if ( event.keyCode === $.ui.keyCode.ESCAPE && this.picker.is( ":visible" ) ) {
-					this.close( event );
-					this._focusTrigger();
-				}
-			}
-		});
-
-		this._on( this.document, {
-			click: function( event ) {
-				if ( this.isOpen && !$( event.target ).closest( this.element.add( this.picker ) ).length ) {
-					this.close( event );
-				}
-			}
-		});
-	},
-
-	_appendTo: function() {
-		var element = this.options.appendTo;
-
-		if ( element ) {
-			element = element.jquery || element.nodeType ?
-				$( element ) :
-				this.document.find( element ).eq( 0 );
-		}
-
-		if ( !element ) {
-			element = this.element.closest( ".ui-front" );
-		}
-
-		if ( !element.length ) {
-			element = this.document[ 0 ].body;
-		}
-
-		return element;
-	},
-
 	_createTmpl: function() {
 		this._createDatepicker();
-		this.picker.find( "button" ).button();
+		this.element.find( "button" ).button();
 
-		if ( this.inline ) {
-			this.picker.children().addClass( "ui-calendar-inline" );
-		}
 		// against display:none in calendar.css
-		this.picker.find( ".ui-calendar" ).css( "display", "block" );
-		this.grid = this.picker.find( ".ui-calendar-calendar" );
+		this.element.find( ".ui-calendar" ).css( "display", "block" );
+		this.grid = this.element.find( ".ui-calendar-calendar" );
 	},
 
 	_createDatepicker: function() {
@@ -352,7 +175,7 @@ return $.widget( "ui.calendar", {
 				"aria-labelledby": this.id + "-title"
 			})
 			.html( pickerHtml )
-			.appendTo( this.picker );
+			.appendTo( this.element );
 	},
 
 	_buildMultiplePicker: function() {
@@ -400,10 +223,9 @@ return $.widget( "ui.calendar", {
 	},
 
 	_buildPreviousLink: function() {
-		var labels = Globalize.translate( "calendar" );
+		var labels = Globalize.translate( "datepicker" );
 
-		return "<button class='ui-calendar-prev ui-corner-all' " +
-			"title='" + labels.prevText + "'>" +
+		return "<button class='ui-calendar-prev ui-corner-all' title='" + labels.prevText + "'>" +
 				"<span class='ui-icon ui-icon-circle-triangle-w'>" +
 					labels.prevText +
 				"</span>" +
@@ -411,10 +233,9 @@ return $.widget( "ui.calendar", {
 	},
 
 	_buildNextLink: function() {
-		var labels = Globalize.translate( "calendar" );
+		var labels = Globalize.translate( "datepicker" );
 
-		return "<button class='ui-calendar-next ui-corner-all' " +
-			"title='" + labels.nextText + "'>" +
+		return "<button class='ui-calendar-next ui-corner-all' title='" + labels.nextText + "'>" +
 				"<span class='ui-icon ui-icon-circle-triangle-e'>" +
 					labels.nextText +
 				"</span>" +
@@ -422,7 +243,7 @@ return $.widget( "ui.calendar", {
 	},
 
 	_buildTitlebar: function() {
-		var labels = Globalize.translate( "calendar" );
+		var labels = Globalize.translate( "datepicker" );
 
 		return "<div role='header' id='" + this.id + "-title'>" +
 			"<div id='" + this.id + "-month-lbl' class='ui-calendar-title'>" +
@@ -452,7 +273,7 @@ return $.widget( "ui.calendar", {
 	_buildGridHeading: function() {
 		var cells = "",
 			i = 0,
-			labels = Globalize.translate( "calendar" );
+			labels = Globalize.translate( "datepicker" );
 
 		if ( this.options.showWeek ) {
 			cells += "<th class='ui-calendar-week-col'>" + labels.weekHeader + "</th>";
@@ -522,7 +343,7 @@ return $.widget( "ui.calendar", {
 	_buildDayLink: function( day ) {
 		var link,
 			classes = [ "ui-state-default" ],
-			labels = Globalize.translate( "calendar" );
+			labels = Globalize.translate( "datepicker" );
 
 		if ( day.date === this.date.day() ) {
 			classes.push( "ui-state-focus" );
@@ -563,17 +384,11 @@ return $.widget( "ui.calendar", {
 	},
 
 	_buildButtons: function() {
-		var labels = Globalize.translate( "calendar" );
+		var labels = Globalize.translate( "datepicker" );
 
 		return "<div class='ui-calendar-buttonpane ui-widget-content'>" +
 			"<button class='ui-calendar-current'>" + labels.currentText + "</button>" +
-			"<button class='ui-calendar-close'>" + labels.closeText + "</button>" +
 		"</div>";
-	},
-
-	_focusTrigger: function() {
-		suppressExpandOnFocus = true;
-		this.element.focus();
 	},
 
 	// Refreshing the entire calendar during interaction confuses screen readers, specifically
@@ -586,8 +401,8 @@ return $.widget( "ui.calendar", {
 		// TODO: Prevent disabled cells from being focused
 		if ( this.options.numberOfMonths === 1 ) {
 			this.grid = $( this._buildGrid() );
-			$( ".ui-calendar-title", this.picker ).html( this._buildTitle() );
-			$( ".ui-calendar-calendar", this.picker ).replaceWith( this.grid );
+			$( ".ui-calendar-title", this.element ).html( this._buildTitle() );
+			$( ".ui-calendar-calendar", this.element ).replaceWith( this.grid );
 		} else {
 			this._refreshMultiplePicker();
 		}
@@ -597,74 +412,25 @@ return $.widget( "ui.calendar", {
 		var i = 0;
 
 		for ( ; i < this.options.numberOfMonths; i++ ) {
-			$( ".ui-calendar-title", this.picker ).eq( i ).html( this._buildTitle() );
-			$( ".ui-calendar-calendar", this.picker ).eq( i ).html( this._buildGrid() );
+			$( ".ui-calendar-title", this.element ).eq( i ).html( this._buildTitle() );
+			$( ".ui-calendar-calendar", this.element ).eq( i ).html( this._buildGrid() );
 			this.date.adjust( "M", 1 );
 		}
 		this.date.adjust( "M", -this.options.numberOfMonths );
 
 		// TODO: This assumes focus is on the first grid. For multi pickers, the widget needs
 		// to maintain the currently focused grid and base queries like this off of it.
-		$( this.picker ).find( ".ui-state-focus" ).not( ":first" ).removeClass( "ui-state-focus" );
-	},
-
-	open: function( event ) {
-		if ( this.inline || this.isOpen ) {
-			return;
-		}
-		if ( this._trigger( "beforeOpen", event ) === false ) {
-			return;
-		}
-
-		this.refresh();
-
-		this.picker
-			.attr( "aria-hidden", "false" )
-			.attr( "aria-expanded", "true" )
-			.show()
-			.position( this._buildPosition() )
-			.hide();
-
-		this._show( this.picker, this.options.show );
-
-		// take trigger out of tab order to allow shift-tab to skip trigger
-		// TODO does this really make sense? related bug: tab-shift moves focus to last element on page
-		this.element.attr( "tabindex", -1 );
-		this.isOpen = true;
-
-		this._trigger( "open", event );
-	},
-
-	close: function( event ) {
-		if ( this.inline ) {
-			return;
-		}
-
-		this._setHiddenPicker();
-		this._hide( this.picker, this.options.hide );
-
-		this.element.attr( "tabindex" , 0 );
-
-		this.isOpen = false;
-		this._trigger( "close", event );
+		$( this.element ).find( ".ui-state-focus" ).not( ":first" ).removeClass( "ui-state-focus" );
 	},
 
 	_setHiddenPicker: function() {
-		this.picker
+		this.element
 			.attr( "aria-hidden", "true" )
 			.attr( "aria-expanded", "false" );
 	},
 
-	_buildPosition: function() {
-		return $.extend( {}, { of: this.element }, this.options.position );
-	},
-
 	_select: function( event, time ) {
 		this._setOption( "value", new Date( time ) );
-		if ( !this.inline ) {
-			this.close();
-			this._focusTrigger();
-		}
 		this._trigger( "select", event, {
 			// TODO replace with value option to initialise and read
 			date: this.value()
@@ -675,11 +441,7 @@ return $.widget( "ui.calendar", {
 		if ( arguments.length ) {
 			this._setOption( "value", Globalize.parseDate( value, this.options.dateFormat ) );
 		} else {
-			if ( this.inline ) {
-				return Globalize.format( this.date.selected(), this.options.dateFormat );
-			} else {
-				return this.element.val();
-			}
+			return Globalize.format( this.option( "value" ), this.options.dateFormat );
 		}
 	},
 
@@ -691,36 +453,16 @@ return $.widget( "ui.calendar", {
 		}
 	},
 
-	isValid: function() {
-		if ( this.inline ) {
-			return true;
-		}
-
-		return this._getParsedValue() !== null;
-	},
-
 	_destroy: function() {
-		if ( this.inline ) {
-			this.picker.empty();
-		} else {
-			this.picker.remove();
-			this.element
-				.removeAttr( "aria-haspopup" )
-				.removeAttr( "aria-owns" );
-		}
-	},
-
-	widget: function() {
-		return this.picker;
-	},
-
-	_getParsedValue: function() {
-		return Globalize.parseDate( this.element.val(), this.options.dateFormat );
+		this.element
+			.empty()
+			.removeAttr( "aria-haspopup" )
+			.removeAttr( "aria-owns" );
 	},
 
 	option: function( key ) {
 		if ( arguments.length === 0 || ( arguments.length === 1 && key === "value" ) ) {
-			this.options.value = this.inline ? this.date.selected() : this._getParsedValue();
+			this.options.value = this.date.selectedDate();
 		}
 		return this._superApply( arguments );
 	},
@@ -730,17 +472,10 @@ return $.widget( "ui.calendar", {
 			if ( value instanceof Date ) {
 				this.date.setTime( value.getTime() ).select();
 				this.refresh();
-				if ( !this.inline ) {
-					this.element.val( this.date.format() );
-				}
 			}
 		}
 
 		this._super( key, value );
-
-		if ( key === "appendTo" ) {
-			this.picker.appendTo( this._appendTo() );
-		}
 
 		if ( key === "eachDay" ) {
 			this.date.eachDay = this.options.eachDay;
@@ -749,17 +484,10 @@ return $.widget( "ui.calendar", {
 
 		if ( key === "dateFormat" ) {
 			this.date.setFormat( this.options.dateFormat );
-			if ( !this.inline ) {
-				this.element.val( this.date.format() );
-			}
 		}
 
 		if ( key === "showWeek" ) {
 			this.refresh();
-		}
-
-		if ( key === "position" ) {
-			this.picker.position( this._buildPosition() );
 		}
 	}
 });
