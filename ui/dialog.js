jquery/jquery-ui

commit df6110c0d424ff3306fdd5576011f2dcf4d242d0

Core: Deprecate .focus( n ), replace in dialog with explicit timeouts
Fixes #9646

ui/dialog.js
@@ -342,10 +342,14 @@ return $.widget( "ui.dialog", {
 					last = tabbables.filter( ":last" );
 
 				if ( ( event.target === last[0] || event.target === this.uiDialog[0] ) && !event.shiftKey ) {
-					first.focus( 1 );
+					this._delay(function() {
+						first.focus();
+					});
 					event.preventDefault();
 				} else if ( ( event.target === first[0] || event.target === this.uiDialog[0] ) && event.shiftKey ) {
-					last.focus( 1 );
+					this._delay(function() {
+						first.focus();
+					});
 					event.preventDefault();
 				}
 			},

tests/unit/core/core.js
@@ -4,34 +4,6 @@ module( "core - jQuery extensions" );
 
 TestHelpers.testJshint( "core" );
 
-asyncTest( "focus - original functionality", function() {
-	expect( 1 );
-	$( "#inputTabindex0" )
-		.one( "focus", function() {
-			ok( true, "event triggered" );
-			start();
-		})
-		.focus();
-});
-
-asyncTest( "focus", function() {
-	expect( 2 );
-
-	// support: IE 8
-	// IE sometimes gets confused about what's focused if we don't explicitly
-	// focus a different element first
-	$( "body" ).focus();
-
-	$( "#inputTabindex0" )
-		.one( "focus", function() {
-			ok( true, "event triggered" );
-			start();
-		})
-		.focus( 500, function() {
-			ok( true, "callback triggered" );
-		});
-});
-
 test( "innerWidth - getter", function() {
 	expect( 2 );
 	var el = $( "#dimensions" );

tests/unit/core/core_deprecated.js
@@ -2,6 +2,34 @@
 
 module( "core - deprecated" );
 
+asyncTest( "focus - original functionality", function() {
+	expect( 1 );
+	$( "#inputTabindex0" )
+		.one( "focus", function() {
+			ok( true, "event triggered" );
+			start();
+		})
+		.focus();
+});
+
+asyncTest( "focus", function() {
+	expect( 2 );
+
+	// support: IE 8
+	// IE sometimes gets confused about what's focused if we don't explicitly
+	// focus a different element first
+	$( "body" ).focus();
+
+	$( "#inputTabindex0" )
+		.one( "focus", function() {
+			ok( true, "event triggered" );
+			start();
+		})
+		.focus( 500, function() {
+			ok( true, "callback triggered" );
+		});
+});
+
 test( "zIndex", function() {
 	expect( 7 );
 	var el = $( "#zIndexAutoWithParent" ),

ui/core.js
@@ -48,22 +48,6 @@ $.extend( $.ui, {
 
 // plugins
 $.fn.extend({
-	focus: (function( orig ) {
-		return function( delay, fn ) {
-			return typeof delay === "number" ?
-				this.each(function() {
-					var elem = this;
-					setTimeout(function() {
-						$( elem ).focus();
-						if ( fn ) {
-							fn.call( elem );
-						}
-					}, delay );
-				}) :
-				orig.apply( this, arguments );
-		};
-	})( $.fn.focus ),
-
 	scrollParent: function() {
 		var position = this.css( "position" ),
 			excludeStaticParent = position === "absolute",
@@ -225,6 +209,22 @@ $.ui.ie = !!/msie [\w.]+/.exec( navigator.userAgent.toLowerCase() );
 
 $.support.selectstart = "onselectstart" in document.createElement( "div" );
 $.fn.extend({
+	focus: (function( orig ) {
+		return function( delay, fn ) {
+			return typeof delay === "number" ?
+				this.each(function() {
+					var elem = this;
+					setTimeout(function() {
+						$( elem ).focus();
+						if ( fn ) {
+							fn.call( elem );
+						}
+					}, delay );
+				}) :
+				orig.apply( this, arguments );
+		};
+	})( $.fn.focus ),
+
 	disableSelection: function() {
 		return this.bind( ( $.support.selectstart ? "selectstart" : "mousedown" ) +
 			".ui-disableSelection", function( event ) {

commit a0b84767a76098cdcc6375dfe28a7fee866bd395

Dialog: Fix shift-tab handling, focus the correct element
Copy-paste error introduced in df6110c0d424ff3306fdd5576011f2dcf4d242d0

Updates the tabbing test to be more specific about which element should
have focus, instead of only checking if focus is within the dialog.

Ref #9646
Ref #10103
Closes gh-1264

ui/dialog.js
@@ -348,7 +348,7 @@ return $.widget( "ui.dialog", {
 					event.preventDefault();
 				} else if ( ( event.target === first[0] || event.target === this.uiDialog[0] ) && event.shiftKey ) {
 					this._delay(function() {
-						first.focus();
+						last.focus();
 					});
 					event.preventDefault();
 				}

tests/unit/dialog/dialog_core.js
@@ -140,12 +140,14 @@ test( "#7960: resizable handles below modal overlays", function() {
 asyncTest( "Prevent tabbing out of dialogs", function() {
 	expect( 3 );
 
-	var element = $( "<div><input><input></div>" ).dialog(),
-		inputs = element.find( "input" ),
-		widget = element.dialog( "widget" )[ 0 ];
+	var element = $( "<div><input name='0'><input name='1'></div>" ).dialog(),
+		inputs = element.find( "input" );
+
+	// Remove close button to test focus on just the two buttons
+	element.dialog( "widget" ).find( ".ui-button").remove();
 
 	function checkTab() {
-		ok( $.contains( widget, document.activeElement ), "Tab key event moved focus within the modal" );
+		equal( document.activeElement, inputs[ 0 ], "Tab key event moved focus within the modal" );
 
 		// check shift tab
 		$( document.activeElement ).simulate( "keydown", { keyCode: $.ui.keyCode.TAB, shiftKey: true });
@@ -153,15 +155,15 @@ asyncTest( "Prevent tabbing out of dialogs", function() {
 	}
 
 	function checkShiftTab() {
-		ok( $.contains( widget, document.activeElement ), "Shift-Tab key event moved focus within the modal" );
+		equal( document.activeElement, inputs[ 1 ], "Shift-Tab key event moved focus back to second input" );
 
 		element.remove();
 		setTimeout( start );
 	}
 
-	inputs[1].focus();
+	inputs[ 1 ].focus();
 	setTimeout(function() {
-		equal( document.activeElement, inputs[1], "Focus set on second input" );
+		equal( document.activeElement, inputs[ 1 ], "Focus set on second input" );
 		inputs.eq( 1 ).simulate( "keydown", { keyCode: $.ui.keyCode.TAB });
 
 		setTimeout( checkTab );