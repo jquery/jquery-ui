730cde53b49fc5c6b843a185d019672544b51e81
Felix Nagel <info@felixnagel.com>
2014-06-12 14:36:45 +0200

    Datepicker: Simplify usage of calendar options and avoid duplications

--- ui/datepicker.js
+++ ui/datepicker.js
@@ -26,26 +26,19 @@
 	}
 }(function( $ ) {
 
-// TODO Use uniqueId, if possible
-var idIncrement = 0,
-	// TODO Move this to the instance
+var widget,
+	calendarOptions = [ "dateFormat", "eachDay", "max", "min", "numberOfMonths", "showWeek" ],
+	// TODO Move this to the instance?
 	suppressExpandOnFocus = false;
 
-$.widget( "ui.datepicker", {
+widget = $.widget( "ui.datepicker", {
 	version: "@VERSION",
 	options: {
 		appendTo: null,
-		dateFormat: { date: "short" },
-		// TODO Review
-		eachDay: $.noop,
-		max: null,
-		min: null,
-		numberOfMonths: 1,
 		position: {
 			my: "left top",
 			at: "left bottom"
 		},
-		showWeek: false,
 		show: true,
 		hide: true,
 
@@ -89,13 +82,7 @@ $.widget( "ui.datepicker", {
 
 		// Initialize calendar widget
 		this.calendarInstance = this.calendar
-			.calendar({
-				dateFormat: this.options.dateFormat,
-				eachDay: this.options.eachDay,
-				max: this.options.max,
-				min: this.options.min,
-				numberOfMonths: this.options.numberOfMonths,
-				showWeek: this.options.showWeek,
+			.calendar( $.extend( {}, this.options, {
 				value: this._getParsedValue(),
 				select: function( event ) {
 					that.element.val( that.calendarInstance.value() );
@@ -103,7 +90,7 @@ $.widget( "ui.datepicker", {
 					that._focusTrigger();
 					that._trigger( "select", event );
 				}
-			})
+			}) )
 			.calendar( "instance" );
 
 		this._setHiddenPicker();
@@ -317,7 +304,7 @@ $.widget( "ui.datepicker", {
 	_setOption: function( key, value ) {
 		this._super( key, value );
 
-		if ( $.inArray( key, [ "showWeek", "numberOfMonths", "dateFormat", "eachDay", "min", "max" ] ) !== -1 ) {
+		if ( $.inArray( key, calendarOptions ) !== -1 ) {
 			this.calendarInstance.option( key, value );
 		}
 
@@ -334,4 +321,11 @@ $.widget( "ui.datepicker", {
 		}
 	}
 });
+	
+$.each( calendarOptions, function( index, option ) {
+	$.ui.datepicker.prototype.options[ option ] = $.ui.calendar.prototype.options[ option ];
+});
+
+return widget;
+
 }));
