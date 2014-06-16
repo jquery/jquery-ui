f338f59272dc01fcf69e3096f59973398f7d91ee
Felix Nagel <info@felixnagel.com>
2014-06-16 22:12:27 +0200

    Datepicker tests: Add common unit tests

--- tests/unit/datepicker/datepicker_common.js
+++ tests/unit/datepicker/datepicker_common.js
@@ -1,7 +1,25 @@
-/*
 TestHelpers.commonWidgetTests( "datepicker", {
 	defaults: {
-		disabled: false
+		appendTo: null,
+		dateFormat: { date: "short" },
+		disabled: false,
+		eachDay: $.noop,
+		max: null,
+		min: null,
+		numberOfMonths: 1,
+		position: {
+			my: "left top",
+			at: "left bottom"
+		},
+		showWeek: false,
+		show: true,
+		hide: true,
+
+		// callbacks
+		beforeOpen: null,
+		close: null,
+		create: null,
+		open: null,
+		select: null
 	}
 });
-*/
