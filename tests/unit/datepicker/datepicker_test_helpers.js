1c33a3af91d43f5bd72169c135cc84055a8c3e7d
Felix Nagel <info@felixnagel.com>
2014-06-17 19:53:08 +0200

    Datepicker tests: Rewrite event unit tests

--- tests/unit/datepicker/datepicker_test_helpers.js
+++ tests/unit/datepicker/datepicker_test_helpers.js
@@ -15,14 +15,12 @@ TestHelpers.datepicker = {
 		equal( d1.toString(), d2.toString(), message );
 	},
 	init: function( id, options ) {
-		options = $.extend( { show: false }, options || {} );
+		options = $.extend( { show: false, hide: false }, options || {} );
 		return $( id ).datepicker( options );
 	},
 	initNewInput: function( options ) {
 		options = $.extend( { show: false, hide: false }, options || {} );
 		return $( "<input>" ).datepicker( options )
 			.appendTo( "#qunit-fixture" );
-	},
-	onFocus: TestHelpers.onFocus,
-	PROP_NAME: "datepicker"
+	}
 };
