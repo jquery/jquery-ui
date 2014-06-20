8c43f0296c27fba5a823ea24177c97db7ea752bc
Felix Nagel <info@felixnagel.com>
2014-06-20 17:50:12 +0200

    Calendar tests: Add common unit tests

--- tests/unit/calendar/calendar_common.js
+++ tests/unit/calendar/calendar_common.js
@@ -1,7 +1,17 @@
-/*
 TestHelpers.commonWidgetTests( "calendar", {
 	defaults: {
-		disabled: false
+		buttons: [],
+		dateFormat: { date: "short" },
+		disabled: false,
+		eachDay: $.noop,
+		max: null,
+		min: null,
+		numberOfMonths: 1,
+		showWeek: false,
+		value: null,
+
+		// callbacks
+		create: null,
+		select: null
 	}
 });
-*/
