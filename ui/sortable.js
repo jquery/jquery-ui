75fb4de30d6908d361d3ebd8b20d6daee0891ec2
Ki Young, Lee <code2710@gmail.com>
2014-08-01 17:24:44 +0900

    add null check "this.placeholder[0].parentNode"
    this.placeholder[0].parentNode is null or undefined thie line. It's same as line 497

--- ui/sortable.js
+++ ui/sortable.js
@@ -1267,7 +1267,9 @@ return $.widget("ui.sortable", $.ui.mouse, {
 		}
 
 		//$(this.placeholder[0]).remove(); would have been the jQuery way - unfortunately, it unbinds ALL events from the original node!
-		this.placeholder[0].parentNode.removeChild(this.placeholder[0]);
+		if(this.placeholder[0].parentNode) {
+			this.placeholder[0].parentNode.removeChild(this.placeholder[0]);
+		}
 
 		if(this.helper[0] !== this.currentItem[0]) {
 			this.helper.remove();
