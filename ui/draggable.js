df1a734c2c99d533960bb083fe465c40892fc81a
TJ VanToll <tj.vantoll@gmail.com>
2014-08-15 11:09:19 -0400

    Draggable: Only blur the focused element if the event occurs on a handle
    Fixes #10527

--- ui/draggable.js
+++ ui/draggable.js
@@ -94,20 +94,9 @@ $.widget("ui.draggable", $.ui.mouse, {
 	},
 
 	_mouseCapture: function(event) {
+		var o = this.options;
 
-		var document = this.document[ 0 ],
-			o = this.options;
-
-		// support: IE9
-		// IE9 throws an "Unspecified error" accessing document.activeElement from an <iframe>
-		try {
-			// Support: IE9+
-			// If the <body> is blurred, IE will switch windows, see #9520
-			if ( document.activeElement && document.activeElement.nodeName.toLowerCase() !== "body" ) {
-				// Blur any element that currently has focus, see #4261
-				$( document.activeElement ).blur();
-			}
-		} catch ( error ) {}
+		this._blurActiveElement( event );
 
 		// among others, prevent a drag on a resizable-handle
 		if (this.helper || o.disabled || $(event.target).closest(".ui-resizable-handle").length > 0) {
@@ -132,6 +121,27 @@ $.widget("ui.draggable", $.ui.mouse, {
 
 		return true;
 
+	},
+
+	_blurActiveElement: function() {
+		var document = this.document[ 0 ];
+
+		// support: IE9
+		// IE9 throws an "Unspecified error" accessing document.activeElement from an <iframe>
+		try {
+
+			// Support: IE9, IE10
+			// If the <body> is blurred, IE will switch windows, see #9520
+			if ( document.activeElement && document.activeElement.nodeName.toLowerCase() !== "body" ) {
+
+				// Only need to blur if the event occurred on the draggable, see #10527
+				if ( this.handleElement.is( event.target ) ) {
+
+					// Blur any element that currently has focus, see #4261
+					$( document.activeElement ).blur();
+				}
+			}
+		} catch ( error ) {}
 	},
 
 	_mouseStart: function(event) {
