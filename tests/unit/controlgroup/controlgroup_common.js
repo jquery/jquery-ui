a736b22dae0bd575bf40bac1b3c47dfada721a02
Alexander Schmitz <arschmitz@gmail.com>
2014-08-22 23:39:35 -0400

    Controlgroup: Inital commit of new widget
    This widget replaces the buttonset widget

---
+++ tests/unit/controlgroup/controlgroup_common.js
@@ -0,0 +1,15 @@
+TestHelpers.commonWidgetTests( "controlgroup", {
+	defaults: {
+		disabled: null,
+		items: {
+			"button": "input[type=button], input[type=submit], input[type=reset], button, a",
+			"checkboxradio": "input[type='checkbox'], input[type='radio']",
+			"selectmenu": "select"
+		},
+		direction: "horizontal",
+		excludeInvisible: true,
+
+		// Callbacks
+		create: null
+	}
+});
