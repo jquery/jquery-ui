b39bb7250e8d462f7cd031b6b5845ff0e0765998
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-09-04 16:04:04 -0300

    Datepicker: Use Globalize 1.0.0
    - Remove unneeded territory DE from de-DE locale;

--- tests/unit/calendar/core.js
+++ tests/unit/calendar/core.js
@@ -116,14 +116,14 @@ test( "Localization", function() {
 			);
 		};
 
-	Globalize.locale( "de-DE" );
+	Globalize.locale( "de" );
 	initCalendar();
 	testLocalization( "Init: " );
 	element.calendar( "destroy" );
 
 	Globalize.locale( defaultLocale.locale );
 	initCalendar();
-	Globalize.locale( "de-DE" );
+	Globalize.locale( "de" );
 	element.calendar( "refresh" );
 	testLocalization( "After init: " );
 
