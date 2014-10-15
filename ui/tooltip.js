d1da40842e0ac9f080c1b3b23d4c446c87cd9a6a
Alexander Schmitz <arschmitz@gmail.com>
2014-10-15 11:26:50 -0400

    Tooltip: Add support for setOption with classes

--- ui/tooltip.js
+++ ui/tooltip.js
@@ -107,6 +107,17 @@ $.widget( "ui.tooltip", {
 			.appendTo( this.document[ 0 ].body );
 	},
 
+	_elementsFromClassKey: function( classKey ) {
+		switch( classKey ) {
+			case "ui-tooltip":
+				return this.element.find( "ui-tooltip" );
+			case "ui-tooltip-title":
+				return this.element.find( "ui-tooltip-title" );
+			default:
+				return this._superApply( arguments );
+		}
+	},
+
 	_setOption: function( key, value ) {
 		var that = this;
 
