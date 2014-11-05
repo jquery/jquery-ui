5f849d239e2c85137d90895bcf94d4478e306a48
Alexander Schmitz <arschmitz@gmail.com>
2014-11-05 09:40:40 -0500

    Progressbar: simplify tests for classes

--- tests/unit/progressbar/progressbar_core.js
+++ tests/unit/progressbar/progressbar_core.js
@@ -1,33 +1,29 @@
 module( "progressbar: core" );
 
 test( "markup structure", function() {
-	expect( 12 );
+	expect( 7 );
 	var element = $( "#progressbar" ).progressbar(),
 		value = element.children().eq( 0 );
-	ok( element.hasClass( "ui-progressbar" ), "main element is .ui-progressbar" );
-	ok( element.hasClass( "ui-widget" ), "main element is .ui-widget" );
-	ok( element.hasClass( "ui-widget-content" ), "main element is .ui-widget-content" );
-	ok( element.hasClass( "ui-corner-all" ), "main element is .ui-corner-all" );
-	ok( !value.hasClass( "ui-progressbar-complete" ), "value is not .ui-progressbar-complete" );
-	ok( !value.hasClass( "ui-corner-right" ), "value is not .ui-corner-right" );
+	ok( element.is( ".ui-progressbar.ui-widget.ui-widget-content.ui-corner-all" ),
+		"main element has proper classes" );
+	ok( !value.is( ".ui-progressbar-complete.ui-corner-right" ),
+		"value does not have ui-corner-right or ui-progressbar-complete" );
 	element.progressbar( "option", "value", 100 );
-	ok( value.hasClass( "ui-progressbar-complete" ), "value is .ui-progressbar-complete" );
-	ok( value.hasClass( "ui-corner-right" ), "value is .ui-corner-right" );
+	ok( value.is( ".ui-progressbar-complete.ui-corner-right" ), "value has proper classes" );
 
 	ok( !element.hasClass( "ui-progressbar-indeterminate" ),
 		"main element is not .ui-progressbar-indeterminate" );
 	equal( element.children().length, 1, "main element contains one child" );
-	ok( element.children().eq( 0 ).hasClass( "ui-progressbar-value" ),
+	ok( element.children().eq( 0 ).is( ".ui-progressbar-value" ),
 		"child is .ui-progressbar-value" );
 	equal( element.children().children().length, 0, "no overlay div" );
 });
 
 test( "markup structure - indeterminate", function() {
-	expect( 5 );
+	expect( 4 );
 	var element = $( "#progressbar" ).progressbar({ value: false });
-	ok( element.hasClass( "ui-progressbar" ), "main element is .ui-progressbar" );
-	ok( element.hasClass( "ui-progressbar-indeterminate" ),
-		"main element is .ui-progressbar-indeterminate" );
+	ok( element.is( ".ui-progressbar.ui-progressbar-indeterminate" ),
+		"main element has proper classes" );
 	equal( element.children().length, 1, "main element contains one child" );
 	ok( element.children().eq( 0 ).hasClass( "ui-progressbar-value" ),
 		"child is .ui-progressbar-value" );
