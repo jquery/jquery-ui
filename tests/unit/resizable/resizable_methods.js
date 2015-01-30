40d1df6ec6a5a90fd83d7c3a45446f35b8869e6e
Alexander Schmitz <arschmitz@gmail.com>
2015-01-30 09:59:26 -0500

    Resizable: Use new has/lacksClasses assertions for all class checks

--- tests/unit/resizable/resizable_methods.js
+++ tests/unit/resizable/resizable_methods.js
@@ -5,15 +5,15 @@
 
 module( "resizable: methods" );
 
-test( "disable", function() {
+test( "disable", function( assert ) {
 	expect( 5 );
 
 	var element = $( "#resizable1" ).resizable({ disabled: false }),
 		chainable = element.resizable( "disable" );
 
-	ok( !element.resizable( "widget" ).hasClass( "ui-state-disabled" ), "element does not get ui-state-disabled" );
+	assert.lacksClasses( element.resizable( "widget" ), "ui-state-disabled" );
 	ok( !element.resizable( "widget" ).attr( "aria-disabled" ), "element does not get aria-disabled" );
-	ok( element.resizable( "widget" ).hasClass( "ui-resizable-disabled" ), "element gets ui-resizable-disabled" );
+	assert.hasClasses( element.resizable( "widget" ), "ui-resizable-disabled" );
 	equal( element.resizable( "option", "disabled" ), true, "disabled option setter" );
 	equal( chainable, element, "disable is chainable" );
 });
