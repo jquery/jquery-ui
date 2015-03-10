6091afadd708500008954acba6bf952cacf97917
Scott González <scott.gonzalez@gmail.com>
2015-03-10 08:19:33 -0400

    [fixup]: Internal use only comments

--- ui/core.js
+++ ui/core.js
@@ -51,6 +51,7 @@ $.extend( $.ui, {
 		UP: 38
 	},
 
+	// Internal use only
 	safeActiveElement: function( document ) {
 		var activeElement;
 
@@ -65,6 +66,7 @@ $.extend( $.ui, {
 		return activeElement;
 	},
 
+	// Internal use only
 	safeBlur: function( element ) {
 
 		// Support: IE9 - 10 only
