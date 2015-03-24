76c27556f48bea48d3787c241d35e190d46c3245
Scott González <scott.gonzalez@gmail.com>
2015-03-24 14:10:37 -0400

    Core: Handle IE11 bug with document.activeElement in iframes
    Fixes #11539
    Closes gh-1518

--- ui/core.js
+++ ui/core.js
@@ -63,6 +63,13 @@ $.extend( $.ui, {
 			activeElement = document.body;
 		}
 
+		// Support: IE 11 only
+		// IE11 returns a seemingly empty object in some cases when accessing
+		// document.activeElement from an <iframe>
+		if ( !activeElement.nodeName ) {
+			activeElement = document.body;
+		}
+
 		return activeElement;
 	},
 
