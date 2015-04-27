1ee8e230bdc6036bec1e28573c67e2e9ca67252c
Alexander Schmitz <arschmitz@gmail.com>
2015-04-27 02:34:56 -0400

    Tabs: Use $.ui.escapeSelector to avoid invalid selectors

--- ui/tabs.js
+++ ui/tabs.js
@@ -713,7 +713,7 @@ $.widget( "ui.tabs", {
 	_getIndex: function( index ) {
 		// meta-function to give users option to provide a href string instead of a numerical index.
 		if ( typeof index === "string" ) {
-			index = this.anchors.index( this.anchors.filter( "[href$='" + index + "']" ) );
+			index = this.anchors.index( this.anchors.filter( "[href$='" + $.ui.escapeSelector( index ) + "']" ) );
 		}
 
 		return index;
