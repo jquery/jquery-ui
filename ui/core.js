9abfc4637796a381128cd0a013e814e7f69847c2
Alexander Schmitz <arschmitz@gmail.com>
2015-05-05 14:52:11 -0400

    Core: Fixup typo

--- ui/core.js
+++ ui/core.js
@@ -137,9 +137,8 @@ $.fn.extend( {
 	},
 
 	// Support: IE8 Only
-	// IE8 does not support the form attribute and when it is supplied it over wirtes the form prop
-	// with a string so we need to try and work around this by looking for the closest form if the
-	// prop is a string.
+	// IE8 does not support the form attribute and when it is supplied. It overwrites the form prop
+	// with a string, so we need to find the proper form.
 	form: function() {
 		return typeof this[ 0 ].form === "string" ? this.closest( "form" ) : $( this[ 0 ].form );
 	},
