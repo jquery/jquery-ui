6fd9fc92a372e222e3543f52979c601ac88ad81e
Alexander Schmitz <arschmitz@gmail.com>
2015-05-13 22:06:14 -0400

    Tests: Add custom build of core to tests
    This adds a custom build which excludes the event/alias and deprecated modules
    
    Fixes #12770
    Closes gh-1556

--- tests/lib/qunit.js
+++ tests/lib/qunit.js
@@ -27,7 +27,7 @@ QUnit.config.urlConfig.push({
 		"1.11.0", "1.11.1", "1.11.2", "1.11.3",
 		"2.0.0", "2.0.1", "2.0.2", "2.0.3",
 		"2.1.0", "2.1.1", "2.1.2", "2.1.3",
-		"compat-git", "git"
+		"compat-git", "git", "custom"
 	],
 	tooltip: "Which jQuery Core version to test against"
 });
