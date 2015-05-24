b8f5be07fd7a8f43c1a8f173f5688b9ed2c7fd20
Robin van der Vliet <github@robinvandervliet.nl>
2015-05-24 21:00:39 +0200

    Frisian (fy) translation

---
+++ ui/i18n/datepicker-fy.js
@@ -0,0 +1,37 @@
+/* Frisian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Robin van der Vliet <http://robinvandervliet.com/> */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([ "../datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}(function( datepicker ) {
+
+datepicker.regional.fy = {
+	closeText: 'Slute',
+	prevText: 'Foarrige',
+	nextText: 'Folgjende',
+	currentText: 'Hjoed',
+	monthNames: ['jannewaris', 'febrewaris', 'maart', 'april', 'maaie', 'juny',
+	'july', 'augustus', 'septimber', 'oktober', 'novimber', 'desimber'],
+	monthNamesShort: ['jan', 'feb', 'mrt', 'apr', 'mai', 'jun',
+	'jul', 'aug', 'sep', 'okt', 'nov', 'des'],
+	dayNames: ['snein', 'moandei', 'tiisdei', 'woansdei', 'tongersdei', 'freed', 'sneon'],
+	dayNamesShort: ['sni', 'moa', 'tii', 'woa', 'ton', 'fre', 'sno'],
+	dayNamesMin: ['si', 'mo', 'ti', 'wo', 'to', 'fr', 'so'],
+	weekHeader: 'Wk',
+	dateFormat: 'dd-mm-yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''};
+datepicker.setDefaults(datepicker.regional.fy);
+
+return datepicker.regional.fy;
+
+}));
