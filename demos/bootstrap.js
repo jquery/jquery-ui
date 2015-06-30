0930dfc7fa2cbb433031dd6784f84e1ebd9a397c
Alexander Schmitz <arschmitz@gmail.com>
2015-06-30 18:41:41 -0400

    Demos: Add special case for effects and wrap eval code in an IIFE

--- demos/bootstrap.js
+++ demos/bootstrap.js
@@ -7,6 +7,22 @@
 	// Read the modules
 	var modules = script.getAttribute( "data-modules" );
 	var pathParts = window.location.pathname.split( "/" );
+	var effectsAll = [
+		"effect-blind",
+		"effect-bounce",
+		"effect-clip",
+		"effect-drop",
+		"effect-explode",
+		"effect-fade",
+		"effect-fold",
+		"effect-highlight",
+		"effect-puff",
+		"effect-pulsate",
+		"effect-scale",
+		"effect-shake",
+		"effect-size",
+		"effect-slide"
+	];
 
 	require.config( {
 		baseUrl: "../../ui",
@@ -15,11 +31,19 @@
 		}
 	} );
 
+	// Replace effects all shortcut modules with all the effects modules
+	if ( modules && modules.indexOf( "effects-all" ) !== -1 ) {
+		modules = modules.replace( /effects-all/, effectsAll.join( " " ) );
+	}
+
 	modules = modules ? modules.replace( /^\s+|\s+$/g, "" ).split( /\s+/ ) : [];
 	modules.push( pathParts[ pathParts.length - 2 ] );
 
 	require( modules, function() {
 		$( "body" ).css( "visibility", "visible" );
-		eval( $( script ).html() );
+
+		// We wrap the code in an IIFE so that return statements work and we
+		// dont polute the global scope with variables
+		$.globalEval( "(function(){ " + $( script ).html() + "})();" );
 	} );
 })();
