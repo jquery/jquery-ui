118936ef5d32832e3e06cb4d2ebcfecad18292ac
Alexander Schmitz <arschmitz@gmail.com>
2015-08-24 09:00:11 -0400

    Datepicker: Style updates
    Ref #14246

--- ui/i18n/datepicker-af.js
+++ ui/i18n/datepicker-af.js
@@ -12,7 +12,7 @@
 	}
 }( function( datepicker ) {
 
-datepicker.regional[ "af" ] = {
+datepicker.regional.af = {
 	closeText: "Selekteer",
 	prevText: "Vorige",
 	nextText: "Volgende",
@@ -21,17 +21,17 @@ datepicker.regional[ "af" ] = {
 	"Julie","Augustus","September","Oktober","November","Desember" ],
 	monthNamesShort: [ "Jan", "Feb", "Mrt", "Apr", "Mei", "Jun",
 	"Jul", "Aug", "Sep", "Okt", "Nov", "Des" ],
-	dayNames: [ "Sondag", "Maandag", "Dinsdag", 'Woensdag', 'Donderdag', 'Vrydag', 'Saterdag' ],
-	dayNamesShort: [ 'Son', 'Maa', 'Din', 'Woe', 'Don', 'Vry', 'Sat' ],
-	dayNamesMin: [ 'So','Ma','Di','Wo','Do','Vr','Sa' ],
-	weekHeader: 'Wk',
-	dateFormat: 'dd/mm/yy',
+	dayNames: [ "Sondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrydag", "Saterdag" ],
+	dayNamesShort: [ "Son", "Maa", "Din", "Woe", "Don", "Vry", "Sat" ],
+	dayNamesMin: [ "So","Ma","Di","Wo","Do","Vr","Sa" ],
+	weekHeader: "Wk",
+	dateFormat: "dd/mm/yy",
 	firstDay: 1,
 	isRTL: false,
 	showMonthAfterYear: false,
-	yearSuffix: '' };
-datepicker.setDefaults( datepicker.regional[ 'af' ] );
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.af );
 
-return datepicker.regional[ 'af' ];
+return datepicker.regional.af;
 
 } ) );
