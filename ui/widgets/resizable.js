cc8e8bde8442ac4ac9899ccbecda165c955f6b2f
Jörn Zaefferer <joern.zaefferer@gmail.com>
2015-10-01 11:23:19 +0200

    Resizable: Bind to pointer events

--- ui/widgets/resizable.js
+++ ui/widgets/resizable.js
@@ -212,7 +212,7 @@ $.widget( "ui.resizable", $.ui.mouse, {
 					this.handles[ i ] = this.element.children( this.handles[ i ] ).first().show();
 				} else if ( this.handles[ i ].jquery || this.handles[ i ].nodeType ) {
 					this.handles[ i ] = $( this.handles[ i ] );
-					this._on( this.handles[ i ], { "mousedown": that._mouseDown } );
+					this._on( this.handles[ i ], { "pointerdown": that._mouseDown } );
 				}
 
 				if ( this.elementIsWrapper && this.originalElement[ 0 ].nodeName.match( /^(textarea|input|select|button)$/i ) ) {
@@ -241,7 +241,7 @@ $.widget( "ui.resizable", $.ui.mouse, {
 		this._handles = this._handles.add( this.element.find( ".ui-resizable-handle" ) );
 		this._handles.disableSelection();
 
-		this._handles.on( "mouseover", function() {
+		this._handles.on( "pointerover", function() {
 			if ( !that.resizing ) {
 				if ( this.className ) {
 					axis = this.className.match( /ui-resizable-(se|sw|ne|nw|n|e|s|w)/i );
@@ -254,14 +254,14 @@ $.widget( "ui.resizable", $.ui.mouse, {
 			this._handles.hide();
 			this._addClass( "ui-resizable-autohide" );
 			$( this.element )
-				.on( "mouseenter", function() {
+				.on( "pointerenter", function() {
 					if ( o.disabled ) {
 						return;
 					}
 					that._removeClass( "ui-resizable-autohide" );
 					that._handles.show();
 				} )
-				.on( "mouseleave", function() {
+				.on( "pointerleave", function() {
 					if ( o.disabled ) {
 						return;
 					}
