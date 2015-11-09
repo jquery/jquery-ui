228ca7250ab006cacda83c897e66c112312130fa
Scott González <scott.gonzalez@gmail.com>
2015-11-09 09:16:13 -0500

    Resizable: Fix data reference for `ui-resizable-alsoresize`
    Fixes #14788
    
    (cherry picked from commit 7cc88bfe327ba826ffa5199c96c91178862b22f0)

--- ui/resizable.js
+++ ui/resizable.js
@@ -1024,7 +1024,7 @@ $.ui.plugin.add("resizable", "alsoResize", {
 	},
 
 	stop: function() {
-		$(this).removeData("resizable-alsoresize");
+		$( this ).removeData( "ui-resizable-alsoresize" );
 	}
 });
 
