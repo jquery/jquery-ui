61870bdd91ca7fb8264d4aff02ac345b77beaa89
Alexander Schmitz <arschmitz@gmail.com>
2016-03-30 22:52:49 -0400

    Progressbar: Fix line length issues

--- ui/widgets/progressbar.js
+++ ui/widgets/progressbar.js
@@ -128,7 +128,9 @@ return $.widget( "ui.progressbar", {
 	},
 
 	_percentage: function() {
-		return this.indeterminate ? 100 : 100 * ( this.options.value - this.min ) / ( this.options.max - this.min );
+		return this.indeterminate ?
+			100 :
+			100 * ( this.options.value - this.min ) / ( this.options.max - this.min );
 	},
 
 	_refreshValue: function() {
