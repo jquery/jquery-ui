e089b1dd491063d016472ac8163560e8320db180
Amanpreet Singh <apsdehal@gmail.com>
2016-04-03 20:23:34 +0530

    Checkboxradio: Shift to use no globals

--- tests/unit/checkboxradio/events.js
+++ tests/unit/checkboxradio/events.js
@@ -1,14 +1,16 @@
 define( [
+	"qunit",
 	"jquery",
 	"ui/widgets/checkboxradio"
-], function( $ ) {
+], function( QUnit, $ ) {
 
-module( "Checkboxradio: events" );
+QUnit.module( "Checkboxradio: events" );
 
-asyncTest(
+QUnit.test(
 	"Resetting a checkbox's form should refresh the visual state of the checkbox",
 	function( assert ) {
-		expect( 2 );
+		var ready = assert.async();
+		assert.expect( 2 );
 		var form = $( "<form>" +
 			"<label for='c1'></label><input id='c1' type='checkbox' checked>" +
 			"</form>" ),
@@ -22,20 +24,21 @@ asyncTest(
 
 		setTimeout( function() {
 			assert.hasClasses( widget, "ui-state-active" );
-			start();
+			ready();
 		}, 1 );
 	}
 );
 
-asyncTest( "Checkbox shows focus when using keyboard navigation", function( assert ) {
-	expect( 2 );
+QUnit.test( "Checkbox shows focus when using keyboard navigation", function( assert ) {
+	var ready = assert.async();
+	assert.expect( 2 );
 	var check = $( "#check" ).checkboxradio(),
 		label = $( "label[for='check']" );
 	assert.lacksClasses( label, "ui-state-focus" );
 	check.focus();
 	setTimeout( function() {
 		assert.hasClasses( label, "ui-state-focus" );
-		start();
+		ready();
 	} );
 } );
 
--- tests/unit/checkboxradio/methods.js
+++ tests/unit/checkboxradio/methods.js
@@ -1,29 +1,30 @@
 define( [
+	"qunit",
 	"jquery",
 	"ui/widgets/checkboxradio"
-], function( $ ) {
+], function( QUnit, $ ) {
 
-module( "Checkboxradio: methods" );
+QUnit.module( "Checkboxradio: methods" );
 
 $.each( [ "checkbox", "radio" ], function( index, value ) {
-	test( value + ": refresh", function( assert ) {
+	QUnit.test( value + ": refresh", function( assert ) {
 		var widget, icon,
 			checkbox = value === "checkbox",
 			input = $( "#" + value + "-method-refresh" );
 
-		expect( checkbox ? 11 : 8 );
+		assert.expect( checkbox ? 11 : 8 );
 
 		input.checkboxradio();
 
 		widget = input.checkboxradio( "widget" );
 		icon = widget.find( ".ui-icon" );
-		strictEqual( icon.length, 1,
+		assert.strictEqual( icon.length, 1,
 			"There is initally one icon" );
 
 		icon.remove();
 		input.checkboxradio( "refresh" );
 		icon = widget.find( ".ui-icon" );
-		strictEqual( icon.length, 1,
+		assert.strictEqual( icon.length, 1,
 			"Icon is recreated on refresh if absent" );
 		assert.hasClasses( icon, "ui-icon-blank" );
 		if ( checkbox ) {
@@ -48,49 +49,49 @@ $.each( [ "checkbox", "radio" ], function( index, value ) {
 		assert.lacksClasses( widget, "ui-checkboxradio-checked" );
 	} );
 
-	test( value + ": destroy", function( assert ) {
-		expect( 1 );
+	QUnit.test( value + ": destroy", function( assert ) {
+		assert.expect( 1 );
 		assert.domEqual( "#" + value + "-method-destroy", function() {
 			$( "#" + value + "-method-destroy" ).checkboxradio().checkboxradio( "destroy" );
 		} );
 	} );
 
-	test( value + ": disable / enable", function( assert ) {
-		expect( 4 );
+	QUnit.test( value + ": disable / enable", function( assert ) {
+		assert.expect( 4 );
 		var input = $( "#" + value + "-method-disable" ),
 			widget = input.checkboxradio().checkboxradio( "widget" );
 
 		input.checkboxradio( "disable" );
 		assert.hasClasses( widget, "ui-state-disabled" );
-		strictEqual( input.is( ":disabled" ), true,
+		assert.strictEqual( input.is( ":disabled" ), true,
 			value + " is disabled when disable is called" );
 
 		input.checkboxradio( "enable" );
 		assert.lacksClasses( widget, "ui-state-disabled" );
-		strictEqual( input.is( ":disabled" ), false,
+		assert.strictEqual( input.is( ":disabled" ), false,
 			value + " has disabled prop removed when enable is called" );
 	} );
 
-	test(  value + ": widget returns the label", function() {
-		expect( 1 );
+	QUnit.test(  value + ": widget returns the label", function( assert ) {
+		assert.expect( 1 );
 
 		var input = $( "#" + value + "-method-refresh" ),
 			label = $( "#" + value + "-method-refresh-label" );
 
 		input.checkboxradio();
-		strictEqual( input.checkboxradio( "widget" )[ 0 ], label[ 0 ],
+		assert.strictEqual( input.checkboxradio( "widget" )[ 0 ], label[ 0 ],
 			"widget method returns label" );
 	} );
 } );
 
-test( "Input wrapped in a label preserved on refresh", function() {
+QUnit.test( "Input wrapped in a label preserved on refresh", function( assert ) {
 	var input = $( "#label-with-no-for" ).checkboxradio(),
 		element = input.checkboxradio( "widget" );
 
-	expect( 1 );
+	assert.expect( 1 );
 
 	input.checkboxradio( "refresh" );
-	strictEqual( input.parent()[ 0 ], element[ 0 ], "Input preserved" );
+	assert.strictEqual( input.parent()[ 0 ], element[ 0 ], "Input preserved" );
 } );
 
 } );
