f375fd1300b4e08c0e71a073131260dfb8e5762f
Amanpreet Singh <apsdehal@gmail.com>
2016-04-03 20:13:27 +0530

    Button: Shift to no globals

--- tests/unit/button/events.js
+++ tests/unit/button/events.js
@@ -1,18 +1,20 @@
 define( [
+	"qunit",
 	"jquery",
 	"ui/widgets/button"
-], function( $ ) {
+], function( QUnit, $ ) {
 
-module( "Button: events" );
+QUnit.module( "Button: events" );
 
-asyncTest( "Anchor recieves click event when spacebar is pressed", function() {
-	expect( 1 );
+QUnit.test( "Anchor recieves click event when spacebar is pressed", function( assert ) {
+	var ready = assert.async();
+	assert.expect( 1 );
 	var element = $( "#anchor-button" ).button();
 
 	element.on( "click", function( event ) {
 		event.preventDefault();
-		ok( true, "click occcured as a result of spacebar" );
-		start();
+		assert.ok( true, "click occcured as a result of spacebar" );
+		ready();
 	} );
 
 	element.trigger( $.Event( "keyup", { keyCode: $.ui.keyCode.SPACE } ) );
--- tests/unit/button/methods.js
+++ tests/unit/button/methods.js
@@ -1,32 +1,33 @@
 define( [
+	"qunit",
 	"jquery",
 	"ui/widgets/button"
-], function( $ ) {
+], function( QUnit, $ ) {
 
-module( "Button: methods" );
+QUnit.module( "Button: methods" );
 
-test( "destroy", function( assert ) {
-	expect( 1 );
+QUnit.test( "destroy", function( assert ) {
+	assert.expect( 1 );
 	assert.domEqual( "#button", function() {
 		$( "#button" ).button().button( "destroy" );
 	} );
 } );
 
-test( "refresh: Ensure disabled state is preserved correctly.", function() {
-	expect( 3 );
+QUnit.test( "refresh: Ensure disabled state is preserved correctly.", function( assert ) {
+	assert.expect( 3 );
 
 	var element = $( "<a href='#'></a>" );
 	element.button( { disabled: true } ).button( "refresh" );
-	ok( element.button( "option", "disabled" ),
+	assert.ok( element.button( "option", "disabled" ),
 		"Anchor button should remain disabled after refresh" );
 
 	element = $( "<button></button>" );
 	element.button( { disabled: true } ).button( "refresh" );
-	ok( element.button( "option", "disabled" ), "<button> should remain disabled after refresh" );
+	assert.ok( element.button( "option", "disabled" ), "<button> should remain disabled after refresh" );
 
 	element = $( "<button></button>" );
 	element.button( { disabled: true } ).prop( "disabled", false ).button( "refresh" );
-	ok( !element.button( "option", "disabled" ),
+	assert.ok( !element.button( "option", "disabled" ),
 		"Changing a <button>'s disabled property should update the state after refresh." );
 
 } );
