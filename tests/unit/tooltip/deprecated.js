92f122d5b1c1eaa77ae0ab3af8bf68bf268e300d
Amanpreet Singh <apsdehal@gmail.com>
2016-04-06 19:29:12 +0530

    Tooltip: Shift to use no globals

--- tests/unit/tooltip/deprecated.js
+++ tests/unit/tooltip/deprecated.js
@@ -1,12 +1,13 @@
 define( [
+	"qunit",
 	"jquery",
 	"ui/widgets/tooltip"
-], function( $ ) {
+], function( QUnit, $ ) {
 
-module( "tooltip: (deprecated) options" );
+QUnit.module( "tooltip: (deprecated) options" );
 
-test( "tooltipClass", function( assert ) {
-	expect( 1 );
+QUnit.test( "tooltipClass", function( assert ) {
+	assert.expect( 1 );
 	var element = $( "#tooltipped1" ).tooltip( {
 		tooltipClass: "custom"
 	} ).tooltip( "open" );
