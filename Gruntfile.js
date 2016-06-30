23d1db58458cd6e33aaf45cbd078328a8a1cf8bf
Scott González <scott.gonzalez@gmail.com>
2016-06-30 10:02:33 -0400

    Build: Adjust author info for Dan Strohl

--- Gruntfile.js
+++ Gruntfile.js
@@ -432,9 +432,7 @@ grunt.registerTask( "update-authors", function() {
 		}
 
 		authors = authors.map(function( author ) {
-			if ( author.match( /^Dan Strohl </ ) ) {
-				return "Dan Strohl";
-			} else if ( author.match( /^Jacek Jędrzejewski </ ) ) {
+			if ( author.match( /^Jacek Jędrzejewski </ ) ) {
 				return "Jacek Jędrzejewski (http://jacek.jedrzejewski.name)";
 			} else if ( author.match( /^Pawel Maruszczyk </ ) ) {
 				return "Pawel Maruszczyk (http://hrabstwo.net)";
