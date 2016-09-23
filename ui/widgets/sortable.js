1d409528a164c550e4e167c367f33ab3b7ad0e66
Robin <mail@robin-fowler.com>
2016-09-23 12:40:36 +0100

    Sortable: Fix parent offset detection
    Fixes #15021
    Closes gh-1749

--- ui/widgets/sortable.js
+++ ui/widgets/sortable.js
@@ -224,17 +224,21 @@ return $.widget( "ui.sortable", $.ui.mouse, {
 				left: event.pageX - this.offset.left,
 				top: event.pageY - this.offset.top
 			},
-			parent: this._getParentOffset(),
 
 			// This is a relative to absolute position minus the actual position calculation -
 			// only used for relative positioned helper
 			relative: this._getRelativeOffset()
 		} );
 
-		// Only after we got the offset, we can change the helper's position to absolute
+		// After we get the helper offset, but before we get the parent offset we can
+		// change the helper's position to absolute
 		// TODO: Still need to figure out a way to make relative sorting possible
 		this.helper.css( "position", "absolute" );
 		this.cssPosition = this.helper.css( "position" );
+
+		$.extend( this.offset, {
+			parent: this._getParentOffset()
+		} );
 
 		//Generate the original position
 		this.originalPosition = this._generatePosition( event );
