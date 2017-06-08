4a1e8e71fc6532077f3be5da233a93ae00cf1c24
dorinbujor93 <dbu@dinnerbooking.com>
2017-06-08 16:09:47 +0300

    Spaces to Tabs

--- ui/widgets/datepicker.js
+++ ui/widgets/datepicker.js
@@ -88,8 +88,8 @@ function Datepicker() {
 		prevText: "Prev", // Display text for previous month link
 		nextText: "Next", // Display text for next month link
 		currentText: "Today", // Display text for current month link
-        weekHeaderText: "Week number", // Display text for week column header
-        monthNames: [ "January","February","March","April","May","June",
+		weekHeaderText: "Week number", // Display text for week column header
+		monthNames: [ "January","February","March","April","May","June",
 			"July","August","September","October","November","December" ], // Names of months for drop-down and formatting
 		monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ], // For formatting
 		dayNames: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ], // For formatting
