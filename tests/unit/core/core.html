jquery/jquery-ui

commit d3025968f349c37a8ca41bfc63ee1b37d9d7354f

Focusable: Fix handling of `visibility: inherit`
Ref #14596
Ref gh-1583
Closes gh-1605

ui/focusable.js
@@ -42,8 +42,19 @@ $.ui.focusable = function( element, hasTabindex ) {
 		"a" === nodeName ?
 			element.href || hasTabindex :
 			hasTabindex ) &&
-		$( element ).is( ":visible" ) && $( element ).css( "visibility" ) === "visible";
+		$( element ).is( ":visible" ) && visible( $( element ) );
 };
+
+// Support: IE 8 only
+// IE 8 doesn't resolve inherit to visible/hidden for computed values
+function visible( element ) {
+	var visibility = element.css( "visibility" );
+	while ( visibility === "inherit" ) {
+		element = element.parent();
+		visibility = element.css( "visibility" );
+	}
+	return visibility !== "hidden";
+}
 
 $.extend( $.expr[ ":" ], {
 	focusable: function( element ) {

tests/unit/core/core.html
@@ -81,10 +81,20 @@
 		<input id="visibilityHiddenAncestor-input">
 		<span tabindex="1" id="visibilityHiddenAncestor-span">.</span>
 
-		<span id="nestedVisibilityOverrideAncestor" style="visibility: visible">
+		<span id="nestedVisibilityOverrideAncestor" style="visibility: visible;">
 			<input id="nestedVisibilityOverrideAncestor-input">
 			<span tabindex="1" id="nestedVisibilityOverrideAncestor-span">.</span>
 		</span>
+
+		<span tabIndex="1" id="nestedVisibilityInheritWithHiddenAncestor"
+			style="visibility: inherit;">.</span>
+		<input id="nestedVisibilityInheritWithHiddenAncestor-input" style="visibility: inherit;">
+	</div>
+
+	<div id="visibilityVisibleAncestor" style="visibility: visible;">
+		<span tabIndex="1" id="nestedVisibilityInheritWithVisibleAncestor"
+			style="visibility: inherit;">.</span>
+		<input id="nestedVisibilityInheritWithVisibleAncestor-input" style="visibility: inherit;">
 	</div>
 
 	<span tabindex="1" id="displayNone-span" style="display: none;">.</span>

tests/unit/core/selector.js
@@ -88,7 +88,7 @@ test( "data", function() {
 } );
 
 test( "focusable - visible, enabled elements", function() {
-	expect( 18 );
+	expect( 20 );
 
 	isNotFocusable( "#formNoTabindex", "form" );
 	isFocusable( "#formTabindex", "form with tabindex" );
@@ -108,6 +108,10 @@ test( "focusable - visible, enabled elements", function() {
 	isNotFocusable( "#visibleAncestor-div", "div" );
 	isFocusable( "#visibleAncestor-spanWithTabindex", "span with tabindex" );
 	isFocusable( "#visibleAncestor-divWithNegativeTabindex", "div with tabindex" );
+	isFocusable( "#nestedVisibilityInheritWithVisibleAncestor",
+			"span, visibility: inherit inside visibility: visible parent" );
+	isFocusable( "#nestedVisibilityInheritWithVisibleAncestor-input",
+			"input, visibility: inherit inside visibility: visible parent" );
 } );
 
 test( "focusable - disabled elements", function() {
@@ -125,7 +129,7 @@ test( "focusable - disabled elements", function() {
 } );
 
 test( "focusable - hidden styles", function() {
-	expect( 10 );
+	expect( 12 );
 
 	isNotFocusable( "#displayNoneAncestor-input", "input, display: none parent" );
 	isNotFocusable( "#displayNoneAncestor-span", "span with tabindex, display: none parent" );
@@ -135,6 +139,9 @@ test( "focusable - hidden styles", function() {
 
 	isFocusable( "#nestedVisibilityOverrideAncestor-input", "input, visibility: visible parent but visibility: hidden grandparent" );
 	isFocusable( "#nestedVisibilityOverrideAncestor-span", "span with tabindex, visibility: visible parent but visibility: hidden grandparent " );
+
+	isNotFocusable( "#nestedVisibilityInheritWithHiddenAncestor", "span, visibility: inherit inside visibility: hidden parent" );
+	isNotFocusable( "#nestedVisibilityInheritWithHiddenAncestor-input", "input, visibility: inherit inside visibility: hidden parent" );
 
 	isNotFocusable( "#displayNone-input", "input, display: none" );
 	isNotFocusable( "#visibilityHidden-input", "input, visibility: hidden" );

commit b3a9875af61a558f5ee566feb491511b254ad020

Focusable: Fix handling of `visibility: collapse`
"collapse" is similar to "hidden", with a slight difference in the case
of tr/tbody/td/colgroup elements.
See https://www.w3.org/TR/CSS22/visufx.html#visibility
See https://www.w3.org/TR/CSS22/tables.html#dynamic-effects
See https://developer.mozilla.org/en-US/docs/Web/CSS/visibility#Table_example

"visibility: collapse" elements are always not focusable, though.

Commit d3025968f34 introduced a regression by testing with `!== "hidden"`
instead of `=== "visible"`.

ui/focusable.js
@@ -70,7 +70,7 @@ function visible( element ) {
 		element = element.parent();
 		visibility = element.css( "visibility" );
 	}
-	return visibility !== "hidden";
+	return visibility === "visible";
 }
 
 $.extend( $.expr.pseudos, {

tests/unit/core/core.html
@@ -108,9 +108,17 @@
 
 	<span tabindex="1" id="displayNone-span" style="display: none;">.</span>
 	<span tabindex="1" id="visibilityHidden-span" style="visibility: hidden;">.</span>
+	<span tabindex="1" id="visibilityCollapse-span" style="visibility: collapse;">.</span>
 
 	<input id="displayNone-input" style="display: none;">
 	<input id="visibilityHidden-input" style="visibility: hidden;">
+	<input id="visibilityCollapse-input" style="visibility: collapse;">
+
+	<table>
+		<tr>
+			<td tabindex="1" id="visibilityCollapse-td" style="visibility: collapse;">.</td>
+		</tr>
+	</table>
 </div>
 
 <div>

tests/unit/core/selector.js
@@ -132,7 +132,7 @@ QUnit.test( "focusable - disabled elements", function( assert ) {
 } );
 
 QUnit.test( "focusable - hidden styles", function( assert ) {
-	assert.expect( 12 );
+	assert.expect( 15 );
 
 	assert.isNotFocusable( "#displayNoneAncestor-input", "input, display: none parent" );
 	assert.isNotFocusable( "#displayNoneAncestor-span", "span with tabindex, display: none parent" );
@@ -148,9 +148,13 @@ QUnit.test( "focusable - hidden styles", function( assert ) {
 
 	assert.isNotFocusable( "#displayNone-input", "input, display: none" );
 	assert.isNotFocusable( "#visibilityHidden-input", "input, visibility: hidden" );
+	assert.isNotFocusable( "#visibilityCollapse-input", "input, visibility: collapse" );
 
 	assert.isNotFocusable( "#displayNone-span", "span with tabindex, display: none" );
 	assert.isNotFocusable( "#visibilityHidden-span", "span with tabindex, visibility: hidden" );
+	assert.isNotFocusable( "#visibilityCollapse-span", "span with tabindex, visibility: collapse" );
+
+	assert.isNotFocusable( "#visibilityCollapse-td", "td with tabindex, visibility: collapse" );
 } );
 
 QUnit.test( "focusable - natively focusable with various tabindex", function( assert ) {