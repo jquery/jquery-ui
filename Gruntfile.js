f56059ee5d86cbc3023bf738adec97fbf40fd32c
Eric Miller <eric.miller@moodys.com>
2020-12-16 15:11:10 -0600

    Created eslint.json config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,26 +176,31 @@ grunt.initConfig( {
 			}
 		}
 	},
-
-	jscs: {
-		ui: {
-			options: {
-				config: true
-			},
-			files: {
-				src: [ "demos/**/*.js", "build/**/*.js", "ui/**/*.js" ]
-			}
+	eslint:{
+		options: {
+			configFile: "eslint.json"
 		},
-		tests: {
-			options: {
-				config: true,
-				maximumLineLength: null
-			},
-			files: {
-				src: [ "tests/**/*.js" ]
-			}
-		}
+		target:["demos/**/*.js", "build/**/*.js", "ui/**/*.js"]
 	},
+	// jscs: {
+	// 	ui: {
+	// 		options: {
+	// 			config: true
+	// 		},
+	// 		files: {
+	// 			src: [ "demos/**/*.js", "build/**/*.js", "ui/**/*.js" ]
+	// 		}
+	// 	},
+	// 	tests: {
+	// 		options: {
+	// 			config: true,
+	// 			maximumLineLength: null
+	// 		},
+	// 		files: {
+	// 			src: [ "tests/**/*.js" ]
+	// 		}
+	// 	}
+	// },
 	uglify: minify,
 	htmllint: {
 		good: {
@@ -508,7 +513,7 @@ grunt.registerTask( "update-authors", function() {
 
 grunt.registerTask( "default", [ "lint", "requirejs", "test" ] );
 grunt.registerTask( "jenkins", [ "default", "concat" ] );
-grunt.registerTask( "lint", [ "asciilint", "jshint", "jscs", "csslint", "htmllint" ] );
+grunt.registerTask( "lint", [ "asciilint", "jshint", "eslint", "csslint", "htmllint" ] );
 grunt.registerTask( "test", [ "qunit" ] );
 grunt.registerTask( "sizer", [ "requirejs:js", "uglify:main", "compare_size:all" ] );
 grunt.registerTask( "sizer_all", [ "requirejs:js", "uglify", "compare_size" ] );
