03ccd3436190132b0e9caadd7968a2db46daa63d
Tatiana Kiseleva <takrin54@gmail.com>
2020-12-18 06:59:53 +0000

    #15262: Correct codestyle.

--- ui/widgets/slider.js
+++ ui/widgets/slider.js
@@ -556,8 +556,8 @@ return $.widget( "ui.slider", $.ui.mouse, {
 		max = parseFloat( max.toFixed( this._precision() ) );
 		var optionMax = parseFloat( this.options.max.toFixed( this._precision() ) );
 
-		if ( this.options.includeMax) {
-			if (max < optionMax ) {
+		if ( this.options.includeMax ) {
+			if ( max < optionMax ) {
 
 				// If max is not divisible by step and max from options
 				// should be covered, add one more step.
