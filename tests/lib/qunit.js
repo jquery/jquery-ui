2fee4d817b50ad94f6b66da2e0774922f9164b03
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2021-03-07 14:08:12 +0100

    Build: Add jQuery 3.6.0, update Migrate from 3.3.1 to 3.3.2

--- tests/lib/qunit.js
+++ tests/lib/qunit.js
@@ -34,6 +34,7 @@ QUnit.config.urlConfig.push( {
 		"3.3.0", "3.3.1",
 		"3.4.0", "3.4.1",
 		"3.5.0", "3.5.1",
+		"3.6.0",
 		"3.x-git", "git", "custom"
 	],
 	tooltip: "Which jQuery Core version to test against"
