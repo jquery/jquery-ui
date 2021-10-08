c5aef9b4b2ce78e1703cfd3e6ed47cd62a7015ea
Josep Sanz <josep.sanz@saltos.org>
2021-10-09 00:24:45 +0200

    Widget & Tooltip: Regression from 1.12.1
    The 1.13.0 release remove the try-catch that prevents spurious errors
    
    Fixes: 1f2011e
    Closes: jquerygh-1990

--- ui/widget.js
+++ ui/widget.js
@@ -36,12 +36,16 @@ $.cleanData = ( function( orig ) {
 	return function( elems ) {
 		var events, elem, i;
 		for ( i = 0; ( elem = elems[ i ] ) != null; i++ ) {
+			try {
 
-			// Only trigger remove when necessary to save time
-			events = $._data( elem, "events" );
-			if ( events && events.remove ) {
-				$( elem ).triggerHandler( "remove" );
-			}
+				// Only trigger remove when necessary to save time
+				events = $._data( elem, "events" );
+				if ( events && events.remove ) {
+					$( elem ).triggerHandler( "remove" );
+				}
+
+			// Http://bugs.jquery.com/ticket/8235
+			} catch ( e ) {}
 		}
 		orig( elems );
 	};

jquery/jquery-ui

commit 1f2011ece3fe6847874677e9a8210fa202498ccb

Widget: Don't swallow errors in `remove` events
The try/catch was only there to support jQuery <1.6.3, which we no
longer support.

Ref jquery/jquery#3554

ui/widget.js
@@ -32,16 +32,12 @@ $.cleanData = ( function( orig ) {
 	return function( elems ) {
 		var events, elem, i;
 		for ( i = 0; ( elem = elems[ i ] ) != null; i++ ) {
-			try {
 
-				// Only trigger remove when necessary to save time
-				events = $._data( elem, "events" );
-				if ( events && events.remove ) {
-					$( elem ).triggerHandler( "remove" );
-				}
-
-			// Http://bugs.jquery.com/ticket/8235
-			} catch ( e ) {}
+			// Only trigger remove when necessary to save time
+			events = $._data( elem, "events" );
+			if ( events && events.remove ) {
+				$( elem ).triggerHandler( "remove" );
+			}
 		}
 		orig( elems );
 	};

commit c5aef9b4b2ce78e1703cfd3e6ed47cd62a7015ea

Widget & Tooltip: Regression from 1.12.1
The 1.13.0 release remove the try-catch that prevents spurious errors

Fixes: 1f2011e
Closes: jquerygh-1990

ui/widget.js
@@ -36,12 +36,16 @@ $.cleanData = ( function( orig ) {
 	return function( elems ) {
 		var events, elem, i;
 		for ( i = 0; ( elem = elems[ i ] ) != null; i++ ) {
+			try {
 
-			// Only trigger remove when necessary to save time
-			events = $._data( elem, "events" );
-			if ( events && events.remove ) {
-				$( elem ).triggerHandler( "remove" );
-			}
+				// Only trigger remove when necessary to save time
+				events = $._data( elem, "events" );
+				if ( events && events.remove ) {
+					$( elem ).triggerHandler( "remove" );
+				}
+
+			// Http://bugs.jquery.com/ticket/8235
+			} catch ( e ) {}
 		}
 		orig( elems );
 	};