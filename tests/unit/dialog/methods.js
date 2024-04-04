jquery/jquery-ui

commit 7adb13ac7c30a7ba33f1db256952be77b9e7a310

Dialog: Fix IE tests about interactions with other dialogs
The workaround triggering a second delayed focus to a tabbable element was
applied in jQueries 3.4 & 3.5 to fix IE issues. It also needs to be applied
in jQuery 3.6 which this commit adds.

This will no longer be needed in jQuery 3.7 and jQuery 3.x-git tests are
already passing because of that.

Closes gh-2160

ui/widgets/dialog.js
@@ -881,12 +881,15 @@ $.widget( "ui.dialog", {
 					event.preventDefault();
 					instance._focusTabbable();
 
-					// Support: jQuery >=3.4 <3.6 only
-					// Focus re-triggering in jQuery 3.4/3.5 makes the original element
-					// have its focus event propagated last, breaking the re-targeting.
-					// Trigger focus in a delay in addition if needed to avoid the issue
+					// Support: jQuery >=3.4 <3.7 only
+					// In jQuery 3.4-3.6, there are multiple issues with focus/blur
+					// trigger chains or when triggering is done on a hidden element
+					// at least once.
+					// Trigger focus in a delay in addition if needed to avoid the issues.
 					// See https://github.com/jquery/jquery/issues/4382
-					if ( jqMinor === "3.4." || jqMinor === "3.5." ) {
+					// See https://github.com/jquery/jquery/issues/4856
+					// See https://github.com/jquery/jquery/issues/4950
+					if ( jqMinor === "3.4." || jqMinor === "3.5." || jqMinor === "3.6." ) {
 						instance._delay( instance._restoreTabbableFocus );
 					}
 				}

commit ccc2a2e172ecfd6cf42c2c3781830bf6954b72f5

Tests: Workaround a jQuery 1.8 issue with one draggable test
The test "#8958: dialog can be opened while opening" had special behavior in
jQuery 1.8 and the patch from commit 7adb13ac7c30a7ba33f1db256952be77b9e7a310
broke those assumptions. Adjust the test so that it passes again.

tests/unit/dialog/methods.js
@@ -217,6 +217,8 @@ QUnit.test( "#8958: dialog can be opened while opening", function( assert ) {
 	var ready = assert.async( 3 );
 	assert.expect( 1 );
 
+	var jQuery1Dot8 = jQuery.fn.jquery.indexOf( "1.8" ) === 0;
+
 	var element = $( "<div>" ).dialog( {
 		autoOpen: false,
 		modal: true,
@@ -246,6 +248,15 @@ QUnit.test( "#8958: dialog can be opened while opening", function( assert ) {
 		// handle a call to the open() method during the process of the dialog
 		// being opened.
 		.on( "blur", function() {
+
+			// Support: jQuery 1.8 only
+			// The test never fully worked in jQuery 1.8 since the input's blur
+			// handler never fired. It's only been uncovered when `ready()` calls
+			// have been added to all handlers. Skip this handler in jQuery 1.8.
+			if ( jQuery1Dot8 ) {
+				return;
+			}
+
 			element.dialog( "open" );
 
 			// Detach the handlers to avoid firing them outside of this
@@ -255,6 +266,13 @@ QUnit.test( "#8958: dialog can be opened while opening", function( assert ) {
 			ready();
 		} )
 		.trigger( "focus" );
+
+	// Support: jQuery 1.8 only
+	// Account for the skipped `ready()` call above. To make sure the count
+	// is constant, call it here instead.
+	if ( jQuery1Dot8 ) {
+		ready();
+	}
 } );
 
 QUnit.test( "#5531: dialog width should be at least minWidth on creation", function( assert ) {